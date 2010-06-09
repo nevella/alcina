4aaae5627d1eaa963e73a99fda6f489619ec9145
nick.reddel@gmail.com <nick.reddel@gmail.com@812e3da0-21a7-11df-99f8-b513f4f1e6c6>
2010-06-09 19:13:44 +0000

    move template folders
    git-svn-id: https://alcina.googlecode.com/svn/trunk@118 812e3da0-21a7-11df-99f8-b513f4f1e6c6

--- template/entities/src/cc/alcina/template/cs/constants/AlcinaTemplateSiteConstants.java
+++
@@ -1,6 +0,0 @@
-package cc.alcina.template.cs.constants;
-
-public class AlcinaTemplateSiteConstants {
-	public static final String VERSION = "build: m0.1-pre";
-	public static final String DOMAIN = "alcina.cc";
-}
--- template/entities/src/cc/alcina/template/cs/csobjects/AlcinaTemplateObjectsProvider.java
+++
@@ -1,8 +0,0 @@
-package cc.alcina.template.cs.csobjects;
-
-import cc.alcina.framework.common.client.logic.template.AlcinaTemplate;
-
-@AlcinaTemplate
-public interface AlcinaTemplateObjectsProvider {
-	public AlcinaTemplateObjects getAlcinaTemplateObjects();
-}
