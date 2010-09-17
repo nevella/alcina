e27ce56ad880b35284de22c04ddd7b7a4ae80802
nick.reddel@gmail.com <nick.reddel@gmail.com@812e3da0-21a7-11df-99f8-b513f4f1e6c6>
2010-09-17 13:42:20 +0000

    Minor cleanup to wrapped object access
    git-svn-id: https://alcina.googlecode.com/svn/trunk@232 812e3da0-21a7-11df-99f8-b513f4f1e6c6

--- template/entity/src/cc/alcina/template/entityaccess/AlcinaTemplateWrappedObjectProvider.java
+++ template/entity/src/cc/alcina/template/entityaccess/AlcinaTemplateWrappedObjectProvider.java
@@ -16,26 +16,27 @@ import cc.alcina.template.cs.constants.AlcinaTemplateAccessConstants;
 import cc.alcina.template.cs.persistent.AlcinaTemplateUser;
 
 @SuppressWarnings("unchecked")
-public class AlcinaTemplateWrappedObjectProvider implements WrappedObjectProvider {
+public class AlcinaTemplateWrappedObjectProvider implements
+		WrappedObjectProvider {
 	public <T extends WrapperPersistable> WrappedObjectImpl<T> getObjectWrapperForUser(
 			Class<T> c, long id, EntityManager em) throws Exception {
-		WrappedObjectImpl<T> wrappedObject = em.find(
-				WrappedObjectImpl.class, id);
-		if (wrappedObject == null && !(GwtMultiplePersistable.class.isAssignableFrom(c))) {
-			String userName = (PersistentSingleton.class.isAssignableFrom(c)) ? AlcinaTemplateAccessConstants.SYSTEM_USER
-					: PermissionsManager.get().getUserName();
+		WrappedObjectImpl<T> wrappedObject = em.find(WrappedObjectImpl.class,
+				id);
+		AlcinaTemplateUser user = (PersistentSingleton.class
+				.isAssignableFrom(c)) ? new AlcinaTemplateCommonPersistence(em)
+				.getSystemUser() : (AlcinaTemplateUser) PermissionsManager
+				.get().getUser();
+		if (wrappedObject == null
+				&& !(GwtMultiplePersistable.class.isAssignableFrom(c))) {
 			List l = em
 					.createQuery(
-							"from WrappedObjectImpl w where w.user.userName = ? and w.className = ?")
-					.setParameter(1, userName).setParameter(2, c.getName())
+							"from WrappedObjectImpl w where w.user.id = ? and w.className = ?")
+					.setParameter(1, user.getId()).setParameter(2, c.getName())
 					.getResultList();
 			wrappedObject = (WrappedObjectImpl) ((l.size() == 0) ? null : l
 					.get(0));
 		}
 		if (wrappedObject == null) {
-			AlcinaTemplateUser user = (PersistentSingleton.class.isAssignableFrom(c)) ? new AlcinaTemplateCommonPersistence(
-					em).getSystemUser()
-					: (AlcinaTemplateUser) PermissionsManager.get().getUser();
 			wrappedObject = new WrappedObjectImpl();
 			em.persist(wrappedObject);
 			wrappedObject.setUser(user);
@@ -45,19 +46,18 @@ public class AlcinaTemplateWrappedObjectProvider implements WrappedObjectProvide
 		return wrappedObject;
 	}
 
-	public <T extends WrapperPersistable> T getWrappedObjectForUser(
-			Class<T> c, EntityManager em) throws Exception {
+	public <T extends WrapperPersistable> T getWrappedObjectForUser(Class<T> c,
+			EntityManager em) throws Exception {
 		return getWrappedObjectForUser(c, 0, em);
 	}
 
-	public <T extends WrapperPersistable> T getWrappedObjectForUser(
-			Class<T> c, long id, EntityManager em) throws Exception {
-		WrappedObjectImpl<T> wrappedObject = getObjectWrapperForUser(c, id,
-				em);
+	public <T extends WrapperPersistable> T getWrappedObjectForUser(Class<T> c,
+			long id, EntityManager em) throws Exception {
+		WrappedObjectImpl<T> wrappedObject = getObjectWrapperForUser(c, id, em);
 		return wrappedObject.getObject();
 	}
 
 	public List<Class> getJaxbSubclasses() {
-		 return Registry.get().lookup(JaxbContextRegistration.class);
+		return Registry.get().lookup(JaxbContextRegistration.class);
 	}
 }
