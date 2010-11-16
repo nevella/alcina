56d8bb0d69f641819641ed2a97ac00e1d4779f89
nick.reddel@gmail.com <nick.reddel@gmail.com@812e3da0-21a7-11df-99f8-b513f4f1e6c6>
2010-11-16 21:35:39 +0000

    Test transform manager
    git-svn-id: https://alcina.googlecode.com/svn/trunk@267 812e3da0-21a7-11df-99f8-b513f4f1e6c6

---
+++ framework/common/src/cc/alcina/framework/common/client/logic/domaintransform/TestTransformManager.java
@@ -0,0 +1,8 @@
+package cc.alcina.framework.common.client.logic.domaintransform;
+
+import cc.alcina.framework.common.client.logic.domain.HasIdAndLocalId;
+
+public class TestTransformManager extends ClientTransformManager {
+	public void deregisterObject(HasIdAndLocalId hili) {
+	}
+}
