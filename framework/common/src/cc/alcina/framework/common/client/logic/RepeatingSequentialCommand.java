ae9e309bed379ca85c7f42867f19627c1ff3d01e
nick.reddel@gmail.com <nick.reddel@gmail.com@812e3da0-21a7-11df-99f8-b513f4f1e6c6>
2011-04-09 00:04:41 +0000

    Add exception handling
    git-svn-id: https://alcina.googlecode.com/svn/trunk@350 812e3da0-21a7-11df-99f8-b513f4f1e6c6

--- framework/common/src/cc/alcina/framework/common/client/logic/RepeatingSequentialCommand.java
+++ framework/common/src/cc/alcina/framework/common/client/logic/RepeatingSequentialCommand.java
@@ -3,29 +3,38 @@ package cc.alcina.framework.common.client.logic;
 import java.util.ArrayList;
 import java.util.List;
 
+import cc.alcina.framework.common.client.WrappedRuntimeException;
+
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.RepeatingCommand;
 
 public class RepeatingSequentialCommand implements RepeatingCommand {
 	private List<RepeatingCommand> tasks = new ArrayList<RepeatingCommand>();
-	public void cancel(){
+
+	public void cancel() {
 		tasks.clear();
 	}
-	public void add(RepeatingCommand task){
-		if(tasks.isEmpty()){
+
+	public void add(RepeatingCommand task) {
+		if (tasks.isEmpty()) {
 			Scheduler.get().scheduleIncremental(this);
 		}
 		tasks.add(task);
-		
 	}
+
 	@Override
 	public boolean execute() {
-		if(tasks.isEmpty()){
+		if (tasks.isEmpty()) {
 			return false;
 		}
-		boolean result = tasks.get(0).execute();
-		if(!result){
-			tasks.remove(0);
+		try {
+			boolean result = tasks.get(0).execute();
+			if (!result) {
+				tasks.remove(0);
+			}
+		} catch (Exception e) {
+			cancel();
+			throw new WrappedRuntimeException(e);
 		}
 		return !tasks.isEmpty();
 	}
