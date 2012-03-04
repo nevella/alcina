3c65b2e357b251ace4212c0f685719c22394a578
nick.reddel@gmail.com <nick.reddel@gmail.com@812e3da0-21a7-11df-99f8-b513f4f1e6c6>
2012-03-04 17:28:04 +0000

    Add a null notifier
    git-svn-id: https://alcina.googlecode.com/svn/trunk@608 812e3da0-21a7-11df-99f8-b513f4f1e6c6

--- framework/gwt/src/cc/alcina/framework/gwt/client/widget/ModalNotifier.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/widget/ModalNotifier.java
@@ -10,4 +10,32 @@ public interface ModalNotifier {
 	public void setStatus(String status);
 	
 	public void setProgress(double progress);
+	public static class ModalNotifierNull implements ModalNotifier{
+
+		@Override
+		public void modalOn() {
+			
+		}
+
+		@Override
+		public void modalOff() {
+			
+		}
+
+		@Override
+		public void setMasking(boolean masking) {
+			
+		}
+
+		@Override
+		public void setStatus(String status) {
+			
+		}
+
+		@Override
+		public void setProgress(double progress) {
+			
+		}
+		
+	}
 }
