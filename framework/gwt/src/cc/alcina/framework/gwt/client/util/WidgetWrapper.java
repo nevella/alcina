05abec9d92feed244f2fd8ab6792f5c179efa9b4
nick.reddel@gmail.com <nick.reddel@gmail.com@812e3da0-21a7-11df-99f8-b513f4f1e6c6>
2012-04-18 20:42:55 +0000

    Add hasclickablehandlers
    git-svn-id: https://alcina.googlecode.com/svn/trunk@644 812e3da0-21a7-11df-99f8-b513f4f1e6c6

--- framework/gwt/src/cc/alcina/framework/gwt/client/util/WidgetWrapper.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/util/WidgetWrapper.java
@@ -5,13 +5,17 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 
-public class WidgetWrapper extends Composite {
+public class WidgetWrapper extends Composite implements HasClickHandlers {
 	private FlowPanel fp;
 
 	private WidgetElementReplacer replacer;
@@ -71,5 +75,9 @@ public class WidgetWrapper extends Composite {
 			return fp;
 		}
 		return null;
+	}
+
+	public HandlerRegistration addClickHandler(ClickHandler handler) {
+		return addDomHandler(handler, ClickEvent.getType());
 	}
 }
