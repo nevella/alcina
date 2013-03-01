226976778f7c06ae87f16c09f7a4bf1e6d287aed
Nick Reddel <nick.reddel@gmail.com>
2013-03-01 11:18:55 -0800

    CountingMap now implements Linked-

--- framework/common/src/cc/alcina/framework/common/client/util/CountingMap.java
+++ framework/common/src/cc/alcina/framework/common/client/util/CountingMap.java
@@ -1,13 +1,12 @@
 package cc.alcina.framework.common.client.util;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 
-public class CountingMap<K> extends HashMap<K, Integer> {
+public class CountingMap<K> extends LinkedHashMap<K, Integer> {
 	public void add(K key) {
 		if (!containsKey(key)) {
 			put(key, 1);
