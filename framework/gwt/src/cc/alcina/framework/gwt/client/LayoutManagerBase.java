6588f3860acdd796fea822db9f153bc79ad3eb96
Nick Reddel <nick.reddel@gmail.com>
2013-06-08 16:04:50 -0700

    Move to the consort/dependency model

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/LayoutManagerBase.java
@@ -0,0 +1,55 @@
+package cc.alcina.framework.gwt.client;
+
+import cc.alcina.framework.gwt.client.widget.layout.LayoutEvents;
+import cc.alcina.framework.gwt.client.widget.layout.LayoutEvents.LayoutEvent;
+import cc.alcina.framework.gwt.client.widget.layout.LayoutEvents.LayoutEventListener;
+
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
+
+public abstract class LayoutManagerBase implements
+LayoutEventListener, ResizeHandler{
+	public void setDisplayInitialised(boolean displayInitialised) {
+		this.displayInitialised = displayInitialised;
+	}
+
+	public boolean isDisplayInitialised() {
+		return displayInitialised;
+	}
+	public LayoutManagerBase() {
+		Window.addResizeHandler(this);
+		LayoutEvents.get().addLayoutEventListener(this);
+	}
+	private boolean displayInitialised = false;
+	
+	public void onResize(ResizeEvent event) {
+		onWindowResized(Window.getClientWidth(), Window.getClientHeight(), true);
+	}
+	public abstract void redrawLayout();
+	private int lastWidth, lastHeight;
+
+	// prevents old IE resize infinite loop
+	protected boolean onWindowResized(int clientWidth, int clientHeight,
+			boolean fromBrowser) {
+		if (fromBrowser && lastWidth == clientWidth
+				&& lastHeight == clientHeight) {
+			return false;
+		}
+		lastWidth = clientWidth;
+		lastHeight = clientHeight;
+		return !isLayoutInitialising();
+	}
+
+	public void onLayoutEvent(LayoutEvent event) {
+		if (isLayoutInitialising()) {
+			return;
+		}
+		onWindowResized(Window.getClientWidth(), Window.getClientHeight(),
+				false);
+	}
+
+	public boolean isLayoutInitialising(){
+		return displayInitialised;
+	}
+}
