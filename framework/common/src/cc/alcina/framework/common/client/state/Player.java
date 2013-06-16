4656d48de176ff61a5e0bf45dabf2e4e086cb27e
Nick Reddel <nick.reddel@gmail.com>
2013-06-16 08:37:41 -0700

    tweak default play behaviour

--- framework/common/src/cc/alcina/framework/common/client/state/Player.java
+++ framework/common/src/cc/alcina/framework/common/client/state/Player.java
@@ -93,7 +93,7 @@ public abstract class Player<D> {
 		} else {
 			runnable.run();
 		}
-		if (!isAsynchronous() && getProvides().size() == 1) {
+		if (!isAsynchronous() && getProvides().size() <= 1) {
 			wasPlayed();
 		}
 	}
