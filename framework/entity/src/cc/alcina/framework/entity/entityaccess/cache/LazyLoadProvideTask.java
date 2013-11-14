af284cfbfd74cddd091b75c542cc942b83a3a06f
Nick Reddel <nick.reddel@gmail.com>
2013-11-14 14:54:18 -0800

    generalise memcache lazy loading, with eviction

---
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/LazyLoadProvideTask.java
@@ -0,0 +1,82 @@
+package cc.alcina.framework.entity.entityaccess.cache;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import cc.alcina.framework.common.client.logic.domain.HasIdAndLocalId;
+import cc.alcina.framework.entity.entityaccess.cache.CacheDescriptor.PreProvideTask;
+
+public abstract class LazyLoadProvideTask<T extends HasIdAndLocalId> implements
+		PreProvideTask<T> {
+	private long minEvictionAge;
+
+	private int minEvictionSize;
+
+	private Class<T> clazz;
+
+	public LazyLoadProvideTask(long minEvictionAge, int minEvictionSize,
+			Class<T> clazz) {
+		this.minEvictionAge = minEvictionAge;
+		this.minEvictionSize = minEvictionSize;
+		this.clazz = clazz;
+	}
+
+	private LinkedHashMap<Long, Long> idEvictionAge = new LinkedHashMap<Long, Long>();
+
+	private synchronized List<T> requireLazyLoad(Collection<T> objects) {
+		List<T> result = new ArrayList<T>();
+		for (T t : objects) {
+			Long evictionAge = idEvictionAge.get(t.getId());
+			if (evictionAge == null) {
+				result.add(t);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public void run(AlcinaMemCache alcinaMemCache, Class clazz,
+			Collection<T> objects) throws Exception {
+		if (clazz != this.clazz) {
+			return;
+		}
+		List<T> requireLoad = requireLazyLoad(objects);
+		if (!requireLoad.isEmpty()) {
+			if (!beforeLazyLoad(requireLoad)) {
+				return;
+			}
+			synchronized (this) {
+				// reget, just in case of interim eviction
+				requireLoad = requireLazyLoad(objects);
+				lazyLoad(alcinaMemCache, requireLoad);
+				registerLoaded(alcinaMemCache, requireLoad);
+			}
+		}
+	}
+
+	private void registerLoaded(AlcinaMemCache alcinaMemCache, List<T> requireLoad) {
+		for (T t : requireLoad) {
+			idEvictionAge.remove(t.getId());
+			idEvictionAge.put(t.getId(), System.currentTimeMillis());
+		}
+		Iterator<Entry<Long, Long>> itr = idEvictionAge.entrySet().iterator();
+		while (idEvictionAge.size() > minEvictionSize && itr.hasNext()) {
+			Entry<Long, Long> entry = itr.next();
+			if ((System.currentTimeMillis() - entry.getValue()) > minEvictionAge) {
+				evict(alcinaMemCache,entry.getKey());
+				itr.remove();
+			}
+		}
+	}
+
+	protected abstract void evict(AlcinaMemCache alcinaMemCache, Long key);
+
+	protected abstract void lazyLoad(AlcinaMemCache alcinaMemCache,
+			Collection<T> objects) throws Exception;
+
+	protected abstract boolean beforeLazyLoad(List<T> toLoad);
+}
