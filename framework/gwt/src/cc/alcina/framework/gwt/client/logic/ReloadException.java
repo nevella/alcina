05bf8234f1df04a7285a487acb3035293448a6e9
Nick Reddel <nick.reddel@gmail.com>
2014-01-15 12:07:24 -0800

    reloadme exception

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/logic/ReloadException.java
@@ -0,0 +1,13 @@
+package cc.alcina.framework.gwt.client.logic;
+
+public class ReloadException extends RuntimeException {
+
+	public ReloadException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ReloadException(String message) {
+		super(message);
+	}
+	
+}
