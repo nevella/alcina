ab56a89566bc0464fb8aa4a42ff6ec5690cf6123
Nick Reddel <nick.reddel@gmail.com>
2014-04-02 12:14:37 -0700

    just use one timer thread for our gwt timer emulation

---
+++ framework/entity/src/cc/alcina/framework/entity/util/TimerWrapperProviderJvm.java
@@ -0,0 +1,58 @@
+package cc.alcina.framework.entity.util;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cc.alcina.framework.common.client.util.TimerWrapper;
+import cc.alcina.framework.common.client.util.TimerWrapper.TimerWrapperProvider;
+
+/**
+ * XP (JVM/GWT) timer functionality
+ * 
+ * @author nreddel@barnet.com.au
+ * 
+ */
+/*
+ * will always be a singleton
+ */
+public class TimerWrapperProviderJvm implements TimerWrapperProvider {
+	@Override
+	public TimerWrapper getTimer(Runnable runnable) {
+		return new TimerWrapperJvm(runnable);
+	}
+
+	@Override
+	public void scheduleDeferred(Runnable runnable) {
+		getTimer(runnable).scheduleSingle(1);
+	}
+
+	private Timer timer = new Timer();
+
+	public class TimerWrapperJvm implements TimerWrapper {
+		private TimerTask task;
+
+		private TimerWrapperJvm(final Runnable runnable) {
+			task = new TimerTask() {
+				@Override
+				public void run() {
+					runnable.run();
+				}
+			};
+		}
+
+		@Override
+		public void cancel() {
+			task.cancel();
+		}
+
+		@Override
+		public void scheduleRepeating(long periodMillis) {
+			timer.scheduleAtFixedRate(task, periodMillis, periodMillis);
+		}
+
+		@Override
+		public void scheduleSingle(long delayMillis) {
+			timer.schedule(task, delayMillis);
+		}
+	}
+}
