de93d198a01d94b3c5fbe4f06410e23e69f82936
Nick Reddel <nick.reddel@gmail.com>
2014-04-09 17:02:46 -0700

    selector shift-click

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/widget/HasClickAndDownHandlers.java
@@ -0,0 +1,8 @@
+package cc.alcina.framework.gwt.client.widget;
+
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasMouseDownHandlers;
+
+public interface HasClickAndDownHandlers extends HasClickHandlers,
+		HasMouseDownHandlers {
+}
