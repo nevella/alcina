3fcd8c4e208409bcee780557d3b896c05face4f7
Nick Reddel <nick.reddel@gmail.com>
2014-07-21 11:08:19 -0700

    * convenience username logging * js formatting

--- framework/common/src/cc/alcina/framework/common/client/logic/permissions/PermissionsManager.java
+++ framework/common/src/cc/alcina/framework/common/client/logic/permissions/PermissionsManager.java
@@ -45,6 +45,7 @@ import cc.alcina.framework.common.client.logic.reflection.Permission.SimplePermi
 import cc.alcina.framework.common.client.logic.reflection.PropertyPermissions;
 import cc.alcina.framework.common.client.logic.reflection.RegistryLocation;
 import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
+import cc.alcina.framework.common.client.util.CommonUtils;
 import cc.alcina.framework.common.client.util.LooseContext;
 import cc.alcina.framework.common.client.util.TopicPublisher.GlobalTopicPublisher;
 import cc.alcina.framework.common.client.util.TopicPublisher.TopicListener;
@@ -755,5 +756,9 @@ public class PermissionsManager implements Vetoer, DomainTransformListener {
 
 	public void pushCurrentUser() {
 		pushUser(getUser(), getLoginState());
+	}
+
+	public String getUserString() {
+		return CommonUtils.formatJ("%s/%s", getUserId(), getUserName());
 	}
 }
