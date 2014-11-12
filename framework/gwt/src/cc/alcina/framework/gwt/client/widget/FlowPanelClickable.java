1b9e18ff06e13518fc6c76b3269798ddfad4f31e
Nick Reddel <nick.reddel@gmail.com>
2014-11-12 15:51:49 -0800

    ws

--- framework/gwt/src/cc/alcina/framework/gwt/client/widget/FlowPanelClickable.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/widget/FlowPanelClickable.java
@@ -19,8 +19,11 @@ import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.FlowPanel;
 
 public class FlowPanelClickable extends FlowPanel implements HasClickHandlers,
-		HasMouseMoveHandlers,HasMouseDownHandlers ,HasKeyUpHandlers,
-		HasKeyPressHandlers{
+		HasMouseMoveHandlers, HasMouseDownHandlers, HasKeyUpHandlers,
+		HasKeyPressHandlers {
+	public FlowPanelClickable() {
+	}
+
 	@Override
 	public HandlerRegistration addClickHandler(ClickHandler handler) {
 		return addDomHandler(handler, ClickEvent.getType());
@@ -30,10 +33,12 @@ public class FlowPanelClickable extends FlowPanel implements HasClickHandlers,
 	public HandlerRegistration addMouseMoveHandler(MouseMoveHandler handler) {
 		return addDomHandler(handler, MouseMoveEvent.getType());
 	}
+
 	@Override
 	public HandlerRegistration addMouseDownHandler(MouseDownHandler handler) {
 		return addDomHandler(handler, MouseDownEvent.getType());
 	}
+
 	@Override
 	public HandlerRegistration addKeyUpHandler(KeyUpHandler handler) {
 		return addDomHandler(handler, KeyUpEvent.getType());
