6eeac5ed31c41ab4ce9b5320573efb048107a6ab
Nick Reddel <nick.reddel@gmail.com>
2015-01-29 03:38:10 -0800

    Some GWT/Cell overrides to allow better tree syncing

--- framework/gwt/src/cc/alcina/framework/gwt/client/util/WidgetUtils.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/util/WidgetUtils.java
@@ -152,10 +152,10 @@ public class WidgetUtils {
 	public static native String getComputedStyle(Element elt,
 			String attributeName)/*-{
 	if (elt.currentStyle) {
-	    return elt.currentStyle[attributeName];
+		return elt.currentStyle[attributeName];
 	}
 	if ($wnd.getComputedStyle) {
-	    return $wnd.getComputedStyle(elt, null)[attributeName];
+		return $wnd.getComputedStyle(elt, null)[attributeName];
 	}
 	}-*/;
 
@@ -512,19 +512,19 @@ public class WidgetUtils {
 
 	public static native Element getFocussedDocumentElement()/*-{
 	if ($doc.activeElement) {
-	    var tagName = $doc.activeElement.tagName.toLowerCase();
-	    return tagName != "body" && tagName != "html" ? $doc.activeElement
-		    : null;
+		var tagName = $doc.activeElement.tagName.toLowerCase();
+		return tagName != "body" && tagName != "html" ? $doc.activeElement
+			: null;
 	}
 	return null;
 	}-*/;
 
 	public static native void clearFocussedDocumentElement()/*-{
 	if ($doc.activeElement) {
-	    var tagName = $doc.activeElement.tagName.toLowerCase();
-	    if (tagName != "body" && tagName != "html") {
+		var tagName = $doc.activeElement.tagName.toLowerCase();
+		if (tagName != "body" && tagName != "html") {
 		$doc.activeElement.blur();
-	    }
+		}
 	}
 	}-*/;
 
@@ -780,8 +780,8 @@ public class WidgetUtils {
 	public native static int getRelativeTopTo(Element elem, Element end) /*-{
 	var top = 0;
 	while (elem != end) {
-	    top += elem.offsetTop;
-	    elem = elem.offsetParent;
+		top += elem.offsetTop;
+		elem = elem.offsetParent;
 	}
 	return top;
 	}-*/;
@@ -789,6 +789,7 @@ public class WidgetUtils {
 	public static void scrollIntoView(Element e, int fromTop) {
 		scrollIntoView(e, fromTop, false);
 	}
+
 
 	public static void scrollIntoView(Element e, int fromTop,
 			boolean forceFromTop) {
@@ -876,8 +877,8 @@ public class WidgetUtils {
 	var curr = elem;
 	// This intentionally excludes body which has a null offsetParent.
 	while (curr.offsetParent) {
-	    left -= curr.scrollLeft;
-	    curr = curr.parentNode;
+		left -= curr.scrollLeft;
+		curr = curr.parentNode;
 	}
 
 	return left;
@@ -888,8 +889,8 @@ public class WidgetUtils {
 	var curr = elem;
 	// This intentionally excludes body which has a null offsetParent.
 	while (curr.offsetParent) {
-	    top -= curr.scrollTop;
-	    curr = curr.parentNode;
+		top -= curr.scrollTop;
+		curr = curr.parentNode;
 	}
 	return top;
 	}-*/;
@@ -898,9 +899,9 @@ public class WidgetUtils {
 
 	var e = doc.getElementById(name);
 	if (!e) {
-	    e = doc.getElementsByName(name)
-		    && doc.getElementsByName(name).length == 1 ? doc
-		    .getElementsByName(name)[0] : null;
+		e = doc.getElementsByName(name)
+			&& doc.getElementsByName(name).length == 1 ? doc
+			.getElementsByName(name)[0] : null;
 	}
 	return e;
 	}-*/;
@@ -908,29 +909,29 @@ public class WidgetUtils {
 	public static native String getComputedStyleProperty(Element elem,
 			String strCssRule) /*-{
 	if ($doc.defaultView && $doc.defaultView.getComputedStyle) {
-	    strValue = $doc.defaultView.getComputedStyle(elem, "")
-		    .getPropertyValue(strCssRule);
+		strValue = $doc.defaultView.getComputedStyle(elem, "")
+			.getPropertyValue(strCssRule);
 	} else if (oElm.currentStyle) {
-	    strCssRule = strCssRule.replace(/\-(\w)/g, function(strMatch, p1) {
+		strCssRule = strCssRule.replace(/\-(\w)/g, function(strMatch, p1) {
 		return p1.toUpperCase();
-	    });
-	    strValue = oElm.currentStyle[strCssRule];
+		});
+		strValue = oElm.currentStyle[strCssRule];
 	}
 	return strValue;
 	}-*/;
 
 	public static native int getOffsetHeightWithMargins(Element elem) /*-{
 	if (elem.style.display == 'none') {
-	    return 0;
+		return 0;
 	}
 	var h = elem.offsetHeight;
 	var marginTop = @cc.alcina.framework.gwt.client.util.WidgetUtils::getComputedStyle(Lcom/google/gwt/dom/client/Element;Ljava/lang/String;)(elem,"margin");
 	var marginBottom = @cc.alcina.framework.gwt.client.util.WidgetUtils::getComputedStyle(Lcom/google/gwt/dom/client/Element;Ljava/lang/String;)(elem,"margin");
 	if (marginTop.indexOf("px") != -1) {
-	    h += parseInt(marginTop.substring(0, marginTop.length - 2));
+		h += parseInt(marginTop.substring(0, marginTop.length - 2));
 	}
 	if (marginBottom.indexOf("px") != -1) {
-	    h += parseInt(marginBottom.substring(0, marginBottom.length - 2));
+		h += parseInt(marginBottom.substring(0, marginBottom.length - 2));
 	}
 	return h;
 	}-*/;
@@ -950,7 +951,7 @@ public class WidgetUtils {
 	public static native NodeList getElementsForSelector(Element elt,
 			String selector) /*-{
 	if (!($doc.querySelector)) {
-	    return null;
+		return null;
 	}
 	var from = (elt) ? elt : $doc;
 	return from.querySelectorAll(selector);
@@ -959,30 +960,30 @@ public class WidgetUtils {
 	public static native Element getElementForSelector(Element elt,
 			String selector) /*-{
 	if (!($doc.querySelector)) {
-	    return null;
+		return null;
 	}
 	var from = (elt) ? elt : $doc;
 	var splits = selector.split("::");
 	for (var idx = 0; idx < splits.length; idx += 2) {
-	    var selectorPart = splits[idx];
-	    var textRegex = idx == splits.length - 1 ? null : splits[idx + 1];
-	    if (textRegex == null) {
+		var selectorPart = splits[idx];
+		var textRegex = idx == splits.length - 1 ? null : splits[idx + 1];
+		if (textRegex == null) {
 		return from.querySelector(selectorPart);
-	    }
-	    var nl = from.querySelectorAll(splits[idx]);
-	    var found = false;
-	    for (var i = 0; i < nl.length; i++) {
+		}
+		var nl = from.querySelectorAll(splits[idx]);
+		var found = false;
+		for (var i = 0; i < nl.length; i++) {
 		var item = nl[i];
 		if (item.innerHTML.indexOf(textRegex) != -1
 			|| item.innerHTML.match(new RegExp(textRegex))) {
-		    from = item;
-		    found = true;
-		    break;
+			from = item;
+			found = true;
+			break;
 		}
-	    }
-	    if (!found) {
+		}
+		if (!found) {
 		return null;
-	    }
+		}
 	}
 	return from;
 	}-*/;
@@ -994,7 +995,7 @@ public class WidgetUtils {
 	public static final native void click(Element elt) /*-{
 	elt.click();
 	try {
-	    elt.focus();
+		elt.focus();
 	} catch (e) {
 
 	}
@@ -1007,7 +1008,7 @@ public class WidgetUtils {
 
 	private static native void cancelPossibleIEShortcut() /*-{
 	try {
-	    $wnd.event.keyCode = 0; // this is a hack to capture ctrl+f ctrl+p etc
+		$wnd.event.keyCode = 0; // this is a hack to capture ctrl+f ctrl+p etc
 	} catch (e) {
 
 	}
