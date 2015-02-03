1a9a82d76cc2a36306f707736bdb87d15a14cf85
Nick Reddel <nick.reddel@gmail.com>
2015-02-02 19:50:02 -0800

    Handle restrictions in virtual/detached criteria

--- framework/impl/jboss-hibernate/src/cc/alcina/framework/entity/impl/jboss/VTHandler.java
+++ framework/impl/jboss-hibernate/src/cc/alcina/framework/entity/impl/jboss/VTHandler.java
@@ -6,7 +6,7 @@ import org.hibernate.criterion.DetachedCriteria;
 import cc.alcina.framework.common.client.search.CriteriaGroup;
 
 public interface VTHandler {
-	public DetachedCriteria createDetachedCriteria();
+	public DetachedCriteria createDetachedCriteria(HibernateEJBSearcherBase searcher);
 	public void prepareLink(Criteria toCriteria, DetachedCriteria detachedCriteria);
 	public void link(CriteriaGroup cg);
 }
