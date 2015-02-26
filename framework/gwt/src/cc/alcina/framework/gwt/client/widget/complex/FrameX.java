a2c6ec9666ddacb251b00cea8bf8f4db9c5c37d2
Nick Reddel <nick.reddel@gmail.com>
2015-02-26 12:39:02 -0800

    fix frame/pub display for ff

--- framework/gwt/src/cc/alcina/framework/gwt/client/widget/complex/FrameX.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/widget/complex/FrameX.java
@@ -1,17 +1,16 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package cc.alcina.framework.gwt.client.widget.complex;
 
 import com.google.gwt.dom.client.Document;
@@ -21,14 +20,14 @@ import com.google.gwt.user.client.ui.Frame;
  *
  * @author Nick Reddel
  */
-
- public class FrameX extends Frame {
+public class FrameX extends Frame {
 	public native Document getDocument()/*-{
-		return null;
+	return null;
 	}-*/;
-/**
- * Should not include '<html>', '</html>' tags
- */
+
+	/**
+	 * Should not include '<html>', '</html>' tags
+	 */
 	public void setDocumentHtml(String html) {
 		this.html = html;
 		if (isAttached()) {
@@ -47,11 +46,16 @@ import com.google.gwt.user.client.ui.Frame;
 	}
 
 	private native void _setDocumentHtml(String html)/*-{
-		var elt = this.@cc.alcina.framework.gwt.client.widget.complex.FrameX::getElement()();
-		var oDoc = elt.contentWindow || elt.contentDocument;
-		if (oDoc.document) {
-			oDoc = oDoc.document;
-		}
-		oDoc.documentElement.innerHTML=html;
+	var elt = this.@cc.alcina.framework.gwt.client.widget.complex.FrameX::getElement()();
+	var oDoc = elt.contentWindow || elt.contentDocument;
+	if (oDoc.document) {
+		oDoc = oDoc.document;
+	}
+
+	//Trigger a page "load" (ff issue)
+	oDoc.open();
+	oDoc.close();
+	oDoc.documentElement.innerHTML = html;
+
 	}-*/;
 }
