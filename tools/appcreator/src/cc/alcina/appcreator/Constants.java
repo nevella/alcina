e1be4a2dbe8930d5a0a877121e8848b022c38b31
Nick Reddel <nick.reddel@gmail.com>
2015-04-23 21:02:12 -0700

    appcreator

--- tools/appcreator/src/cc/alcina/appcreator/Constants.java
+++ tools/appcreator/src/cc/alcina/appcreator/Constants.java
@@ -3,6 +3,7 @@ package cc.alcina.appcreator;
 class Constants {
 
 	public static final String TMP_DOWNLOAD_DIR = "tmp.dir";
+	public static final String ALCINA_DOWNLOAD_LIBS_LOCAL = "alcina.DownloadLibs.local";
 	public static final String APP_NAME_LC = "app.name.lc";
 	public static final String APP_NAME = "app.name";
 	public static final String APP_PACKAGE = "app.package";
