8f5711d62fa91618db4af637109ac6b24c3facd4
Nick Reddel <nick.reddel@gmail.com>
2016-03-17 09:00:47 -0700

    cleanup debug logs

--- framework/entity/src/cc/alcina/framework/entity/util/ClasspathScanner.java
+++ framework/entity/src/cc/alcina/framework/entity/util/ClasspathScanner.java
@@ -121,7 +121,6 @@ public class ClasspathScanner {
 
 		@Override
 		public void enumerateClasses(URL url) throws Exception {
-			System.out.println("enumerate - "+url);
 			String file = sanitizeFileURL(url);
 			submitted.incrementAndGet();
 			executor.execute(() -> getClassesFromDirectory(file, file));
