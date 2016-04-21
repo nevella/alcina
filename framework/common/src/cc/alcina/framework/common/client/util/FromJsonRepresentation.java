fc063b9ea94742a8b2aaf121f60ba330f434c6b2
Nick Reddel <nick.reddel@gmail.com>
2016-04-21 13:49:17 -0700

    helpers for json access

--- framework/common/src/cc/alcina/framework/common/client/util/FromJsonRepresentation.java
+++ framework/common/src/cc/alcina/framework/common/client/util/FromJsonRepresentation.java
@@ -1,32 +1,75 @@
 package cc.alcina.framework.common.client.util;
 
 import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.function.Function;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.google.gwt.core.shared.GwtIncompatible;
-
 import cc.alcina.framework.common.client.WrappedRuntimeException;
 import cc.alcina.framework.entity.projection.GraphProjection;
 
 public interface FromJsonRepresentation {
+	static public final DateFormat CONVERSION_DATE_FORMAT = new SimpleDateFormat(
+			"yyyy/MM/dd HH:mm:ss +0000");
+
 	default void fieldMapping(JSONObject jso) {
 		try {
 			Field[] fields = new GraphProjection().getFieldsForClass(this);
 			for (Field field : fields) {
 				String key = field.getName();
 				if (jso.has(key)) {
-					field.set(this, jso.get(key));
+					Object value = jso.get(key);
+					if (value instanceof JSONArray) {
+						value = jsArrayToList((JSONArray) value,
+								Function.identity());
+					}
+					if (field.getType() == Date.class && value != null) {
+						value = CONVERSION_DATE_FORMAT.parse(value.toString());
+					}
+					field.set(this, value);
 				}
 			}
 		} catch (Exception e) {
 			throw new WrappedRuntimeException(e);
 		}
 	}
+
+	public static <V> List<V> jsArrayToList(JSONArray array,
+			Function function) {
+		try {
+			List<V> list = new ArrayList<>();
+			for (int idx = 0; idx < array.length(); idx++) {
+				list.add((V) function.apply(array.get(idx)));
+			}
+			return list;
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static <V> Map<String, V> jsMapToMap(JSONObject jMap) {
+		try {
+			Map<String, V> map = new LinkedHashMap<>();
+			Iterator<String> itr = jMap.keys();
+			for (; itr.hasNext();) {
+				String k = itr.next();
+				map.put(k, (V) jMap.get(k));
+			}
+			return map;
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	void fromJson(JSONObject jso);
 }
--- framework/entity/src/cc/alcina/framework/entity/util/JsonPropertyAccessor.java
+++ framework/entity/src/cc/alcina/framework/entity/util/JsonPropertyAccessor.java
@@ -16,19 +16,23 @@ import cc.alcina.framework.common.client.util.TopicPublisher.GlobalTopicPublishe
 import cc.alcina.framework.entity.domaintransform.MethodIndividualPropertyAccessor;
 
 public class JsonPropertyAccessor implements PropertyAccessor {
-	private boolean ignoreNullWrites;
-
-	public JsonPropertyAccessor() {
-		this(true);
-	}
-
 	public static final String TOPIC_NOTIFICATION_MULTIPLE_JSON_SINGLE_JAVA = JsonPropertyAccessor.class
 			.getName() + ".TOPIC_NOTIFICATION_MULTIPLE_JSON_SINGLE_JAVA";
 
 	public static final String CONTEXT_IGNORE_MISSING_JSON_VALUES = JsonPropertyAccessor.class
 			.getName() + ".CONTEXT_IGNORE_MISSING_JSON_VALUES";
 
+	private boolean ignoreNullWrites;
+
 	private String defaultObjectPrefix;
+
+	private boolean returnNullIfNotFound;
+
+	private boolean returnJsonArray;
+
+	public JsonPropertyAccessor() {
+		this(true);
+	}
 
 	public JsonPropertyAccessor(boolean ignoreNullWrites) {
 		this.ignoreNullWrites = ignoreNullWrites;
@@ -38,9 +42,75 @@ public class JsonPropertyAccessor implements PropertyAccessor {
 		this.defaultObjectPrefix = defaultObjectPrefix;
 	}
 
-	public void setPropertyValue(Object bean, String propertyName, Object value) {
+	@Override
+	public IndividualPropertyAccessor cachedAccessor(Class clazz,
+			String propertyName) {
+		return new MethodIndividualPropertyAccessor(clazz, propertyName);
+	}
+
+	public <T> T get(Object bean, String propertyName) {
+		return (T) getPropertyValue(bean, propertyName);
+	}
+
+	public <A extends Annotation> A getAnnotationForProperty(Class targetClass,
+			Class<A> annotationClass, String propertyName) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Class getPropertyType(Class clazz, String propertyName) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Object getPropertyValue(Object bean, String propertyName) {
+		try {
+			ResolvedJson resolved = new ResolvedJson(bean, propertyName, false);
+			if (resolved.invalid) {
+				return maybeThrow(propertyName);
+			}
+			JSONObject jsonObject = resolved.leaf;
+			propertyName = resolved.resolvedPropertyName;
+			if (!jsonObject.has(propertyName)) {
+				return maybeThrow(propertyName);
+			}
+			Object value = jsonObject.get(propertyName);
+			if (value instanceof JSONArray &&! returnJsonArray) {
+				if (((JSONArray) value).length() == 1) {
+					return ((JSONArray) value).get(0);
+				} else if (((JSONArray) value).length() == 0) {
+					return null;
+				} else {
+					GlobalTopicPublisher.get().publishTopic(
+							TOPIC_NOTIFICATION_MULTIPLE_JSON_SINGLE_JAVA,
+							new Object[] { jsonObject, bean, propertyName });
+					return ((JSONArray) value).get(0);
+				}
+			}
+			if (value == JSONObject.NULL) {
+				value = null;
+			}
+			return (value instanceof String && ((String) value).isEmpty())
+					? null : value;
+		} catch (Exception e) {
+			throw WrappedRuntimeException.wrapIfNotRuntime(e);
+		}
+	}
+
+	private Object maybeThrow(String propertyName) {
+		if (LooseContext.is(CONTEXT_IGNORE_MISSING_JSON_VALUES)
+				|| returnNullIfNotFound) {
+			return null;
+		} else {
+			throw new NoSuchVariantPropertyException(propertyName);
+		}
+	}
+
+	public void setPropertyValue(Object bean, String propertyName,
+			Object value) {
 		try {
 			ResolvedJson resolved = new ResolvedJson(bean, propertyName, true);
+			if (resolved.invalid) {
+				throw new NoSuchVariantPropertyException(propertyName);
+			}
 			JSONObject jsonObject = resolved.leaf;
 			propertyName = resolved.resolvedPropertyName;
 			if (ignoreNullWrites && value == null) {
@@ -52,7 +122,22 @@ public class JsonPropertyAccessor implements PropertyAccessor {
 		}
 	}
 
+	public JsonPropertyAccessor returnNullIfNotFound() {
+		this.returnNullIfNotFound = true;
+		return this;
+	}
+	public JsonPropertyAccessor returnJsonArray() {
+		this.returnJsonArray = true;
+		return this;
+	}
+
 	class ResolvedJson {
+		JSONObject leaf;
+
+		String resolvedPropertyName;
+
+		boolean invalid = false;
+
 		public ResolvedJson(Object bean, String propertyName, boolean create)
 				throws JSONException {
 			leaf = (JSONObject) bean;
@@ -72,8 +157,11 @@ public class JsonPropertyAccessor implements PropertyAccessor {
 						leaf = leaf.getJSONObject(part);
 						resolvedPropertyName = resolvedPropertyName
 								.substring(idx + 1);
-					} else if (leaf.get(part) instanceof JSONArray
-							&& arrayIndex != -1) {
+					} else if (leaf.get(part) instanceof JSONArray) {
+						if (arrayIndex == -1) {
+							invalid = true;
+							return;
+						}
 						JSONArray array = leaf.getJSONArray(part);
 						if (array.length() == arrayIndex && create) {
 							leaf = new JSONObject();
@@ -85,8 +173,8 @@ public class JsonPropertyAccessor implements PropertyAccessor {
 							resolvedPropertyName = resolvedPropertyName
 									.substring(idx + 1);
 						} else {
-							throw new NoSuchVariantPropertyException(
-									propertyName);
+							invalid = true;
+							return;
 						}
 					}
 				} else if (create && defaultObjectPrefix == null) {
@@ -112,59 +200,5 @@ public class JsonPropertyAccessor implements PropertyAccessor {
 				}
 			}
 		}
-
-		JSONObject leaf;
-
-		String resolvedPropertyName;
-	}
-
-	public Object getPropertyValue(Object bean, String propertyName) {
-		try {
-			ResolvedJson resolved = new ResolvedJson(bean, propertyName, false);
-			JSONObject jsonObject = resolved.leaf;
-			propertyName = resolved.resolvedPropertyName;
-			if (!jsonObject.has(propertyName)) {
-				if (LooseContext.is(CONTEXT_IGNORE_MISSING_JSON_VALUES)) {
-					return null;
-				} else {
-					throw new NoSuchVariantPropertyException(propertyName);
-				}
-			}
-			Object value = jsonObject.get(propertyName);
-			if (value instanceof JSONArray) {
-				if (((JSONArray) value).length() == 1) {
-					return ((JSONArray) value).get(0);
-				} else if (((JSONArray) value).length() == 0) {
-					return null;
-				} else {
-					GlobalTopicPublisher.get().publishTopic(
-							TOPIC_NOTIFICATION_MULTIPLE_JSON_SINGLE_JAVA,
-							new Object[] { jsonObject, bean, propertyName });
-					return ((JSONArray) value).get(0);
-				}
-			}
-			if (value == JSONObject.NULL) {
-				value = null;
-			}
-			return (value instanceof String && ((String) value).isEmpty()) ? null
-					: value;
-		} catch (Exception e) {
-			throw WrappedRuntimeException.wrapIfNotRuntime(e);
-		}
-	}
-
-	@Override
-	public IndividualPropertyAccessor cachedAccessor(Class clazz,
-			String propertyName) {
-		return new MethodIndividualPropertyAccessor(clazz, propertyName);
-	}
-
-	public <A extends Annotation> A getAnnotationForProperty(Class targetClass,
-			Class<A> annotationClass, String propertyName) {
-		throw new UnsupportedOperationException();
-	}
-
-	public Class getPropertyType(Class clazz, String propertyName) {
-		throw new UnsupportedOperationException();
 	}
 }
