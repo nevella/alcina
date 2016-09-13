fbb8c646211a63161bf916b892bbf6d8421f159b
Nick Reddel <nick.reddel@gmail.com>
2016-09-13 16:43:43 -0700

    Fs logging (gwt)

---
+++ framework/clientreflectorjvm/src/cc/alcina/framework/common/client/logic/reflection/jvm/FsLogger.java
@@ -0,0 +1,15 @@
+package cc.alcina.framework.common.client.logic.reflection.jvm;
+
+import cc.alcina.framework.common.client.WrappedRuntimeException;
+import cc.alcina.framework.entity.ResourceUtilities;
+
+public class FsLogger {
+	public void write(String log){
+		try {
+			ResourceUtilities.writeStringToFile(log, "/tmp/gwt-fs-log.txt");
+		} catch (Exception e) {
+			throw new WrappedRuntimeException(e);
+		}
+		
+	}
+}
---
+++ framework/clientreflectorjvm/src/cc/alcina/framework/common/translatable/cc/alcina/framework/common/client/logic/reflection/jvm/FsLogger.java
@@ -0,0 +1,10 @@
+package cc.alcina.framework.common.client.logic.reflection.jvm;
+
+import com.google.gwt.core.client.GwtScriptOnly;
+
+@GwtScriptOnly
+public class FsLogger {
+	public void write(String log){
+		
+	}
+}
