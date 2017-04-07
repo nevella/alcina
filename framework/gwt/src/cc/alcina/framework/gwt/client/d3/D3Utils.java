1530e9e51934d192e0084c5cabac838a1f60d798
Nick Reddel <nick.reddel@gmail.com>
2017-04-07 13:32:46 -0700

    d3 utils

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/d3/D3Utils.java
@@ -0,0 +1,39 @@
+package cc.alcina.framework.gwt.client.d3;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Element_Jso;
+
+public class D3Utils {
+	private static int svgIdCounter;
+
+	public static native D3Visualisation createVisualisation(
+			String visualisationName, JavaScriptObject data)/*-{
+        try {
+            var visualisation = $wnd.jdm.graphs[visualisationName](data);
+            return visualisation;
+        } catch (e) {
+            debugger;
+            throw e;
+        }
+
+	}-*/;
+
+	public final static class D3Visualisation extends JavaScriptObject {
+		protected D3Visualisation() {
+		}
+
+		public native void renderGraph(Element_Jso element, int ctrWidth,
+				int ctrHeight)/*-{
+            try {
+                this.render(element, ctrWidth, ctrHeight);
+            } catch (e) {
+                debugger;
+                throw e;
+            }
+		}-*/;
+	}
+
+	public static String nextContainerId() {
+		return "d3-svg-container-" + svgIdCounter++;
+	}
+}
