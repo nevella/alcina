0f6ddc4ce5feda66e88a705010a300ba5cfddcf7
Nick Reddel <nick.reddel@gmail.com>
2017-06-13 20:07:02 -0700

    tilph

--- framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/FlatSearchRow.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/FlatSearchRow.java
@@ -1,5 +1,6 @@
 package cc.alcina.framework.gwt.client.objecttree.search;
 
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
