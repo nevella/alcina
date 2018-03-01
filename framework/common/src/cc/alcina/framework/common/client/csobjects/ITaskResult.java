a3c000fc0053beb17840c6a3aaf7b4ef58443c0a
Nick Reddel <nick.reddel@gmail.com>
2018-03-01 10:46:28 -0800

    Add rpc builder utilities

---
+++ framework/common/src/cc/alcina/framework/common/client/csobjects/ITaskResult.java
@@ -0,0 +1,5 @@
+package cc.alcina.framework.common.client.csobjects;
+
+public interface ITaskResult {
+	public boolean isOk();
+}
---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/util/RpcBuilder.java
@@ -0,0 +1,72 @@
+package cc.alcina.framework.gwt.client.util;
+
+import java.util.function.Consumer;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import cc.alcina.framework.common.client.csobjects.ITaskResult;
+import cc.alcina.framework.gwt.client.logic.MessageManager;
+import cc.alcina.framework.gwt.client.widget.dialog.NonCancellableRemoteDialog;
+
+public class RpcBuilder<T> {
+	private Consumer<T> inner;
+
+	private String caption;
+
+	private Runnable runnable;
+
+	private NonCancellableRemoteDialog crd;
+
+	boolean notifySuccessAsMessage;
+
+	public void call(Runnable runnable) {
+		this.runnable = runnable;
+		callWithModal();
+	}
+
+	public RpcBuilder caption(String caption) {
+		this.caption = caption;
+		return this;
+	}
+
+	public RpcBuilder notifySuccessAsMessage() {
+		notifySuccessAsMessage = true;
+		return this;
+	}
+
+	public RpcBuilder onSuccess(Consumer<T> inner) {
+		this.inner = inner;
+		return this;
+	}
+
+	public AsyncCallback<T> wrappedSuccess() {
+		return new WrappingAsyncCallback<T>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				crd.hide();
+				super.onFailure(caught);
+			}
+
+			@Override
+			protected void onSuccess0(T result) {
+				crd.hide();
+				if (notifySuccessAsMessage && result != null) {
+					if (result instanceof ITaskResult
+							&& !((ITaskResult) result).isOk()) {
+						MessageManager.get()
+								.icyCenterMessage(result.toString());
+					} else {
+						MessageManager.get().showMessage(result.toString());
+					}
+				}
+				inner.accept(result);
+			}
+		};
+	}
+
+	private void callWithModal() {
+		crd = new NonCancellableRemoteDialog(caption, null);
+		crd.show();
+		runnable.run();
+	}
+}
