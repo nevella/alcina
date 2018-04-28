a72cc5108f22878b11034196abba1ce2c9e92dcd
Nick Reddel <nick.reddel@gmail.com>
2018-04-28 06:57:30 -0700

    Add hililocator serializer

---
+++ framework/common/src/cc/alcina/framework/common/client/logic/domaintransform/HiliLocator_CustomFieldSerializer.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package cc.alcina.framework.common.client.logic.domaintransform;
+
+import com.google.gwt.user.client.rpc.CustomFieldSerializer;
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.SerializationStreamReader;
+import com.google.gwt.user.client.rpc.SerializationStreamWriter;
+import com.google.gwt.user.client.rpc.core.java.util.Collection_CustomFieldSerializerBase;
+
+import cc.alcina.framework.common.client.Reflections;
+
+/**
+ * Custom field serializer for HiliLocator
+ */
+@SuppressWarnings("unchecked")
+public final class HiliLocator_CustomFieldSerializer
+		extends CustomFieldSerializer<HiliLocator> {
+	public static void deserialize(SerializationStreamReader streamReader,
+			HiliLocator instance) throws SerializationException {
+		instance.id=streamReader.readLong();
+		instance.localId=streamReader.readLong();
+		instance.clazz=Reflections.classLookup().getClassForName(streamReader.readString());
+	}
+
+	public static void serialize(SerializationStreamWriter streamWriter,
+			HiliLocator instance) throws SerializationException {
+		streamWriter.writeLong(instance.id);
+		streamWriter.writeLong(instance.localId);
+		streamWriter.writeString(instance.clazz.getName());
+	}
+
+	@Override
+	public void deserializeInstance(SerializationStreamReader streamReader,
+			HiliLocator instance) throws SerializationException {
+		deserialize(streamReader, instance);
+	}
+
+	@Override
+	public void serializeInstance(SerializationStreamWriter streamWriter,
+			HiliLocator instance) throws SerializationException {
+		serialize(streamWriter, instance);
+	}
+}
