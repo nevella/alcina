6f49f552f56285de47b1dcf5b731cc11cc65d942
Nick Reddel <nick.reddel@gmail.com>
2018-05-08 10:19:15 -0700

    Add double criterion

---
+++ framework/common/src/cc/alcina/framework/common/client/search/DoubleCriterion.java
@@ -0,0 +1,69 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package cc.alcina.framework.common.client.search;
+
+import cc.alcina.framework.common.client.logic.domain.HasValue;
+
+/**
+ * 
+ * @author Nick Reddel
+ */
+public class DoubleCriterion extends SearchCriterion implements HasValue<Double> {
+	static final transient long serialVersionUID = -1L;
+
+	private Double value;
+
+	public DoubleCriterion() {
+	}
+
+	public DoubleCriterion(Double value) {
+		setValue(value);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public EqlWithParameters eql() {
+		EqlWithParameters result = new EqlWithParameters();
+		if (value == null) {
+			return result;
+		}
+		result.eql = targetPropertyNameWithTable() + " =  ? ";
+		result.parameters.add(value);
+		return result;
+	}
+
+	public Double getDouble() {
+		return getValue();
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setDouble(Double value) {
+		setValue(value);
+	}
+
+	public void setValue(Double value) {
+		Double old_value = this.value;
+		this.value = value;
+		propertyChangeSupport().firePropertyChange("value", old_value, value);
+
+	}
+
+	@Override
+	public String toString() {
+		return value == null ? "" : value.toString();
+	}
+}
---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/packs/BaseDoubleCriterionPack.java
@@ -0,0 +1,79 @@
+package cc.alcina.framework.gwt.client.objecttree.search.packs;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+import com.totsp.gwittir.client.ui.AbstractBoundWidget;
+import com.totsp.gwittir.client.validator.Validator;
+
+import cc.alcina.framework.common.client.cache.CacheFilter;
+import cc.alcina.framework.common.client.collections.CollectionFilter;
+import cc.alcina.framework.common.client.gwittir.validator.DoubleValidator;
+import cc.alcina.framework.common.client.search.DoubleCriterion;
+import cc.alcina.framework.gwt.client.gwittir.widget.TextBox;
+import cc.alcina.framework.gwt.client.objecttree.search.FlatSearchable;
+import cc.alcina.framework.gwt.client.objecttree.search.StandardSearchOperator;
+
+public class BaseDoubleCriterionPack {
+	public interface BaseDoubleCriterionHandler<T> extends Function<T, Double> {
+		@Override
+		public Double apply(T t);
+
+		default CacheFilter getFilter0(DoubleCriterion sc) {
+			Double value = sc.getDouble();
+			if (value == null) {
+				return null;
+			}
+			double p_value = value;
+			return new CacheFilter(new CollectionFilter<T>() {
+				@Override
+				public boolean allow(T o) {
+					Double d = apply(o);
+					if (d == null) {
+						return false;
+					}
+					double p_d = d;
+					switch (sc.getOperator()) {
+					case GREATER_THAN:
+						return p_d > p_value;
+					case LESS_THAN:
+						return p_d < p_value;
+					case EQUALS:
+						return p_d == p_value;
+					default:
+						return false;
+					}
+				}
+			});
+		}
+	}
+
+	public static abstract class BaseDoubleCriterionSearchable<T extends DoubleCriterion>
+			extends FlatSearchable<T> {
+		public BaseDoubleCriterionSearchable(Class<T> clazz,String objectName,String fieldName) {
+			super(clazz, objectName, fieldName,
+					Arrays.asList(StandardSearchOperator.EQUALS,
+							StandardSearchOperator.GREATER_THAN,
+							StandardSearchOperator.LESS_THAN));
+		}
+
+		@Override
+		public AbstractBoundWidget createEditor() {
+			return new TextBox();
+		}
+
+		@Override
+		public String getCriterionPropertyName() {
+			return "value";
+		}
+
+		@Override
+		public boolean hasValue(DoubleCriterion sc) {
+			return sc.getDouble() != null;
+		}
+		@Override
+		public Validator getValidator() {
+			return new DoubleValidator();
+		}
+	}
+}
