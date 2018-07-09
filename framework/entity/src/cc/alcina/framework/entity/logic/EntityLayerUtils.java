5e43beaf94e666d9637effa5de0840d8039056c9
Nick Reddel <nick.reddel@gmail.com>
2018-07-09 12:05:00 -0700

    Add alt logging method

--- framework/entity/src/cc/alcina/framework/entity/logic/EntityLayerUtils.java
+++ framework/entity/src/cc/alcina/framework/entity/logic/EntityLayerUtils.java
@@ -8,35 +8,40 @@ import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
 import cc.alcina.framework.entity.entityaccess.CommonPersistenceProvider;
 
 public class EntityLayerUtils {
-    public static String getLocalHostName() {
-        try {
-            return java.net.InetAddress.getLocalHost().getHostName();
-        } catch (Exception e) {
-            throw new WrappedRuntimeException(e);
-        }
-    }
+	public static String getLocalHostName() {
+		try {
+			return java.net.InetAddress.getLocalHost().getHostName();
+		} catch (Exception e) {
+			throw new WrappedRuntimeException(e);
+		}
+	}
 
-    public static void log(LogMessageType componentKey, String message) {
-        EntityLayerObjects.get().getPersistentLogger()
-                .info(componentKey + " - " + message);
-    }
+	public static void log(LogMessageType componentKey, String message) {
+		EntityLayerObjects.get().getPersistentLogger()
+				.info(componentKey + " - " + message);
+	}
 
-    public static void log(LogMessageType componentKey, String message,
-            Throwable throwable) {
-        Logger persistentLogger = EntityLayerObjects.get()
-                .getPersistentLogger();
-        if (persistentLogger == null) {
-            throwable.printStackTrace();
-            return;
-        }
-        persistentLogger.warn(
-                componentKey + " - " + message + "\n" + throwable.toString(),
-                throwable);
-    }
+	public static void log(LogMessageType componentKey, String message,
+			Throwable throwable) {
+		Logger persistentLogger = EntityLayerObjects.get()
+				.getPersistentLogger();
+		if (persistentLogger == null) {
+			throwable.printStackTrace();
+			return;
+		}
+		persistentLogger.warn(
+				componentKey + " - " + message + "\n" + throwable.toString(),
+				throwable);
+	}
 
-    // convenience
-    public static void persistentLog(String message, Object componentKey) {
-        Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
-                .log(message, componentKey.toString());
-    }
+	public static void persistentLog(Enum componentKey, String message) {
+		Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
+				.log(message, componentKey.toString());
+	}
+
+	// convenience
+	public static void persistentLog(String message, Object componentKey) {
+		Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
+				.log(message, componentKey.toString());
+	}
 }
