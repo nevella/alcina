c8ae4d4b615c89c088ecbc38eee26208fe7ce73c
Nick Reddel <nick.reddel@gmail.com>
2018-10-30 10:24:07 -0700

    Localdom tweak

--- extras/jsdom/src/com/google/gwt/dom/client/ElementRemote.java
+++ extras/jsdom/src/com/google/gwt/dom/client/ElementRemote.java
@@ -907,7 +907,6 @@ public class ElementRemote extends NodeRemote implements DomElement {
       buf : '',
       div : $doc.createElement('div')
     }
-    debugger;
     addNodeToBuiltHtml(this, buffer, 0);
     return buffer.buf;
 	}-*/;
