9b5ef9427c3c467ff37e30fc8d938de7313dee09
Nick Reddel <nick.reddel@gmail.com>
2018-11-12 13:03:33 -0800

    Make serializable

--- framework/entity/src/cc/alcina/framework/entity/util/CachingConcurrentMap.java
+++ framework/entity/src/cc/alcina/framework/entity/util/CachingConcurrentMap.java
@@ -6,6 +6,9 @@ import cc.alcina.framework.common.client.util.CachingMap;
 import cc.alcina.framework.common.client.util.ThrowingFunction;
 
 public class CachingConcurrentMap<I, O> extends CachingMap<I, O> {
+	public CachingConcurrentMap() {
+	}
+
 	public CachingConcurrentMap(ThrowingFunction<I, O> converter, int size) {
 		super(converter, new ConcurrentHashMap<I, O>(size));
 	}
