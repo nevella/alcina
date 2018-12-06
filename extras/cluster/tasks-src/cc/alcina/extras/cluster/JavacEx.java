08071f4d2d87198b08088f36b0d51cd0ad000c58
Nick Reddel <nick.reddel@gmail.com>
2018-12-05 20:52:17 -0800

    Add /g to build overrides

--- extras/cluster/tasks-src/cc/alcina/extras/cluster/JavacEx.java
+++ extras/cluster/tasks-src/cc/alcina/extras/cluster/JavacEx.java
@@ -13,42 +13,45 @@ import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.resources.FileResource;
 
 public class JavacEx extends Javac {
-	private List<FileSet> excludes = new ArrayList();
+    private List<FileSet> excludes = new ArrayList();
 
-	private Map<String, File> nameLookup;
+    private Map<String, File> nameLookup;
 
-	public void addFileset(FileSet fileset) {
-		excludes.add(fileset);
-	}
+    public void addFileset(FileSet fileset) {
+        excludes.add(fileset);
+    }
 
-	private void maybeRemove(String path) {
-		boolean debug = Boolean.getBoolean("JavacEx.debug");
-		if (nameLookup.containsKey(path)) {
-			if (debug) {
-				System.out.println("Removed duplicate: " + path);
-			}
-			nameLookup.remove(path);
-		} else {
-			if (debug) {
-				System.out.println("Not removed: " + path);
-			}
-		}
-	}
+    private void maybeRemove(String path) {
+        boolean debug = Boolean.getBoolean("JavacEx.debug");
+        if (nameLookup.containsKey(path)) {
+            if (debug) {
+                System.out.println("Removed duplicate: " + path);
+            }
+            nameLookup.remove(path);
+        } else {
+            if (debug) {
+                System.out.println("Not removed: " + path);
+            }
+        }
+    }
 
-	protected void compile() {
-		nameLookup = Arrays.asList(compileList).stream()
-				.collect(Collectors.toMap(f -> f.getPath(), f -> f));
-		for (FileSet files : excludes) {
-			Iterator itr = files.iterator();
-			for (; itr.hasNext();) {
-				FileResource resource = (FileResource) itr.next();
-				String path = resource.getFile().getPath();
-				maybeRemove(path);
-				maybeRemove(path.replace("/var/local", "/private/var/local"));
-			}
-		}
-		ArrayList<File> list2 = new ArrayList<File>(nameLookup.values());
-		compileList = (File[]) list2.toArray(new File[list2.size()]);
-		super.compile();
-	}
+    @Override
+    protected void compile() {
+        nameLookup = Arrays.asList(compileList).stream()
+                .collect(Collectors.toMap(f -> f.getPath(), f -> f));
+        for (FileSet files : excludes) {
+            Iterator itr = files.iterator();
+            for (; itr.hasNext();) {
+                FileResource resource = (FileResource) itr.next();
+                String path = resource.getFile().getPath();
+                maybeRemove(path);
+                maybeRemove(path.replace("/var/local", "/private/var/local"));
+                maybeRemove(path.replace("/var/local", "/private/var/local")
+                        .replace("/private/var/local/git/", "/g"));
+            }
+        }
+        ArrayList<File> list2 = new ArrayList<File>(nameLookup.values());
+        compileList = (File[]) list2.toArray(new File[list2.size()]);
+        super.compile();
+    }
 }
