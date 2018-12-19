e1b71232824fb09efecba0bd4ae75383547a18cc
Nick Reddel <nick.reddel@gmail.com>
2018-12-19 11:09:35 -0800

    Work on grouping/reporting

--- framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/StandardSearchOperator.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/StandardSearchOperator.java
@@ -9,29 +9,32 @@ import cc.alcina.framework.common.client.util.CommonUtils;
 
 @ClientInstantiable
 public enum StandardSearchOperator implements SearchOperator {
-	CONTAINS, DOES_NOT_CONTAIN, EQUALS, LESS_THAN, GREATER_THAN, ALL_OF,
-	AT_LEAST_ONE_OF, DOES_NOT_EQUAL;
-	public static transient List LINEAR = Arrays.asList(EQUALS, LESS_THAN,
-			GREATER_THAN);
+    CONTAINS, DOES_NOT_CONTAIN, EQUALS, LESS_THAN, GREATER_THAN, ALL_OF,
+    AT_LEAST_ONE_OF, DOES_NOT_EQUAL;
+    public static transient List LINEAR = Arrays.asList(EQUALS, LESS_THAN,
+            GREATER_THAN);
 
-	public static transient List EQUAL_OR_NOT = Arrays.asList(EQUALS,
-			DOES_NOT_EQUAL);
+    public static transient List EQUAL_OR_NOT = Arrays.asList(EQUALS,
+            DOES_NOT_EQUAL);
 
-	public static transient List CONTAINS_AND_ALL_OF = Arrays.asList(CONTAINS,
-			ALL_OF);
+    public static transient List CONTAINS_AND_ALL_OF = Arrays.asList(CONTAINS,
+            ALL_OF);
 
-	private String displayName;
+    public static transient List MEMBERSHIP = Arrays.asList(CONTAINS, ALL_OF,
+            DOES_NOT_CONTAIN);
 
-	private StandardSearchOperator() {
-	}
+    private String displayName;
 
-	private StandardSearchOperator(String displayName) {
-		this.displayName = displayName;
-	}
+    private StandardSearchOperator() {
+    }
 
-	@Override
-	public String getName() {
-		return Optional.ofNullable(displayName)
-				.orElse(CommonUtils.friendlyConstant(this));
-	}
+    private StandardSearchOperator(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String getName() {
+        return Optional.ofNullable(displayName)
+                .orElse(CommonUtils.friendlyConstant(this));
+    }
 }
