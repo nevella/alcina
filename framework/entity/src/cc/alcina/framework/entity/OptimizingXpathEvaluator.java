9107561b6c2bad94746905c428ac68ca0c57d8ff
Nick Reddel <nick.reddel@gmail.com>
2019-01-23 14:29:07 -0800

    Fix regression in xpath evaluator

--- framework/entity/src/cc/alcina/framework/entity/OptimizingXpathEvaluator.java
+++ framework/entity/src/cc/alcina/framework/entity/OptimizingXpathEvaluator.java
@@ -16,107 +16,107 @@ import cc.alcina.framework.common.client.util.CachingMap;
 import cc.alcina.framework.common.client.util.ThrowingFunction;
 
 public class OptimizingXpathEvaluator {
-	private boolean optimiseXpathEvaluationSpeed = false;
+    private boolean optimiseXpathEvaluationSpeed = false;
 
-	private Node removedNode;
+    private Node removedNode;
 
-	private Element removedParent;
+    private Element removedParent;
 
-	private Node removedNextSib;
+    private Node removedNextSib;
 
-	private Pattern nonOptimise = Pattern
-			.compile("\\.\\.|ancestor|parent|following::|preceding::");
+    private Pattern nonOptimise = Pattern
+            .compile("\\.\\.|ancestor|parent|following::|preceding::");
 
-	private Node node;
+    private Node node;
 
-	private CachingMap<String, XPathExpression> expressionCache;
+    private CachingMap<String, XPathExpression> expressionCache;
 
-	public OptimizingXpathEvaluator(XpathAndExpressionCache xexc) {
-		this.expressionCache = xexc.expressionCache;
-	}
+    public OptimizingXpathEvaluator(XpathAndExpressionCache xexc) {
+        this.expressionCache = xexc.expressionCache;
+    }
 
-	public Element getElementByXpath(String xpathStr, Node node) {
-		return (Element) evaluate(xpathStr, node, XPathConstants.NODE, n -> {
-			return (Element) n;
-		});
-	}
+    public Element getElementByXpath(String xpathStr, Node node) {
+        return (Element) evaluate(xpathStr, node, XPathConstants.NODE, n -> {
+            return (Element) n;
+        });
+    }
 
-	public List<Element> getElementsByXpath(String xpathStr, Node node) {
-		return (List<Element>) evaluate(xpathStr, node, XPathConstants.NODESET,
-				o -> {
-					return XmlUtils.nodeListToElementList((NodeList) o);
-				});
-	}
+    public List<Element> getElementsByXpath(String xpathStr, Node node) {
+        return (List<Element>) evaluate(xpathStr, node, XPathConstants.NODESET,
+                o -> {
+                    return XmlUtils.nodeListToElementList((NodeList) o);
+                });
+    }
 
-	public Node getNodeByXpath(String xpathStr, Node node) {
-		return (Node) evaluate(xpathStr, node, XPathConstants.NODE, n -> {
-			return (Node) n;
-		});
-	}
+    public Node getNodeByXpath(String xpathStr, Node node) {
+        return (Node) evaluate(xpathStr, node, XPathConstants.NODE, n -> {
+            return (Node) n;
+        });
+    }
 
-	public List<Node> getNodesByXpath(String xpathStr, Node node) {
-		return (List<Node>) evaluate(xpathStr, node, XPathConstants.NODESET,
-				o -> {
-					return XmlUtils.nodeListToList((NodeList) o);
-				});
-	}
+    public List<Node> getNodesByXpath(String xpathStr, Node node) {
+        return (List<Node>) evaluate(xpathStr, node, XPathConstants.NODESET,
+                o -> {
+                    return XmlUtils.nodeListToList((NodeList) o);
+                });
+    }
 
-	public String getTextContentOrEmpty(String xpath, Node from) {
-		Node node = getNodeByXpath(xpath, from);
-		return node == null ? "" : node.getTextContent();
-	}
+    public String getTextContentOrEmpty(String xpath, Node from) {
+        Node node = getNodeByXpath(xpath, from);
+        return node == null ? "" : node.getTextContent();
+    }
 
-	public boolean isOptimiseXpathEvaluationSpeed() {
-		return optimiseXpathEvaluationSpeed;
-	}
+    public boolean isOptimiseXpathEvaluationSpeed() {
+        return optimiseXpathEvaluationSpeed;
+    }
 
-	public void setOptimiseXpathEvaluationSpeed(
-			boolean optimiseXpathEvaluationSpeed) {
-		this.optimiseXpathEvaluationSpeed = optimiseXpathEvaluationSpeed;
-	}
+    public void setOptimiseXpathEvaluationSpeed(
+            boolean optimiseXpathEvaluationSpeed) {
+        this.optimiseXpathEvaluationSpeed = optimiseXpathEvaluationSpeed;
+    }
 
-	private <T> T evaluate(String xpathStr, Node node, QName qName,
-			ThrowingFunction<Object, T> mapper) {
-		try {
-			maybeRemove(xpathStr, node);
-			Object result = expressionCache.get(xpathStr).evaluate(this.node,
-					qName);
-			maybeReinsert();
-			return mapper.apply(result);
-		} catch (Exception e) {
-			throw new WrappedRuntimeException(e);
-		}
-	}
+    private <T> T evaluate(String xpathStr, Node node, QName qName,
+            ThrowingFunction<Object, T> mapper) {
+        try {
+            xpathStr = maybeRemove(xpathStr, node);
+            Object result = expressionCache.get(xpathStr).evaluate(this.node,
+                    qName);
+            maybeReinsert();
+            return mapper.apply(result);
+        } catch (Exception e) {
+            throw new WrappedRuntimeException(e);
+        }
+    }
 
-	private void maybeReinsert() {
-		if (removedNode != null) {
-			removedParent.insertBefore(removedNode, removedNextSib);
-			removedNode = null;
-		}
-	}
+    private void maybeReinsert() {
+        if (removedNode != null) {
+            removedParent.insertBefore(removedNode, removedNextSib);
+            removedNode = null;
+        }
+    }
 
-	private String maybeRemove(String xpathStr, Node node) {
-		while (xpathStr.startsWith("../") && node.getParentNode() != null
-				&& node.getParentNode().getNodeType() == Node.ELEMENT_NODE) {
-			xpathStr = xpathStr.substring(3);
-			node = node.getParentNode();
-		}
-		this.node = node;
-		if (isOptimiseXpathEvaluationSpeed()
-				&& !nonOptimise.matcher(xpathStr).find()
-				&& node.getNodeType() == Node.ELEMENT_NODE
-				&& node.getParentNode() != null) {
-			if (xpathStr.contains("sibling")) {
-				node = node.getParentNode();
-			}
-			if (node.getParentNode().getNodeType() != Node.ELEMENT_NODE) {
-				return xpathStr;
-			}
-			removedParent = (Element) node.getParentNode();
-			removedNextSib = node.getNextSibling();
-			removedNode = node;
-			removedParent.removeChild(node);
-		}
-		return xpathStr;
-	}
+    private String maybeRemove(String xpathStr, Node node) {
+        while (xpathStr.startsWith("../") && node.getParentNode() != null
+                && node.getParentNode().getNodeType() == Node.ELEMENT_NODE) {
+            xpathStr = xpathStr.substring(3);
+            node = node.getParentNode();
+        }
+        this.node = node;
+        if (isOptimiseXpathEvaluationSpeed()
+                && !nonOptimise.matcher(xpathStr).find()
+                && node.getNodeType() == Node.ELEMENT_NODE
+                && node.getParentNode() != null) {
+            if (xpathStr.contains("sibling")) {
+                node = node.getParentNode();
+            }
+            if (node.getParentNode().getNodeType() != Node.ELEMENT_NODE) {
+                return xpathStr;
+            }
+            removedParent = (Element) node.getParentNode();
+            removedNextSib = node.getNextSibling();
+            removedNode = node;
+            removedParent.removeChild(node);
+        }
+        return xpathStr;
+    }
 }
