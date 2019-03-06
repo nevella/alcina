b6c1ff105a604e875766d90a94f66559d0f594a7
Nick Reddel <nick.reddel@gmail.com>
2019-03-06 11:08:50 -0800

    Add table util

--- framework/entity/src/cc/alcina/framework/entity/parser/structured/node/XmlNodeHtmlTableBuilder.java
+++ framework/entity/src/cc/alcina/framework/entity/parser/structured/node/XmlNodeHtmlTableBuilder.java
@@ -10,173 +10,188 @@ import cc.alcina.framework.common.client.util.LooseContext;
 import cc.alcina.framework.entity.SEUtilities;
 
 public class XmlNodeHtmlTableBuilder extends XmlNodeBuilder {
-	public static final transient String CONTEXT_NO_TD_STYLES = XmlNodeHtmlTableBuilder.class
-			.getName() + ".CONTEXT_NO_TD_STYLES";
+    public static final transient String CONTEXT_NO_TD_STYLES = XmlNodeHtmlTableBuilder.class
+            .getName() + ".CONTEXT_NO_TD_STYLES";
 
-	public static String toHtmlGrid(List<String> headers, List<Row> values,
-			String title, int maxColWidth) {
-		XmlDoc doc = XmlDoc.basicHtmlDoc();
-		if (!LooseContext.is(CONTEXT_NO_TD_STYLES)) {
-			doc.xpath("//head").node().builder().tag("style")
-					.text("td {white-space: nowrap; \n"
-							+ "    overflow: hidden;\n"
-							+ "max-width:%sem; text-overflow:ellipsis;padding-right:1em;}"
-							+ ".numeric{text-align:right}", maxColWidth)
-					.append();
-		}
-		XmlNode node = doc.xpath("//body").node();
-		XmlNodeHtmlTableBuilder tableBuilder = node.html().tableBuilder();
-		XmlNodeHtmlTableRowBuilder headerBuilder = tableBuilder.row();
-		headerBuilder.className("header");
-		headers.forEach(headerBuilder::cell);
-		values.forEach(row -> {
-			XmlNodeHtmlTableRowBuilder rowBuilder = tableBuilder.row();
-			((List<Cell>) row.cells).stream().forEach(cell -> {
-				String value = Ax.blankToEmpty(cell.value).replace("\\n", "\n");
-				value = SEUtilities.normalizeWhitespaceAndTrim(value);
-				String href = cell.href;
-				if (Ax.notBlank(href)) {
-					XmlNode td = rowBuilder.cell().append();
-					td.html().addLink(value, href, "_blank");
-				} else {
-					rowBuilder.cell(value);
-				}
-			});
-		});
-		return doc.prettyToString();
-	}
+    public static String toHtmlGrid(List<String> headers, List<Row> values,
+            String title, int maxColWidth) {
+        XmlDoc doc = XmlDoc.basicHtmlDoc();
+        if (!LooseContext.is(CONTEXT_NO_TD_STYLES)) {
+            doc.xpath("//head").node().builder().tag("style")
+                    .text("td {white-space: nowrap; \n"
+                            + "    overflow: hidden;\n"
+                            + "max-width:%sem; text-overflow:ellipsis;padding-right:1em;}"
+                            + ".numeric{text-align:right}", maxColWidth)
+                    .append();
+        }
+        XmlNode node = doc.xpath("//body").node();
+        XmlNodeHtmlTableBuilder tableBuilder = node.html().tableBuilder();
+        XmlNodeHtmlTableRowBuilder headerBuilder = tableBuilder.row();
+        headerBuilder.className("header");
+        headers.forEach(headerBuilder::cell);
+        values.forEach(row -> {
+            XmlNodeHtmlTableRowBuilder rowBuilder = tableBuilder.row();
+            ((List<Cell>) row.cells).stream().forEach(cell -> {
+                String value = Ax.blankToEmpty(cell.value).replace("\\n", "\n");
+                value = SEUtilities.normalizeWhitespaceAndTrim(value);
+                String href = cell.href;
+                if (Ax.notBlank(href)) {
+                    XmlNode td = rowBuilder.cell().append();
+                    td.html().addLink(value, href, "_blank");
+                } else {
+                    rowBuilder.cell(value);
+                }
+            });
+        });
+        return doc.prettyToString();
+    }
 
-	private String rowClassName;
+    private String rowClassName;
 
-	public XmlNodeHtmlTableBuilder(XmlNode xmlNode) {
-		relativeTo = xmlNode;
-		tag("table");
-	}
+    public XmlNodeHtmlTableBuilder(XmlNode xmlNode) {
+        relativeTo = xmlNode;
+        tag("table");
+    }
 
-	public XmlNodeHtmlTableRowBuilder row() {
-		XmlNode tableNode = ensureBuilt();
-		return new XmlNodeHtmlTableRowBuilder(tableNode);
-	}
+    public XmlNodeHtmlTableRowBuilder row() {
+        XmlNode tableNode = ensureBuilt();
+        return new XmlNodeHtmlTableRowBuilder(tableNode);
+    }
 
-	public void rowClassName(String rowClassName) {
-		this.rowClassName = rowClassName;
-	}
+    public void rowClassName(String rowClassName) {
+        this.rowClassName = rowClassName;
+    }
 
-	private XmlNode ensureBuilt() {
-		if (built) {
-			return builtNode();
-		} else {
-			return append();
-		}
-	}
+    private XmlNode ensureBuilt() {
+        if (built) {
+            return builtNode();
+        } else {
+            return append();
+        }
+    }
 
-	public class XmlNodeHtmlTableCellBuilder extends XmlNodeBuilder {
-		public XmlNodeHtmlTableCellBuilder(XmlNode rowNode) {
-			relativeTo = rowNode;
-			tag("td");
-		}
+    public class XmlNodeHtmlTableCellBuilder extends XmlNodeBuilder {
+        public XmlNodeHtmlTableCellBuilder(XmlNode rowNode) {
+            relativeTo = rowNode;
+            tag("td");
+        }
 
-		public XmlNodeHtmlTableCellBuilder blank() {
-			text("\u00A0");
-			return this;
-		}
+        public XmlNodeHtmlTableCellBuilder blank() {
+            text("\u00A0");
+            return this;
+        }
 
-		public XmlNodeHtmlTableCellBuilder cell() {
-			ensureBuilt();
-			return new XmlNodeHtmlTableCellBuilder(relativeTo);
-		}
+        public XmlNodeHtmlTableCellBuilder cell() {
+            ensureBuilt();
+            return new XmlNodeHtmlTableCellBuilder(relativeTo);
+        }
 
-		public XmlNodeHtmlTableCellBuilder cell(Object text) {
-			text(CommonUtils.nullSafeToString(text));
-			return cell();
-		}
+        public XmlNodeHtmlTableCellBuilder cell(Object text) {
+            text(CommonUtils.nullSafeToString(text));
+            return cell();
+        }
 
-		@Override
-		public XmlNodeHtmlTableCellBuilder className(String className) {
-			super.className(className);
-			return this;
-		}
+        public XmlNodeHtmlTableCellBuilder cell(String template,
+                Object... args) {
+            text(Ax.format(template, args));
+            return cell();
+        }
 
-		public XmlNodeHtmlTableCellBuilder colSpan(int colSpan) {
-			attr("colSpan", String.valueOf(colSpan));
-			return this;
-		}
+        @Override
+        public XmlNodeHtmlTableCellBuilder className(String className) {
+            super.className(className);
+            return this;
+        }
 
-		public XmlNode ensureBuilt() {
-			if (built) {
-				return builtNode();
-			} else {
-				append();
-				return builtNode();
-			}
-		}
+        public XmlNodeHtmlTableCellBuilder colSpan(int colSpan) {
+            attr("colSpan", String.valueOf(colSpan));
+            return this;
+        }
 
-		public XmlNodeHtmlTableCellBuilder nowrap() {
-			style("white-space: nowrap");
-			return this;
-		}
+        public XmlNode ensureBuilt() {
+            if (built) {
+                return builtNode();
+            } else {
+                append();
+                return builtNode();
+            }
+        }
 
-		public XmlNodeHtmlTableRowBuilder row() {
-			ensureBuilt();
-			return new XmlNodeHtmlTableRowBuilder(relativeTo.parent());
-		}
+        public XmlNodeHtmlTableCellBuilder nowrap() {
+            style("white-space: nowrap");
+            return this;
+        }
 
-		public XmlNodeHtmlTableCellBuilder spacer() {
-			return text("\u00a0").cell();
-		}
+        public XmlNodeHtmlTableCellBuilder numeric() {
+            return className("numeric");
+        }
 
-		@Override
-		public XmlNodeHtmlTableCellBuilder style(String style) {
-			super.style(style);
-			return this;
-		}
+        public XmlNodeHtmlTableRowBuilder row() {
+            ensureBuilt();
+            return new XmlNodeHtmlTableRowBuilder(relativeTo.parent());
+        }
 
-		@Override
-		public XmlNodeHtmlTableCellBuilder text(String text) {
-			super.text(text);
-			return this;
-		}
-	}
+        public XmlNodeHtmlTableCellBuilder spacer() {
+            return text("\u00a0").cell();
+        }
 
-	public class XmlNodeHtmlTableRowBuilder extends XmlNodeBuilder {
-		private XmlNode node;
+        @Override
+        public XmlNodeHtmlTableCellBuilder style(String style) {
+            super.style(style);
+            return this;
+        }
 
-		public XmlNodeHtmlTableRowBuilder(XmlNode tableNode) {
-			relativeTo = tableNode;
-			tag("tr");
-			if (Ax.notBlank(rowClassName)) {
-				attr("class", rowClassName);
-			}
-		}
+        @Override
+        public XmlNodeHtmlTableCellBuilder text(String text) {
+            super.text(text);
+            return this;
+        }
+    }
 
-		public XmlNodeHtmlTableCellBuilder cell() {
-			ensureBuilt();
-			return new XmlNodeHtmlTableCellBuilder(builtNode());
-		}
+    public class XmlNodeHtmlTableRowBuilder extends XmlNodeBuilder {
+        private XmlNode node;
 
-		public XmlNodeHtmlTableCellBuilder cell(String text) {
-			return cell().text(text).cell();
-		}
+        public XmlNodeHtmlTableRowBuilder(XmlNode tableNode) {
+            relativeTo = tableNode;
+            tag("tr");
+            if (Ax.notBlank(rowClassName)) {
+                attr("class", rowClassName);
+            }
+        }
 
-		public void ensureBuilt() {
-			if (built) {
-			} else {
-				node = append();
-			}
-		}
+        public XmlNodeHtmlTableCellBuilder cell() {
+            ensureBuilt();
+            return new XmlNodeHtmlTableCellBuilder(builtNode());
+        }
 
-		public XmlNode getNode() {
-			return this.node;
-		}
+        public XmlNodeHtmlTableCellBuilder cell(String text) {
+            return cell().text(text).cell();
+        }
 
-		public XmlNodeHtmlTableCellBuilder spacer() {
-			return cell().spacer();
-		}
+        public XmlNodeHtmlTableCellBuilder cell(String template,
+                Object... args) {
+            return cell().cell(Ax.format(template, args));
+        }
 
-		@Override
-		public XmlNodeHtmlTableRowBuilder style(String style) {
-			super.style(style);
-			return this;
-		}
-	}
+        public void ensureBuilt() {
+            if (built) {
+            } else {
+                node = append();
+            }
+        }
+
+        public XmlNode getNode() {
+            return this.node;
+        }
+
+        public XmlNodeHtmlTableCellBuilder spacer() {
+            return cell().spacer();
+        }
+
+        @Override
+        public XmlNodeHtmlTableRowBuilder style(String style) {
+            super.style(style);
+            return this;
+        }
+    }
 }
