e5d75fe1cebef9495099e6e61aa08998331dbc4e
Nick Reddel <nick.reddel@gmail.com>
2019-03-06 16:23:19 -0800

    Improve connection issue handling

--- framework/entity/src/cc/alcina/framework/entity/entityaccess/updaters/DbUpdater.java
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/updaters/DbUpdater.java
@@ -5,16 +5,23 @@ import java.sql.Statement;
 
 import javax.persistence.EntityManager;
 
-import cc.alcina.framework.common.client.WrappedRuntimeException;
 import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
 import cc.alcina.framework.entity.MetricLogging;
 import cc.alcina.framework.entity.entityaccess.CommonPersistenceBase.CommonPersistenceConnectionProvider;
 
 public abstract class DbUpdater implements Comparable<DbUpdater> {
+    public boolean allowNullEntityManager() {
+        return false;
+    }
+
     @Override
     public int compareTo(DbUpdater o) {
         return getUpdateNumber().compareTo(o.getUpdateNumber());
     }
+
+    public abstract Integer getUpdateNumber();
+
+    public abstract void run(EntityManager em) throws Exception;
 
     public boolean runAsync() {
         return false;
@@ -22,46 +29,6 @@ public abstract class DbUpdater implements Comparable<DbUpdater> {
 
     public boolean runPreCache() {
         return false;
-    }
-
-    public abstract Integer getUpdateNumber();
-
-    public abstract void run(EntityManager em) throws Exception;
-
-    protected void exSql(String sql) throws Exception {
-        exSql(sql, false);
-    }
-
-    protected void exSql(String sql, boolean squelchErrors) throws Exception {
-        System.out.println("SQL: " + sql);
-        int j;
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            conn = Registry.impl(CommonPersistenceConnectionProvider.class)
-                    .getConnection();
-            stmt = conn.createStatement();
-            j = stmt.executeUpdate(sql);
-            System.out.println(j + "  results");
-        } catch (Exception e) {
-            if (e.getMessage().contains("already exists")) {
-                System.out.println("---ignore - already exists");
-            } else {
-                if (!squelchErrors) {
-                    throw e;
-                }
-                System.out.println(e.getMessage());
-            }
-        } finally {
-            try {
-                if (stmt != null) {
-                    stmt.close();
-                }
-                conn.close();
-            } catch (Exception e) {
-                throw new WrappedRuntimeException(e);
-            }
-        }
     }
 
     protected void ex(Statement stmt, String sql) throws Exception {
@@ -88,7 +55,29 @@ public abstract class DbUpdater implements Comparable<DbUpdater> {
         MetricLogging.get().end("query");
     }
 
-    public boolean allowNullEntityManager() {
-        return false;
+    protected void exSql(String sql) throws Exception {
+        exSql(sql, false);
+    }
+
+    protected void exSql(String sql, boolean squelchErrors) throws Exception {
+        System.out.println("SQL: " + sql);
+        int j;
+        Statement stmt;
+        try (Connection conn = Registry
+                .impl(CommonPersistenceConnectionProvider.class)
+                .getConnection()) {
+            stmt = conn.createStatement();
+            j = stmt.executeUpdate(sql);
+            System.out.println(j + "  results");
+        } catch (Exception e) {
+            if (e.getMessage().contains("already exists")) {
+                System.out.println("---ignore - already exists");
+            } else {
+                if (!squelchErrors) {
+                    throw e;
+                }
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
