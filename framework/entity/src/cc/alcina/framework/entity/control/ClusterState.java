3b0929576d539ffd5a853a337736bd1e3227b8f7
Nick Reddel <nick.reddel@gmail.com>
2019-03-25 19:00:42 -0700

    Work on cluster state

--- framework/entity/src/cc/alcina/framework/entity/control/ClusterState.java
+++ framework/entity/src/cc/alcina/framework/entity/control/ClusterState.java
@@ -3,113 +3,123 @@ package cc.alcina.framework.entity.control;
 import java.util.List;
 
 public class ClusterState {
-	private String clusterId;
+    private String clusterId;
 
-	private String currentWriterHost;
+    private String currentWriterHost;
 
-	private List<String> allHosts;
+    private List<String> allHosts;
 
-	private String preferredWriterHost;
+    private String preferredWriterHost;
 
-	private String httpProxyBalancerUrl;
+    private String httpProxyBalancerUrl;
 
-	private String httpsProxyBalancerUrl;
+    private String httpsProxyBalancerUrl;
 
-	private String httpsProxyBalancerUrl2;
+    private String httpsProxyBalancerUrl2;
 
-	private String proxyToHttpPort;
+    private String proxyToHttpPort;
 
-	private String proxyToHttpsPort;
+    private String proxyToHttpsPort;
 
-	private String testUrl;
+    private String testUrl;
 
-	private String zkHostPortUrl;
+    private String zkHostPortUrl;
 
-	public List<String> getAllHosts() {
-		return this.allHosts;
-	}
+    private String hostTunnels;
 
-	public String getClusterId() {
-		return this.clusterId;
-	}
+    public List<String> getAllHosts() {
+        return this.allHosts;
+    }
 
-	public String getCurrentWriterHost() {
-		return this.currentWriterHost;
-	}
+    public String getClusterId() {
+        return this.clusterId;
+    }
 
-	public String getHttpProxyBalancerUrl() {
-		return this.httpProxyBalancerUrl;
-	}
+    public String getCurrentWriterHost() {
+        return this.currentWriterHost;
+    }
 
-	public String getHttpsProxyBalancerUrl() {
-		return this.httpsProxyBalancerUrl;
-	}
+    public String getHostTunnels() {
+        return this.hostTunnels;
+    }
 
-	public String getHttpsProxyBalancerUrl2() {
-		return this.httpsProxyBalancerUrl2;
-	}
+    public String getHttpProxyBalancerUrl() {
+        return this.httpProxyBalancerUrl;
+    }
 
-	public String getPreferredWriterHost() {
-		return this.preferredWriterHost;
-	}
+    public String getHttpsProxyBalancerUrl() {
+        return this.httpsProxyBalancerUrl;
+    }
 
-	public String getProxyToHttpPort() {
-		return this.proxyToHttpPort;
-	}
+    public String getHttpsProxyBalancerUrl2() {
+        return this.httpsProxyBalancerUrl2;
+    }
 
-	public String getProxyToHttpsPort() {
-		return this.proxyToHttpsPort;
-	}
+    public String getPreferredWriterHost() {
+        return this.preferredWriterHost;
+    }
 
-	public String getTestUrl() {
-		return this.testUrl;
-	}
+    public String getProxyToHttpPort() {
+        return this.proxyToHttpPort;
+    }
 
-	public String getZkHostPortUrl() {
-		return this.zkHostPortUrl;
-	}
+    public String getProxyToHttpsPort() {
+        return this.proxyToHttpsPort;
+    }
 
-	public void setAllHosts(List<String> allHosts) {
-		this.allHosts = allHosts;
-	}
+    public String getTestUrl() {
+        return this.testUrl;
+    }
 
-	public void setClusterId(String clusterId) {
-		this.clusterId = clusterId;
-	}
+    public String getZkHostPortUrl() {
+        return this.zkHostPortUrl;
+    }
 
-	public void setCurrentWriterHost(String currentWriterHost) {
-		this.currentWriterHost = currentWriterHost;
-	}
+    public void setAllHosts(List<String> allHosts) {
+        this.allHosts = allHosts;
+    }
 
-	public void setHttpProxyBalancerUrl(String httpProxyBalancerUrl) {
-		this.httpProxyBalancerUrl = httpProxyBalancerUrl;
-	}
+    public void setClusterId(String clusterId) {
+        this.clusterId = clusterId;
+    }
 
-	public void setHttpsProxyBalancerUrl(String httpsProxyBalancerUrl) {
-		this.httpsProxyBalancerUrl = httpsProxyBalancerUrl;
-	}
+    public void setCurrentWriterHost(String currentWriterHost) {
+        this.currentWriterHost = currentWriterHost;
+    }
 
-	public void setHttpsProxyBalancerUrl2(String httpsProxyBalancerUrl2) {
-		this.httpsProxyBalancerUrl2 = httpsProxyBalancerUrl2;
-	}
+    public void setHostTunnels(String hostTunnels) {
+        this.hostTunnels = hostTunnels;
+    }
 
-	public void setPreferredWriterHost(String preferredWriterHost) {
-		this.preferredWriterHost = preferredWriterHost;
-	}
+    public void setHttpProxyBalancerUrl(String httpProxyBalancerUrl) {
+        this.httpProxyBalancerUrl = httpProxyBalancerUrl;
+    }
 
-	public void setProxyToHttpPort(String proxyToHttpPort) {
-		this.proxyToHttpPort = proxyToHttpPort;
-	}
+    public void setHttpsProxyBalancerUrl(String httpsProxyBalancerUrl) {
+        this.httpsProxyBalancerUrl = httpsProxyBalancerUrl;
+    }
 
-	public void setProxyToHttpsPort(String proxyToHttpsPort) {
-		this.proxyToHttpsPort = proxyToHttpsPort;
-	}
+    public void setHttpsProxyBalancerUrl2(String httpsProxyBalancerUrl2) {
+        this.httpsProxyBalancerUrl2 = httpsProxyBalancerUrl2;
+    }
 
-	public void setTestUrl(String testUrl) {
-		this.testUrl = testUrl;
-	}
+    public void setPreferredWriterHost(String preferredWriterHost) {
+        this.preferredWriterHost = preferredWriterHost;
+    }
 
-	public void setZkHostPortUrl(String zkHostPortUrl) {
-		this.zkHostPortUrl = zkHostPortUrl;
-	}
+    public void setProxyToHttpPort(String proxyToHttpPort) {
+        this.proxyToHttpPort = proxyToHttpPort;
+    }
+
+    public void setProxyToHttpsPort(String proxyToHttpsPort) {
+        this.proxyToHttpsPort = proxyToHttpsPort;
+    }
+
+    public void setTestUrl(String testUrl) {
+        this.testUrl = testUrl;
+    }
+
+    public void setZkHostPortUrl(String zkHostPortUrl) {
+        this.zkHostPortUrl = zkHostPortUrl;
+    }
 }
--- framework/entity/src/cc/alcina/framework/entity/control/ClusterStateProviderFile.java
+++ framework/entity/src/cc/alcina/framework/entity/control/ClusterStateProviderFile.java
@@ -8,56 +8,57 @@ import cc.alcina.framework.common.client.util.StringMap;
 import cc.alcina.framework.entity.ResourceUtilities;
 
 public class ClusterStateProviderFile implements ClusterStateProvider {
-	public static final String SYS_PROP_CONFIG_FILE_PATHS = ClusterStateProviderFile.class
-			.getName() + ".SYS_PROP_CONFIG_FILE_PATHS";
+    public static final String SYS_PROP_CONFIG_FILE_PATHS = ClusterStateProviderFile.class
+            .getName() + ".SYS_PROP_CONFIG_FILE_PATHS";
 
-	private String configFilePaths;
+    private String configFilePaths;
 
-	public ClusterStateProviderFile() {
-		configFilePaths = System.getProperty(SYS_PROP_CONFIG_FILE_PATHS);
-	}
+    public ClusterStateProviderFile() {
+        configFilePaths = System.getProperty(SYS_PROP_CONFIG_FILE_PATHS);
+    }
 
-	@Override
-	public ClusterState getClusterState(String forClusterId) throws Exception {
-		for (String path : configFilePaths.split(",")) {
-			StringMap props = StringMap.fromPropertyString(
-					ResourceUtilities.readFileToString(path), true);
-			String clusterId = props.get("clusterId");
-			if (clusterId.equals(forClusterId)) {
-				ClusterState state = new ClusterState();
-				state.setClusterId(clusterId);
-				state.setCurrentWriterHost(props.get("currentWriterHost"));
-				state.setPreferredWriterHost(props.get("preferredWriterHost"));
-				String hostString = props.get("allHosts");
-				List<String> hosts = new ArrayList<String>(
-						Arrays.asList(hostString.split(",\\s*")));
-				state.setAllHosts(hosts);
-				state.setHttpProxyBalancerUrl(
-						props.get("httpProxyBalancerUrl"));
-				state.setHttpsProxyBalancerUrl(
-						props.get("httpsProxyBalancerUrl"));
-				state.setHttpsProxyBalancerUrl2(
-						props.get("httpsProxyBalancerUrl2"));
-				state.setProxyToHttpPort(props.get("proxyToHttpPort"));
-				state.setProxyToHttpsPort(props.get("proxyToHttpsPort"));
-				state.setTestUrl(props.get("testUrl"));
-				state.setZkHostPortUrl(props.get("zkHostPortUrl"));
-				return state;
-			}
-		}
-		throw new Exception(
-				String.format("Cluster %s not found", forClusterId));
-	}
+    @Override
+    public ClusterState getClusterState(String forClusterId) throws Exception {
+        for (String path : configFilePaths.split(",")) {
+            StringMap props = StringMap.fromPropertyString(
+                    ResourceUtilities.readFileToString(path), true);
+            String clusterId = props.get("clusterId");
+            if (clusterId.equals(forClusterId)) {
+                ClusterState state = new ClusterState();
+                state.setClusterId(clusterId);
+                state.setCurrentWriterHost(props.get("currentWriterHost"));
+                state.setPreferredWriterHost(props.get("preferredWriterHost"));
+                String hostString = props.get("allHosts");
+                List<String> hosts = new ArrayList<String>(
+                        Arrays.asList(hostString.split(",\\s*")));
+                state.setAllHosts(hosts);
+                state.setHttpProxyBalancerUrl(
+                        props.get("httpProxyBalancerUrl"));
+                state.setHttpsProxyBalancerUrl(
+                        props.get("httpsProxyBalancerUrl"));
+                state.setHttpsProxyBalancerUrl2(
+                        props.get("httpsProxyBalancerUrl2"));
+                state.setProxyToHttpPort(props.get("proxyToHttpPort"));
+                state.setProxyToHttpsPort(props.get("proxyToHttpsPort"));
+                state.setTestUrl(props.get("testUrl"));
+                state.setZkHostPortUrl(props.get("zkHostPortUrl"));
+                state.setHostTunnels(props.get("hostTunnels"));
+                return state;
+            }
+        }
+        throw new Exception(
+                String.format("Cluster %s not found", forClusterId));
+    }
 
-	@Override
-	public void persistClusterState(ClusterState state) {
-		StringMap props = new StringMap();
-		String clusterId = state.getClusterId();
-		props.put(clusterId + "_clusterId", clusterId);
-		props.put(clusterId + "_currentWriterHost",
-				state.getCurrentWriterHost());
-		props.put(clusterId + "_preferredWriterHost",
-				state.getPreferredWriterHost());
-		props.put(clusterId + "_clusterId", clusterId);
-	}
+    @Override
+    public void persistClusterState(ClusterState state) {
+        StringMap props = new StringMap();
+        String clusterId = state.getClusterId();
+        props.put(clusterId + "_clusterId", clusterId);
+        props.put(clusterId + "_currentWriterHost",
+                state.getCurrentWriterHost());
+        props.put(clusterId + "_preferredWriterHost",
+                state.getPreferredWriterHost());
+        props.put(clusterId + "_clusterId", clusterId);
+    }
 }
