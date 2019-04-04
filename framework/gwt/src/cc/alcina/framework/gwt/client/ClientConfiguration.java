4e5ad002abc562709b66f15c37eafcfad1b98816
Nick Reddel <nick.reddel@gmail.com>
2019-04-04 08:52:36 -0700

    Autoformat

--- framework/gwt/src/cc/alcina/framework/gwt/client/ClientConfiguration.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/ClientConfiguration.java
@@ -24,84 +24,83 @@ import cc.alcina.framework.gwt.client.res.AlcinaResources;
 import cc.alcina.framework.gwt.client.util.TimerWrapperGwt.TimerWrapperProviderGwt;
 
 public class ClientConfiguration {
-	public void initServices() {
-		initNotifications();
-		initCss();
-		Registry.impl(ClientNotifications.class).metricLogStart("moduleLoad");
-		initExceptionHandling();
-		initCommonClient();
-		initContentProvider();
-		prepareDebugFromHistory();
-		extraConfiguration();
-	}
+    public void initServices() {
+        initNotifications();
+        initCss();
+        Registry.impl(ClientNotifications.class).metricLogStart("moduleLoad");
+        initExceptionHandling();
+        initCommonClient();
+        initContentProvider();
+        prepareDebugFromHistory();
+        extraConfiguration();
+    }
 
-	protected void afterConfiguration() {
-	}
+    protected void afterConfiguration() {
+    }
 
-	protected CommitToStorageTransformListener
-			createStorageTransformListener() {
-		return new CommitToStorageTransformListener();
-	}
+    protected CommitToStorageTransformListener createStorageTransformListener() {
+        return new CommitToStorageTransformListener();
+    }
 
-	protected ClientTransformManager createTransformManager() {
-		return new ClientTransformManagerCommon();
-	}
+    protected ClientTransformManager createTransformManager() {
+        return new ClientTransformManagerCommon();
+    }
 
-	protected void extraConfiguration() {
-	}
+    protected void extraConfiguration() {
+    }
 
-	protected void initCommonClient() {
-		TransformManager.register(createTransformManager());
-		Domain.registerHandler(new DomainHandlerClient());
-		LooseContext.register(new ClientLooseContextProvider());
-		TransformManager.get().setupClientListeners();
-		TransformManager.get()
-				.addDomainTransformListener(PermissionsManager.get());
-		Registry.registerSingleton(CommitToStorageTransformListener.class,
-				createStorageTransformListener());
-		Registry.registerSingleton(TimerWrapperProvider.class,
-				new TimerWrapperProviderGwt());
-		registerExtraTransformListenersPreStorage();
-		TransformManager.get().addDomainTransformListener(
-				Registry.impl(CommitToStorageTransformListener.class));
-		registerExtraTransformListenersPostStorage();
-		Reflections.registerPropertyAccessor(GwittirBridge.get());
-		Reflections.registerBeanDescriptorProvider(GwittirBridge.get());
-		Reflections.registerClassLookup(ClientReflector.get());
-		Reflections.registerObjectLookup(TransformManager.get());
-	}
+    protected void initCommonClient() {
+        TransformManager.register(createTransformManager());
+        Domain.registerHandler(new DomainHandlerClient());
+        LooseContext.register(new ClientLooseContextProvider());
+        TransformManager.get().setupClientListeners();
+        TransformManager.get()
+                .addDomainTransformListener(PermissionsManager.get());
+        Registry.registerSingleton(CommitToStorageTransformListener.class,
+                createStorageTransformListener());
+        Registry.registerSingleton(TimerWrapperProvider.class,
+                new TimerWrapperProviderGwt());
+        registerExtraTransformListenersPreStorage();
+        TransformManager.get().addDomainTransformListener(
+                Registry.impl(CommitToStorageTransformListener.class));
+        registerExtraTransformListenersPostStorage();
+        Reflections.registerPropertyAccessor(GwittirBridge.get());
+        Reflections.registerBeanDescriptorProvider(GwittirBridge.get());
+        Reflections.registerClassLookup(ClientReflector.get());
+        Reflections.registerObjectLookup(TransformManager.get());
+    }
 
-	protected void initContentProvider() {
-	}
+    protected void initContentProvider() {
+    }
 
-	protected void initCss() {
-		StyleInjector.inject(AlcinaResources.INSTANCE.css().getText());
-	}
+    protected void initCss() {
+        StyleInjector.inject(AlcinaResources.INSTANCE.css().getText());
+    }
 
-	protected void initExceptionHandling() {
-		Registry.registerSingleton(ClientExceptionHandler.class,
-				new ClientExceptionHandler());
-	}
+    protected void initExceptionHandling() {
+        Registry.registerSingleton(ClientExceptionHandler.class,
+                new ClientExceptionHandler());
+    }
 
-	protected void initImageProvider() {
-	}
+    protected void initImageProvider() {
+    }
 
-	protected void initNotifications() {
-		Registry.registerSingleton(ClientNotifications.class,
-				new ClientNotificationsImpl());
-	}
+    protected void initNotifications() {
+        Registry.registerSingleton(ClientNotifications.class,
+                new ClientNotificationsImpl());
+    }
 
-	protected void prepareDebugFromHistory() {
-		AlcinaHistory.initialiseDebugIds();
-		if (AlcinaProperties.is(AlcinaProperties.class,
-				AlcinaProperties.SIMULATE_OFFLINE)) {
-			AlcinaDebugIds.setFlag(AlcinaDebugIds.DEBUG_SIMULATE_OFFLINE);
-		}
-	}
+    protected void prepareDebugFromHistory() {
+        AlcinaHistory.initialiseDebugIds();
+        if (AlcinaProperties.is(AlcinaProperties.class,
+                AlcinaProperties.SIMULATE_OFFLINE)) {
+            AlcinaDebugIds.setFlag(AlcinaDebugIds.DEBUG_SIMULATE_OFFLINE);
+        }
+    }
 
-	protected void registerExtraTransformListenersPostStorage() {
-	}
+    protected void registerExtraTransformListenersPostStorage() {
+    }
 
-	protected void registerExtraTransformListenersPreStorage() {
-	}
+    protected void registerExtraTransformListenersPreStorage() {
+    }
 }
