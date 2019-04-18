b84e2aea6867c97f25b97a3a52eed5aef64fb0e6
Nick Reddel <nick.reddel@gmail.com>
2019-04-17 19:34:12 -0700

    No change

--- framework/entity/src/cc/alcina/framework/entity/util/FsStringCache.java
+++ framework/entity/src/cc/alcina/framework/entity/util/FsStringCache.java
@@ -9,44 +9,44 @@ import cc.alcina.framework.common.client.util.ThrowingFunction;
 import cc.alcina.framework.entity.ResourceUtilities;
 
 public class FsStringCache {
-	private File root;
+    private File root;
 
-	private ThrowingFunction<String, String> contentMapper;
+    private ThrowingFunction<String, String> contentMapper;
 
-	public FsStringCache(File root,
-			ThrowingFunction<String, String> contentMapper) {
-		this.root = root;
-		this.contentMapper = contentMapper;
-	}
+    public FsStringCache(File root,
+            ThrowingFunction<String, String> contentMapper) {
+        this.root = root;
+        this.contentMapper = contentMapper;
+    }
 
-	public String get(String path) {
-		return get(path, path);
-	}
+    public String get(String path) {
+        return get(path, path);
+    }
 
-	public String get(String path, String content) {
-		File cacheFile = getCacheFile(path);
-		if (!cacheFile.exists()) {
-			try {
-				String value = contentMapper.apply(content);
-				ResourceUtilities.write(value, cacheFile);
-			} catch (Exception e) {
-				throw new WrappedRuntimeException(e);
-			}
-		} else {
-		}
-		return ResourceUtilities.read(cacheFile);
-	}
+    public String get(String path, String content) {
+        File cacheFile = getCacheFile(path);
+        if (!cacheFile.exists()) {
+            try {
+                String value = contentMapper.apply(content);
+                ResourceUtilities.write(value, cacheFile);
+            } catch (Exception e) {
+                throw new WrappedRuntimeException(e);
+            }
+        } else {
+        }
+        return ResourceUtilities.read(cacheFile);
+    }
 
-	public void invalidate(String path) {
-		getCacheFile(path).delete();
-	}
+    public void invalidate(String path) {
+        getCacheFile(path).delete();
+    }
 
-	public void invalidateAll() {
-		Arrays.asList(getCacheFile("0").getParentFile().listFiles()).stream()
-				.filter(File::isFile).forEach(File::delete);
-	}
+    public void invalidateAll() {
+        Arrays.asList(getCacheFile("0").getParentFile().listFiles()).stream()
+                .filter(File::isFile).forEach(File::delete);
+    }
 
-	private File getCacheFile(String path) {
-		return new File(Ax.format("%s/%s.dat", root.getPath(), path));
-	}
+    private File getCacheFile(String path) {
+        return new File(Ax.format("%s/%s.dat", root.getPath(), path));
+    }
 }
