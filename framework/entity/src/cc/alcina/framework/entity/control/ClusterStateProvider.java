0162f07bcfed2b812850381a1bc9e461545bbba5
Nick Reddel <nick.reddel@gmail.com>
2019-07-01 05:35:55 -0700

    Work on cluster tools

--- framework/entity/src/cc/alcina/framework/entity/control/ClusterStateProvider.java
+++ framework/entity/src/cc/alcina/framework/entity/control/ClusterStateProvider.java
@@ -1,6 +1,12 @@
 package cc.alcina.framework.entity.control;
 
+import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
+
 public interface ClusterStateProvider {
+    static ClusterStateProvider get() {
+        return Registry.impl(ClusterStateProvider.class);
+    }
+
     default String getMemberClusterState() {
         throw new UnsupportedOperationException();
     }
@@ -8,6 +14,10 @@ public interface ClusterStateProvider {
     default String getVmHealth() {
         throw new UnsupportedOperationException();
     }
+
+    long getVmInstancedId(String launcherName);
+
+    boolean isRestarting(String owner);
 
     default boolean isVmHealthyCached() {
         return true;
