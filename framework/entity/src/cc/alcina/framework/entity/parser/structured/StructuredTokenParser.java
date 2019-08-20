3b7e5ea6613d16366bc16674128290024d1e2302
Nick Reddel <nick.reddel@gmail.com>
2019-08-20 09:30:43 -0700

    Nada

--- framework/entity/src/cc/alcina/framework/entity/parser/structured/StructuredTokenParser.java
+++ framework/entity/src/cc/alcina/framework/entity/parser/structured/StructuredTokenParser.java
@@ -8,47 +8,47 @@ import cc.alcina.framework.entity.parser.structured.node.XmlNode;
 import cc.alcina.framework.entity.parser.structured.node.XmlTokenStream;
 
 public class StructuredTokenParser<C extends StructuredTokenParserContext> {
-	public static List<XmlToken> getTokens(Class<?> tokenClass) {
-		return XmlTokens.get().getTokens(tokenClass);
-	}
+    public static List<XmlToken> getTokens(Class<?> tokenClass) {
+        return XmlTokens.get().getTokens(tokenClass);
+    }
 
-	private List<XmlToken> tokens;
+    private List<XmlToken> tokens;
 
-	public XmlTokenOutput parse(Class<?> tokenClass, XmlTokenStream stream,
-			C context) {
-		return parse(tokenClass, stream, context, () -> true,
-				getTokens(tokenClass));
-	}
+    public XmlTokenOutput parse(Class<?> tokenClass, XmlTokenStream stream,
+            C context) {
+        return parse(tokenClass, stream, context, () -> true,
+                getTokens(tokenClass));
+    }
 
-	public XmlTokenOutput parse(Class<?> tokenClass, XmlTokenStream stream,
-			C context, Supplier<Boolean> shouldContinue,
-			List<XmlToken> tokens) {
-		this.tokens = tokens;
-		XmlDoc outDoc = new XmlDoc("<root/>");
-		XmlTokenOutput out = new XmlTokenOutput(outDoc);
-		context.out = out;
-		out.context = context;
-		context.stream = stream;
-		context.parser = this;
-		context.start();
-		while (stream.hasNext()) {
-			XmlNode node = stream.next();
-			handleNode(node, context);
-			if (!shouldContinue.get()) {
-				break;
-			}
-		}
-		context.end();
-		return out;
-	}
+    public XmlTokenOutput parse(Class<?> tokenClass, XmlTokenStream stream,
+            C context, Supplier<Boolean> shouldContinue,
+            List<XmlToken> tokens) {
+        this.tokens = tokens;
+        XmlDoc outDoc = new XmlDoc("<root/>");
+        XmlTokenOutput out = new XmlTokenOutput(outDoc);
+        context.out = out;
+        out.context = context;
+        context.stream = stream;
+        context.parser = this;
+        context.start();
+        while (stream.hasNext()) {
+            XmlNode node = stream.next();
+            handleNode(node, context);
+            if (!shouldContinue.get()) {
+                break;
+            }
+        }
+        context.end();
+        return out;
+    }
 
-	protected void handleNode(XmlNode node, C context) {
-		for (XmlToken token : tokens) {
-			if (token.matches(context, node)) {
-				XmlStructuralJoin join = new XmlStructuralJoin(node, token);
-				token.onMatch(context, join);
-				break;
-			}
-		}
-	}
+    protected void handleNode(XmlNode node, C context) {
+        for (XmlToken token : tokens) {
+            if (token.matches(context, node)) {
+                XmlStructuralJoin join = new XmlStructuralJoin(node, token);
+                token.onMatch(context, join);
+                break;
+            }
+        }
+    }
 }
