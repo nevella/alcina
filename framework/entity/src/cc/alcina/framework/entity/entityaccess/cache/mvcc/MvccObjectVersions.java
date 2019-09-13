1a7d76fe854fbc853644dcfe0678e46d05369f84
Nick Reddel <nick.reddel@gmail.com>
2019-09-13 06:46:09 -0700

    Add a bunch of mvcc docs

--- framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/mvcc/MvccObjectVersions.java
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/mvcc/MvccObjectVersions.java
@@ -16,102 +16,102 @@ import cc.alcina.framework.common.client.util.Ax;
  * @param <T>
  */
 public class MvccObjectVersions<T extends HasIdAndLocalId> {
-    public static <T extends HasIdAndLocalId> MvccObjectVersions<T> ensure(T t,
-            Transaction transaction) {
-        MvccObject mvccObject = (MvccObject) t;
-        MvccObjectVersions<T> versions = null;
-        // double-check
-        synchronized (mvccObject) {
-            versions = mvccObject.__getMvccVersions__();
-            if (versions == null) {
-                versions = new MvccObjectVersions<T>(t, transaction);
-                mvccObject.__setMvccVersions__(versions);
-            }
-            return versions;
-        }
-    }
+	public static <T extends HasIdAndLocalId> MvccObjectVersions<T> ensure(T t,
+			Transaction transaction) {
+		MvccObject mvccObject = (MvccObject) t;
+		MvccObjectVersions<T> versions = null;
+		// double-check
+		synchronized (mvccObject) {
+			versions = mvccObject.__getMvccVersions__();
+			if (versions == null) {
+				versions = new MvccObjectVersions<T>(t, transaction);
+				mvccObject.__setMvccVersions__(versions);
+			}
+			return versions;
+		}
+	}
 
-    ConcurrentHashMap<Transaction, ObjectVersion<T>> versions = new ConcurrentHashMap<>();
+	ConcurrentHashMap<Transaction, ObjectVersion<T>> versions = new ConcurrentHashMap<>();
 
-    StoreTransaction baseStoreTransaction;
+	StoreTransaction baseStoreTransaction;
 
-    Object versionCreationMonitor = new Object();
+	Object versionCreationMonitor = new Object();
 
-    public MvccObjectVersions(T t, Transaction initialTransaction) {
-        /*
-         * If in the 'preparing' phase, and t is non-local, this is a
-         * modification of the base version transaction.
-         * 
-         * In that case, create an initial copy, keyed to the base transaction -
-         * if developing this allows users to directly modify their copy without
-         * needing to swap to a writeable version. In production, force the swap
-         * (there's a tiny risk of a race)
-         */
-        if (initialTransaction.phase == TransactionPhase.PREPARING
-                && t.provideWasPersisted()) {
-            if (Ax.isTest()) {
-                {
-                    ObjectVersion<T> version = new ObjectVersion<>();
-                    version.transaction = Transactions
-                            .baseTransaction(Mvcc.getStore(t));
-                    version.object = Transactions.copyObject(t);
-                    versions.put(version.transaction, version);
-                }
-                {
-                    ObjectVersion<T> version = new ObjectVersion<>();
-                    version.transaction = initialTransaction;
-                    version.object = t;
-                    versions.put(version.transaction, version);
-                }
-            } else {
-                {
-                    ObjectVersion<T> version = new ObjectVersion<>();
-                    version.transaction = Transactions
-                            .baseTransaction(Mvcc.getStore(t));
-                    version.object = t;
-                    versions.put(version.transaction, version);
-                }
-                {
-                    ObjectVersion<T> version = new ObjectVersion<>();
-                    version.transaction = initialTransaction;
-                    version.object = Transactions.copyObject(t);
-                    versions.put(version.transaction, version);
-                }
-            }
-        } else {
-            /* transforms from another vm or created this tx */
-            {
-                ObjectVersion<T> version = new ObjectVersion<>();
-                version.transaction = initialTransaction;
-                version.object = t;
-                versions.put(version.transaction, version);
-            }
-        }
-    }
+	public MvccObjectVersions(T t, Transaction initialTransaction) {
+		/*
+		 * If in the 'preparing' phase, and t is non-local, this is a
+		 * modification of the base transaction version.
+		 * 
+		 * In that case, create an initial copy, keyed to the base transaction -
+		 * if developing this allows users to directly modify their copy without
+		 * needing to swap to a writeable version. In production, force the swap
+		 * (there's a tiny risk of a race)
+		 */
+		if (initialTransaction.phase == TransactionPhase.PREPARING
+				&& t.provideWasPersisted()) {
+			if (Ax.isTest()) {
+				{
+					ObjectVersion<T> version = new ObjectVersion<>();
+					version.transaction = Transactions
+							.baseTransaction(Mvcc.getStore(t));
+					version.object = Transactions.copyObject(t);
+					versions.put(version.transaction, version);
+				}
+				{
+					ObjectVersion<T> version = new ObjectVersion<>();
+					version.transaction = initialTransaction;
+					version.object = t;
+					versions.put(version.transaction, version);
+				}
+			} else {
+				{
+					ObjectVersion<T> version = new ObjectVersion<>();
+					version.transaction = Transactions
+							.baseTransaction(Mvcc.getStore(t));
+					version.object = t;
+					versions.put(version.transaction, version);
+				}
+				{
+					ObjectVersion<T> version = new ObjectVersion<>();
+					version.transaction = initialTransaction;
+					version.object = Transactions.copyObject(t);
+					versions.put(version.transaction, version);
+				}
+			}
+		} else {
+			/* transforms from another vm or created this tx */
+			{
+				ObjectVersion<T> version = new ObjectVersion<>();
+				version.transaction = initialTransaction;
+				version.object = t;
+				versions.put(version.transaction, version);
+			}
+		}
+	}
 
-    T resolve(boolean write) {
-        Transaction transaction = Transaction.current();
-        ObjectVersion<T> version = versions.get(transaction);
-        if (version != null) {
-            return version.object;
-        }
-        synchronized (versionCreationMonitor) {
-            version = versions.get(transaction);
-            if (version != null) {
-                return version.object;
-            }
-            version = new ObjectVersion<>();
-            version.transaction = transaction;
-            Transaction mostRecent = transaction.mostRecentPriorTransaction(
-                    versions.keys(), baseStoreTransaction.store);
-            version.object = Transactions
-                    .copyObject(versions.get(mostRecent).object);
-            versions.put(transaction, version);
-            if (write) {
-                Domain.register(version.object);
-                // FIXME - probably register prop changes with indexing listener
-            }
-        }
-        return version.object;
-    }
+	T resolve(boolean write) {
+		Transaction transaction = Transaction.current();
+		ObjectVersion<T> version = versions.get(transaction);
+		if (version != null) {
+			return version.object;
+		}
+		synchronized (versionCreationMonitor) {
+			version = versions.get(transaction);
+			if (version != null) {
+				return version.object;
+			}
+			version = new ObjectVersion<>();
+			version.transaction = transaction;
+			Transaction mostRecent = transaction.mostRecentPriorTransaction(
+					versions.keys(), baseStoreTransaction.store);
+			version.object = Transactions
+					.copyObject(versions.get(mostRecent).object);
+			versions.put(transaction, version);
+			if (write) {
+				Domain.register(version.object);
+				// FIXME - probably register prop changes with indexing listener
+			}
+		}
+		return version.object;
+	}
 }
--- framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/mvcc/TransactionalMap.java
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/mvcc/TransactionalMap.java
@@ -22,180 +22,196 @@ import cc.alcina.framework.entity.entityaccess.cache.mvcc.Transaction.Transactio
  * Synchronization:
  * 
  * 'this' acts as a lock on modification of the layers field (since layers may be null)
+ * 
+ * This class is intended for transaction-aware indices and lookups
+ * 
+ * The map is composed of a base layer, a list of (transactional or merged-transactional) layers corresponding to deltas
+ *  at the time of the transaction(s) (note that these layers are visible to all current transactions) 
+ *  and a third grouping of transactional layers 
+ *   ('layers') whose resolution is transaction-depdendent.
+ *   
+ *    Operations are resolved to return the most recent value
+ *   for the combined sequence of layers, from (per-transaction) youngest-to-oldest
+ *   
  */
 public class TransactionalMap<K, V> extends AbstractMap<K, V> {
-    private Layer base;
+	private Layer base;
 
-    private ConcurrentSkipListMap<Transaction, Layer> layers;
+	private ConcurrentSkipListMap<Transaction, Layer> layers;
 
-    private List<Layer> baseLayerList;
+	
+	private List<Layer> baseLayerList;
 
-    public TransactionalMap() {
-        base = new Layer(Transaction.current());
-        baseLayerList = Collections.singletonList(base);
-    }
+	public TransactionalMap() {
+		base = new Layer(Transaction.current());
+		baseLayerList = Collections.singletonList(base);
+	}
 
-    @Override
-    public Set<Entry<K, V>> entrySet() {
-        return new TransactionalEntrySet();
-    }
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		return new TransactionalEntrySet();
+	}
 
-    @Override
-    public V get(Object key) {
-        Objects.requireNonNull(key);
-        if (layers == null) {
-            return base.get(key);
-        }
-        List<TransactionalMap<K, V>.Layer> visibleLayers = visibleLayers();
-        for (int idx = visibleLayers.size() - 1; idx > 0; idx--) {
-            Layer layer = visibleLayers.get(idx);
-            if (layer.containsKey(key)) {
-                return layer.get(key);
-            }
-        }
-        return null;
-    }
+	@Override
+	public V get(Object key) {
+		Objects.requireNonNull(key);
+		if (layers == null) {
+			return base.get(key);
+		}
+		List<TransactionalMap<K, V>.Layer> visibleLayers = visibleLayers();
+		for (int idx = visibleLayers.size() - 1; idx > 0; idx--) {
+			Layer layer = visibleLayers.get(idx);
+			if (layer.containsKey(key)) {
+				return layer.get(key);
+			}
+			if(layer.wasRemoved(key)){
+				return null;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public V put(K key, V value) {
-        Objects.requireNonNull(key);
-        Objects.requireNonNull(value);
-        V existing = get(key);
-        Layer layer = ensureLayer();
-        layer.put(key, value, existing);
-        return existing;
-    }
+	@Override
+	public V put(K key, V value) {
+		Objects.requireNonNull(key);
+		Objects.requireNonNull(value);
+		V existing = get(key);
+		Layer layer = ensureLayer();
+		layer.put(key, value, existing);
+		return existing;
+	}
 
-    @Override
-    public V remove(Object key) {
-        Objects.requireNonNull(key);
-        V existing = get(key);
-        Layer layer = ensureLayer();
-        layer.remove((K) key);
-        return existing;
-    }
+	@Override
+	public V remove(Object key) {
+		Objects.requireNonNull(key);
+		V existing = get(key);
+		Layer layer = ensureLayer();
+		layer.remove((K) key);
+		return existing;
+	}
 
-    private TransactionalMap<K, V>.Layer ensureLayer() {
-        Transaction transaction = Transaction.current();
-        if (layers == null || !layers.containsKey(transaction)) {
-            // double-checked
-            synchronized (this) {
-                if (layers == null || !layers.containsKey(transaction)) {
-                    if (layers == null) {
-                        layers = new ConcurrentSkipListMap<>(
-                                new TransactionComparator());
-                    }
-                    layers.put(transaction, new Layer(transaction));
-                }
-            }
-        }
-        return layers.get(transaction);
-    }
+	private TransactionalMap<K, V>.Layer ensureLayer() {
+//TEST
+		Transaction transaction = Transaction.current();
+		if (layers == null || !layers.containsKey(transaction)) {
+			// double-checked
+			synchronized (this) {
+				if (layers == null || !layers.containsKey(transaction)) {
+					if (layers == null) {
+						layers = new ConcurrentSkipListMap<>(
+								new TransactionComparator());
+					}
+					layers.put(transaction, new Layer(transaction));
+				}
+			}
+		}
+		return layers.get(transaction);
+	}
 
-    private List<Layer> visibleLayers() {
-        if (layers == null) {
-            return baseLayerList;
-        } else {
-            List<Layer> visibleLayers = new ArrayList<>();
-            visibleLayers.add(base);
-            Transaction transaction = Transaction.current();
-            /*
-             * for the moment, assume visible tx size not >> layer size
-             */
-            for (Transaction committed : transaction.committedTransactions
-                    .values()) {
-                TransactionalMap<K, V>.Layer layer = layers.get(committed);
-                if (layer != null) {
-                    visibleLayers.add(layer);
-                }
-            }
-            return visibleLayers;
-        }
-    }
+	private List<Layer> visibleLayers() {
+		if (layers == null) {
+			return baseLayerList;
+		} else {
+			List<Layer> visibleLayers = new ArrayList<>();
+			visibleLayers.add(base);
+			Transaction transaction = Transaction.current();
+			/*
+			 * for the moment, assume visible tx size not >> layer size
+			 */
+			for (Transaction committed : transaction.committedTransactions
+					.values()) {
+				TransactionalMap<K, V>.Layer layer = layers.get(committed);
+				if (layer != null) {
+					visibleLayers.add(layer);
+				}
+			}
+			return visibleLayers;
+		}
+	}
 
-    class Layer {
-        int added;
+	class Layer {
+		int added;
 
-        /*
-         * FIXME - these should be specialised. Removed should be lazy
-         */
-        Map<K, Boolean> removed = new LinkedHashMap<>();
+		/*
+		 * FIXME - these should be specialised. Removed should be lazy
+		 */
+		Map<K, Boolean> removed = new LinkedHashMap<>();
 
-        Map<K, V> modified = new LinkedHashMap<>();
+		Map<K, V> modified = new LinkedHashMap<>();
 
-        Transaction transaction;
+		Transaction transaction;
 
-        public Layer(Transaction transaction) {
-            this.transaction = transaction;
-        }
+		public Layer(Transaction transaction) {
+			this.transaction = transaction;
+		}
 
-        public boolean containsKey(Object key) {
-            return removed.containsKey(key) || modified.containsKey(key);
-        }
+		public boolean containsKey(Object key) {
+			return removed.containsKey(key) || modified.containsKey(key);
+		}
 
-        public V get(Object key) {
-            return modified.get(key);
-        }
+		public V get(Object key) {
+			return modified.get(key);
+		}
 
-        public void put(K key, V value, V existing) {
-            modified.put(key, value);
-            if (existing == null) {
-                added++;
-                removed.remove(key);
-            }
-        }
+		public void put(K key, V value, V existing) {
+			modified.put(key, value);
+			if (existing == null) {
+				added++;
+				removed.remove(key);
+			}
+		}
 
-        public void remove(K key) {
-            removed.put(key, Boolean.TRUE);
-        }
+		public void remove(K key) {
+			removed.put(key, Boolean.TRUE);
+		}
 
-        public boolean wasRemoved(Object key) {
-            return false;
-        }
+		public boolean wasRemoved(Object key) {
+			return removed.containsKey(key);
+		}
 
-        Iterator<Entry<K, V>> modifiedEntrySetIterator() {
-            Iterator<Entry<K, V>> iterator = modified.entrySet().iterator();
-            return iterator;
-        }
-    }
+		Iterator<Entry<K, V>> modifiedEntrySetIterator() {
+			Iterator<Entry<K, V>> iterator = modified.entrySet().iterator();
+			return iterator;
+		}
+	}
 
-    class TransactionalEntrySet extends AbstractSet<Entry<K, V>> {
-        private List<Layer> visibleLayers;
+	class TransactionalEntrySet extends AbstractSet<Entry<K, V>> {
+		private List<Layer> visibleLayers;
 
-        public TransactionalEntrySet() {
-            visibleLayers = visibleLayers();
-        }
+		public TransactionalEntrySet() {
+			visibleLayers = visibleLayers();
+		}
 
-        @Override
-        public Iterator<Entry<K, V>> iterator() {
-            List<Iterator<Entry<K, V>>> iterators = visibleLayers.stream()
-                    .map(Layer::modifiedEntrySetIterator)
-                    .collect(Collectors.toList());
-            Iterator<Entry<K, V>>[] iteratorArray = (Iterator<Entry<K, V>>[]) iterators
-                    .toArray(new Iterator[iterators.size()]);
-            MultiIterator<Entry<K, V>> layerIterator = new MultiIterator<Entry<K, V>>(
-                    false, iteratorArray);
-            Function<Entry<K, V>, Entry<K, V>> mapping = e -> {
-                int idx = layerIterator.getCurrentIteratorIndex();
-                idx++;// check if modified in any subsequent layers
-                for (; idx < visibleLayers.size(); idx++) {
-                    if (visibleLayers.get(idx).containsKey(e.getKey())) {
-                        // will be returned (or not, if removed in a subsequent
-                        // layer) by the subsequent layer iterator
-                        return null;
-                    }
-                }
-                return e;
-            };
-            FilteringIterator<Entry<K, V>> filteringIterator = new FilteringIterator<>(
-                    layerIterator, mapping);
-            return filteringIterator;
-        }
+		@Override
+		public Iterator<Entry<K, V>> iterator() {
+			List<Iterator<Entry<K, V>>> iterators = visibleLayers.stream()
+					.map(Layer::modifiedEntrySetIterator)
+					.collect(Collectors.toList());
+			Iterator<Entry<K, V>>[] iteratorArray = (Iterator<Entry<K, V>>[]) iterators
+					.toArray(new Iterator[iterators.size()]);
+			MultiIterator<Entry<K, V>> layerIterator = new MultiIterator<Entry<K, V>>(
+					false, iteratorArray);
+			Function<Entry<K, V>, Entry<K, V>> mapping = e -> {
+				int idx = layerIterator.getCurrentIteratorIndex();
+				idx++;// check if modified in any subsequent layers
+				for (; idx < visibleLayers.size(); idx++) {
+					if (visibleLayers.get(idx).containsKey(e.getKey())) {
+						// will be returned (or not, if removed in a subsequent
+						// layer) by the subsequent layer iterator
+						return null;
+					}
+				}
+				return e;
+			};
+			FilteringIterator<Entry<K, V>> filteringIterator = new FilteringIterator<>(
+					layerIterator, mapping);
+			return filteringIterator;
+		}
 
-        @Override
-        public int size() {
-            return visibleLayers.stream().collect(Collectors
-                    .summingInt(layer -> layer.added - layer.removed.size()));
-        }
-    }
+		@Override
+		public int size() {
+			return visibleLayers.stream().collect(Collectors
+					.summingInt(layer -> layer.added - layer.removed.size()));
+		}
+	}
 }
