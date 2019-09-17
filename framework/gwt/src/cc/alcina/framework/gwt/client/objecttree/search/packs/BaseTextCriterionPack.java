ba095468c72fe54e430c3710c5c3c930337162a1
Nick Reddel <nick.reddel@gmail.com>
2019-09-17 14:04:27 -0700

    Extend base search criterion behaviour

--- framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/packs/BaseTextCriterionPack.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/packs/BaseTextCriterionPack.java
@@ -14,52 +14,62 @@ import cc.alcina.framework.gwt.client.objecttree.search.StandardSearchOperator;
 import cc.alcina.framework.gwt.client.util.TextUtils;
 
 public class BaseTextCriterionPack {
-    public interface BaseTextCriterionHandler<T> {
-        public boolean test(T t, String text);
+	public interface BaseTextCriterionHandler<T> {
+		public boolean test(T t, String text);
 
-        default DomainFilter getFilter0(TxtCriterion sc) {
-            String text = TextUtils.normalisedLcKey(sc.getText());
-            if (text.isEmpty()) {
-                return null;
-            }
-            return new DomainFilter(new CollectionFilter<T>() {
-                @Override
-                public boolean allow(T o) {
-                    return BaseTextCriterionHandler.this.test(o, text);
-                }
-            }).invertIf(sc
-                    .getOperator() == StandardSearchOperator.DOES_NOT_CONTAIN);
-        }
-    }
+		default DomainFilter getFilter0(TxtCriterion sc) {
+			String text = TextUtils.normalisedLcKey(sc.getText());
+			if (text.isEmpty()) {
+				return null;
+			}
+			return new DomainFilter(new CollectionFilter<T>() {
+				@Override
+				public boolean allow(T o) {
+					return BaseTextCriterionHandler.this.test(o, text);
+				}
+			}).invertIf(sc
+					.getOperator() == StandardSearchOperator.DOES_NOT_CONTAIN);
+		}
+	}
 
-    public static abstract class BaseTextCriterionSearchable
-            extends SubTextCriterionSearchable<TxtCriterion> {
-        public BaseTextCriterionSearchable(String objectName) {
-            super(TxtCriterion.class, objectName);
-        }
-    }
+	public static abstract class BaseTextCriterionSearchable
+			extends SubTextCriterionSearchable<TxtCriterion> {
+		public BaseTextCriterionSearchable(String objectName) {
+			super(TxtCriterion.class, objectName);
+		}
 
-    public static abstract class SubTextCriterionSearchable<T extends TxtCriterion>
-            extends FlatSearchable<T> {
-        public SubTextCriterionSearchable(Class<T> clazz, String objectName) {
-            super(clazz, objectName, "Text",
-                    Arrays.asList(StandardSearchOperator.CONTAINS,
-                            StandardSearchOperator.DOES_NOT_CONTAIN));
-        }
+		public BaseTextCriterionSearchable(String objectName,
+				String fieldName) {
+			super(TxtCriterion.class, objectName, fieldName);
+		}
+	}
 
-        @Override
-        public AbstractBoundWidget createEditor() {
-            return new TextBox();
-        }
+	public static abstract class SubTextCriterionSearchable<T extends TxtCriterion>
+			extends FlatSearchable<T> {
+		public SubTextCriterionSearchable(Class<T> clazz, String objectName) {
+			this(clazz, objectName, "Text");
+		}
 
-        @Override
-        public String getCriterionPropertyName() {
-            return "text";
-        }
+		public SubTextCriterionSearchable(Class<T> clazz, String objectName,
+				String name) {
+			super(clazz, objectName, name,
+					Arrays.asList(StandardSearchOperator.CONTAINS,
+							StandardSearchOperator.DOES_NOT_CONTAIN));
+		}
 
-        @Override
-        public boolean hasValue(TxtCriterion sc) {
-            return CommonUtils.isNotNullOrEmpty(sc.getText());
-        }
-    }
+		@Override
+		public AbstractBoundWidget createEditor() {
+			return new TextBox();
+		}
+
+		@Override
+		public String getCriterionPropertyName() {
+			return "text";
+		}
+
+		@Override
+		public boolean hasValue(TxtCriterion sc) {
+			return CommonUtils.isNotNullOrEmpty(sc.getText());
+		}
+	}
 }
