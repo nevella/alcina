7ea4d5de72473f663d2f306f6acb08c519344aac
Nick Reddel <nick.reddel@gmail.com>
2019-09-18 11:06:48 -0700

    Improve some logging, topic utilities

--- framework/common/src/cc/alcina/framework/common/client/util/TopicPublisher.java
+++ framework/common/src/cc/alcina/framework/common/client/util/TopicPublisher.java
@@ -11,123 +11,127 @@ import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
  * Key 'null' (when passed to addTopicListener) receives all topics
  */
 public class TopicPublisher {
-    // use a list - the listener may be added/removed multiple times (although
-    // that's probably not what's wanted)
-    private Multimap<String, List<TopicListener>> lookup = new Multimap<>();
+	// use a list - the listener may be added/removed multiple times (although
+	// that's probably not what's wanted)
+	private Multimap<String, List<TopicListener>> lookup = new Multimap<>();
 
-    public void addTopicListener(String key, TopicListener listener) {
-        synchronized (lookup) {
-            lookup.add(key, listener);
-        }
-    }
+	public void addTopicListener(String key, TopicListener listener) {
+		synchronized (lookup) {
+			lookup.add(key, listener);
+		}
+	}
 
-    public void listenerDelta(String key, TopicListener listener, boolean add) {
-        if (add) {
-            addTopicListener(key, listener);
-        } else {
-            removeTopicListener(key, listener);
-        }
-    }
+	public void listenerDelta(String key, TopicListener listener, boolean add) {
+		if (add) {
+			addTopicListener(key, listener);
+		} else {
+			removeTopicListener(key, listener);
+		}
+	}
 
-    public void publishTopic(String key, Object message) {
-        List<TopicListener> listeners = null;
-        synchronized (lookup) {
-            listeners = lookup.getAndEnsure(key).stream()
-                    .collect(Collectors.toList());
-            lookup.getAndEnsure(null).stream().forEach(listeners::add);
-        }
-        for (TopicListener listener : listeners) {
-            listener.topicPublished(key, message);
-        }
-    }
+	public void publishTopic(String key, Object message) {
+		List<TopicListener> listeners = null;
+		synchronized (lookup) {
+			listeners = lookup.getAndEnsure(key).stream()
+					.collect(Collectors.toList());
+			lookup.getAndEnsure(null).stream().forEach(listeners::add);
+		}
+		for (TopicListener listener : listeners) {
+			listener.topicPublished(key, message);
+		}
+	}
 
-    public void removeTopicListener(String key, TopicListener listener) {
-        synchronized (lookup) {
-            lookup.subtract(key, listener);
-        }
-    }
+	public void removeTopicListener(String key, TopicListener listener) {
+		synchronized (lookup) {
+			lookup.subtract(key, listener);
+		}
+	}
 
-    @RegistryLocation(registryPoint = ClearStaticFieldsOnAppShutdown.class)
-    public static class GlobalTopicPublisher extends TopicPublisher {
-        private static volatile GlobalTopicPublisher singleton;
+	@RegistryLocation(registryPoint = ClearStaticFieldsOnAppShutdown.class)
+	public static class GlobalTopicPublisher extends TopicPublisher {
+		private static volatile GlobalTopicPublisher singleton;
 
-        public static GlobalTopicPublisher get() {
-            if (singleton == null) {
-                synchronized (GlobalTopicPublisher.class) {
-                    singleton = new GlobalTopicPublisher();
-                    Registry.registerSingleton(GlobalTopicPublisher.class,
-                            singleton);
-                }
-            }
-            return singleton;
-        }
+		public static GlobalTopicPublisher get() {
+			if (singleton == null) {
+				synchronized (GlobalTopicPublisher.class) {
+					singleton = new GlobalTopicPublisher();
+					Registry.registerSingleton(GlobalTopicPublisher.class,
+							singleton);
+				}
+			}
+			return singleton;
+		}
 
-        private GlobalTopicPublisher() {
-            super();
-        }
-    }
+		private GlobalTopicPublisher() {
+			super();
+		}
+	}
 
-    @FunctionalInterface
-    public interface TopicListener<T> {
-        void topicPublished(String key, T message);
-    }
+	@FunctionalInterface
+	public interface TopicListener<T> {
+		void topicPublished(String key, T message);
+	}
 
-    public static class TopicSupport<T> {
-        private String topic;
+	public static class TopicSupport<T> {
+		private String topic;
 
-        private TopicPublisher topicPublisher;
+		private TopicPublisher topicPublisher;
 
-        private boolean wasPublished;
+		private boolean wasPublished;
 
-        public TopicSupport(String topic) {
-            this(topic, true);
-        }
+		public TopicSupport(String topic) {
+			this(topic, true);
+		}
 
-        public TopicSupport(String topic, boolean global) {
-            this.topic = topic;
-            topicPublisher = global ? GlobalTopicPublisher.get()
-                    : new TopicPublisher();
-        }
+		public TopicSupport(String topic, boolean global) {
+			this.topic = topic;
+			topicPublisher = global ? GlobalTopicPublisher.get()
+					: new TopicPublisher();
+		}
 
-        public void add(TopicListener<T> listener) {
-            add(listener, false);
-        }
+		public void add(TopicListener<T> listener) {
+			add(listener, false);
+		}
 
-        public void add(TopicListener<T> listener, boolean fireIfWasPublished) {
-            delta(listener, true);
-            if (wasPublished && fireIfWasPublished) {
-                // note - we don't keep a ref to the last published object -
-                // this assumes the caller knows how to get it. Useful for
-                // adding async one-off listeners when the event may have
-                // already occurred
-                listener.topicPublished(topic, null);
-            }
-        }
+		public void add(TopicListener<T> listener, boolean fireIfWasPublished) {
+			delta(listener, true);
+			if (wasPublished && fireIfWasPublished) {
+				// note - we don't keep a ref to the last published object -
+				// this assumes the caller knows how to get it. Useful for
+				// adding async one-off listeners when the event may have
+				// already occurred
+				listener.topicPublished(topic, null);
+			}
+		}
 
-        public void addRunnable(Runnable runnable, boolean fireIfWasPublished) {
-            add(new TopicListener() {
-                @Override
-                public void topicPublished(String key, Object message) {
-                    runnable.run();
-                }
-            });
-        }
+		public void addRunnable(Runnable runnable) {
+			addRunnable(runnable, false);
+		}
 
-        public void delta(TopicListener<T> listener, boolean add) {
-            topicPublisher.listenerDelta(topic, listener, add);
-        }
+		public void addRunnable(Runnable runnable, boolean fireIfWasPublished) {
+			add(new TopicListener() {
+				@Override
+				public void topicPublished(String key, Object message) {
+					runnable.run();
+				}
+			}, fireIfWasPublished);
+		}
 
-        public void publish(T t) {
-            try {
-                topicPublisher.publishTopic(topic, t);
-                wasPublished = true;
-            } catch (Throwable e) {
-                e.printStackTrace();
-            }
-        }
+		public void delta(TopicListener<T> listener, boolean add) {
+			topicPublisher.listenerDelta(topic, listener, add);
+		}
 
-        public void remove(TopicListener<T> listener) {
-            delta(listener, false);
-        }
-    }
+		public void publish(T t) {
+			try {
+				topicPublisher.publishTopic(topic, t);
+				wasPublished = true;
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void remove(TopicListener<T> listener) {
+			delta(listener, false);
+		}
+	}
 }
--- framework/entity/src/cc/alcina/framework/entity/logic/EntityLayerUtils.java
+++ framework/entity/src/cc/alcina/framework/entity/logic/EntityLayerUtils.java
@@ -16,115 +16,130 @@ import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
 import cc.alcina.framework.common.client.util.Ax;
 import cc.alcina.framework.entity.ResourceUtilities;
 import cc.alcina.framework.entity.SEUtilities;
+import cc.alcina.framework.entity.entityaccess.CommonPersistenceLocal;
 import cc.alcina.framework.entity.entityaccess.CommonPersistenceProvider;
 import cc.alcina.framework.entity.util.SafeConsoleAppender;
 
 public class EntityLayerUtils {
-    public static String getLocalHostName() {
-        try {
-            String defined = ResourceUtilities.get(EntityLayerUtils.class,
-                    "localHostName");
-            if (Ax.isBlank(defined)) {
-                return java.net.InetAddress.getLocalHost().getHostName();
-            } else {
-                return defined;
-            }
-        } catch (Exception e) {
-            throw new WrappedRuntimeException(e);
-        }
-    }
+	public static String getLocalHostName() {
+		try {
+			String defined = ResourceUtilities.get(EntityLayerUtils.class,
+					"localHostName");
+			if (Ax.isBlank(defined)) {
+				return java.net.InetAddress.getLocalHost().getHostName();
+			} else {
+				return defined;
+			}
+		} catch (Exception e) {
+			throw new WrappedRuntimeException(e);
+		}
+	}
 
-    public static void log(LogMessageType componentKey, String message) {
-        EntityLayerObjects.get().getPersistentLogger()
-                .info(componentKey + " - " + message);
-    }
+	public static void log(LogMessageType componentKey, String message) {
+		EntityLayerObjects.get().getPersistentLogger()
+				.info(componentKey + " - " + message);
+	}
 
-    public static void log(LogMessageType componentKey, String message,
-            Throwable throwable) {
-        Logger persistentLogger = EntityLayerObjects.get()
-                .getPersistentLogger();
-        if (persistentLogger == null) {
-            throwable.printStackTrace();
-            return;
-        }
-        persistentLogger.warn(
-                componentKey + " - " + message + "\n" + throwable.toString(),
-                throwable);
-    }
+	public static void log(LogMessageType componentKey, String message,
+			Throwable throwable) {
+		Logger persistentLogger = EntityLayerObjects.get()
+				.getPersistentLogger();
+		if (persistentLogger == null) {
+			throwable.printStackTrace();
+			return;
+		}
+		persistentLogger.warn(
+				componentKey + " - " + message + "\n" + throwable.toString(),
+				throwable);
+	}
 
-    public static void persistentLog(Enum componentKey, String message) {
-        Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
-                .log(message, componentKey.toString());
-    }
+	public static void persistentLog(Enum componentKey, String message) {
+		Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
+				.log(message, componentKey.toString());
+	}
 
-    // convenience
-    public static void persistentLog(String message, Object componentKey) {
-        Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
-                .log(message, componentKey.toString());
-    }
+	public static void persistentLog(Exception e, Object logMessageType) {
+		try {
+			CommonPersistenceLocal cpl = Registry
+					.impl(CommonPersistenceProvider.class)
+					.getCommonPersistence();
+			cpl.log(SEUtilities.getFullExceptionMessage(e),
+					logMessageType.toString());
+			LoggerFactory.getLogger(EntityLayerUtils.class)
+					.warn(logMessageType.toString(), e);
+		} catch (Throwable e1) {
+			e1.printStackTrace();
+		}
+	}
 
-    public static void setLevel(Class clazz, Level level) {
-        setLevel(clazz.getName(), level);
-    }
+	// convenience
+	public static void persistentLog(String message, Object componentKey) {
+		Registry.impl(CommonPersistenceProvider.class).getCommonPersistence()
+				.log(message, componentKey.toString());
+	}
 
-    public static void setLevel(org.slf4j.Logger slf4jlogger, Level level) {
-        if (ResourceUtilities.is(EntityLayerUtils.class, "debugSetLogLevels")) {
-            Ax.out("%s => %s", slf4jlogger.getName(), level);
-        }
-        if (!Ax.isTest() && slf4jlogger.getClass().getName()
-                .equals("org.slf4j.impl.Slf4jLogger")) {
-            // jboss/wildfly
-            try {
-                Field loggerField = SEUtilities
-                        .getFieldByName(slf4jlogger.getClass(), "logger");
-                loggerField.setAccessible(true);
-                java.util.logging.Logger jblmLogger = (java.util.logging.Logger) loggerField
-                        .get(slf4jlogger);
-                java.util.logging.Level julLevel = java.util.logging.Level.WARNING;
-                if (level == Level.DEBUG || level == Level.TRACE
-                        || level == Level.ALL) {
-                    julLevel = java.util.logging.Level.FINE;
-                } else if (level == Level.INFO) {
-                    julLevel = java.util.logging.Level.INFO;
-                }
-                jblmLogger.setLevel(julLevel);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            // devconsole/log4j
-            setLevel0(slf4jlogger.getName(), level);
-        }
-    }
+	public static void setLevel(Class clazz, Level level) {
+		setLevel(clazz.getName(), level);
+	}
 
-    public static void setLevel(String key, Level level) {
-        setLevel(LoggerFactory.getLogger(key), level);
-    }
+	public static void setLevel(org.slf4j.Logger slf4jlogger, Level level) {
+		if (ResourceUtilities.is(EntityLayerUtils.class, "debugSetLogLevels")) {
+			Ax.out("%s => %s", slf4jlogger.getName(), level);
+		}
+		if (!Ax.isTest() && slf4jlogger.getClass().getName()
+				.equals("org.slf4j.impl.Slf4jLogger")) {
+			// jboss/wildfly
+			try {
+				Field loggerField = SEUtilities
+						.getFieldByName(slf4jlogger.getClass(), "logger");
+				loggerField.setAccessible(true);
+				java.util.logging.Logger jblmLogger = (java.util.logging.Logger) loggerField
+						.get(slf4jlogger);
+				java.util.logging.Level julLevel = java.util.logging.Level.WARNING;
+				if (level == Level.DEBUG || level == Level.TRACE
+						|| level == Level.ALL) {
+					julLevel = java.util.logging.Level.FINE;
+				} else if (level == Level.INFO) {
+					julLevel = java.util.logging.Level.INFO;
+				}
+				jblmLogger.setLevel(julLevel);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			// devconsole/log4j
+			setLevel0(slf4jlogger.getName(), level);
+		}
+	}
 
-    public static void setLogLevelsFromCustomProperties() {
-        Map<String, String> map = ResourceUtilities.getCustomProperties();
-        map.forEach((k, v) -> {
-            if (k.startsWith("log.level.")) {
-                k = k.substring("log.level.".length());
-                setLevel(k, Level.toLevel(v));
-            }
-        });
-    }
+	public static void setLevel(String key, Level level) {
+		setLevel(LoggerFactory.getLogger(key), level);
+	}
 
-    private static void setLevel0(String key, Level level) {
-        Logger logger = Logger.getLogger(key);
-        if (Ax.isTest()) {
-            setStandardConsoleAppender(key);
-            logger.setAdditivity(false);
-        }
-        logger.setLevel(level);
-    }
+	public static void setLogLevelsFromCustomProperties() {
+		Map<String, String> map = ResourceUtilities.getCustomProperties();
+		map.forEach((k, v) -> {
+			if (k.startsWith("log.level.")) {
+				k = k.substring("log.level.".length());
+				setLevel(k, Level.toLevel(v));
+			}
+		});
+	}
 
-    private static void setStandardConsoleAppender(String key) {
-        Logger logger = Logger.getLogger(key);
-        logger.removeAllAppenders();
-        Layout layout = new PatternLayout("%-5p [%c{1}] %m%n");
-        Appender appender = new SafeConsoleAppender(layout);
-        logger.addAppender(appender);
-    }
+	private static void setLevel0(String key, Level level) {
+		Logger logger = Logger.getLogger(key);
+		if (Ax.isTest()) {
+			setStandardConsoleAppender(key);
+			logger.setAdditivity(false);
+		}
+		logger.setLevel(level);
+	}
+
+	private static void setStandardConsoleAppender(String key) {
+		Logger logger = Logger.getLogger(key);
+		logger.removeAllAppenders();
+		Layout layout = new PatternLayout("%-5p [%c{1}] %m%n");
+		Appender appender = new SafeConsoleAppender(layout);
+		logger.addAppender(appender);
+	}
 }
