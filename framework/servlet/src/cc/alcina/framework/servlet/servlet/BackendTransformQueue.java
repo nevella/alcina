03356afe6f9ed8b6203443256960eb2a01dde62f
Nick Reddel <nick.reddel@gmail.com>
2019-12-05 15:23:58 -0800

    Handle double deletion in backend transforms

--- framework/servlet/src/cc/alcina/framework/servlet/servlet/BackendTransformQueue.java
+++ framework/servlet/src/cc/alcina/framework/servlet/servlet/BackendTransformQueue.java
@@ -17,97 +17,97 @@ import cc.alcina.framework.servlet.Sx;
 import cc.alcina.framework.servlet.servlet.ServletLayerTransforms.TransformPriorityStd;
 
 class BackendTransformQueue {
-    private AtEndOfEventSeriesTimer persistTimer;
+	private AtEndOfEventSeriesTimer persistTimer;
 
-    List<Runnable> tasks = new ArrayList<>();
+	List<Runnable> tasks = new ArrayList<>();
 
-    Object persistMonitor = new Object();
+	Object persistMonitor = new Object();
 
-    Logger logger = LoggerFactory.getLogger(getClass());
+	Logger logger = LoggerFactory.getLogger(getClass());
 
-    public void enqueue(Runnable runnable) {
-        int size = 0;
-        synchronized (this) {
-            tasks.add(runnable);
-            size = tasks.size();
-            persistTimer.triggerEventOccurred();
-        }
-        if (size > ResourceUtilities.getInteger(BackendTransformQueue.class,
-                "maxRunnables")) {
-            new Thread("backend-transform-queue-commit") {
-                @Override
-                public void run() {
-                    try {
-                        persistQueue();
-                    } catch (Throwable t) {
-                        t.printStackTrace();
-                    }
-                };
-            }.start();
-        }
-    }
+	public void enqueue(Runnable runnable) {
+		int size = 0;
+		synchronized (this) {
+			tasks.add(runnable);
+			size = tasks.size();
+			persistTimer.triggerEventOccurred();
+		}
+		if (size > ResourceUtilities.getInteger(BackendTransformQueue.class,
+				"maxRunnables")) {
+			new Thread("backend-transform-queue-commit") {
+				@Override
+				public void run() {
+					try {
+						persistQueue();
+					} catch (Throwable t) {
+						t.printStackTrace();
+					}
+				};
+			}.start();
+		}
+	}
 
-    private void persistQueue0() {
-        synchronized (persistMonitor) {
-            List<DomainTransformEvent> events = new ArrayList<>();
-            List<Runnable> toCommit = null;
-            synchronized (this) {
-                toCommit = tasks;
-                tasks = new ArrayList<>();
-            }
-            for (Runnable runnable : toCommit) {
-                ThreadlocalTransformManager.cast().resetTltm(null);
-                try {
-                    LooseContext.push();
-                    runnable.run();
-                    events.addAll(TransformManager.get().getTransforms());
-                    TransformManager.get().clearTransforms();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    LooseContext.pop();
-                }
-            }
-            ThreadlocalTransformManager.get().addTransforms(events, false);
-            if (events.size() > 0) {
-                logger.warn("(Backend queue)  - committing {} transforms",
-                        events.size());
-            }
-            try {
-                LooseContext.push();
-                ServletLayerTransforms
-                        .setPriority(TransformPriorityStd.Backend_admin);
-                Sx.commit();
-            } finally {
-                LooseContext.pop();
-            }
-        }
-    }
+	private void persistQueue0() {
+		synchronized (persistMonitor) {
+			List<DomainTransformEvent> events = new ArrayList<>();
+			List<Runnable> toCommit = null;
+			synchronized (this) {
+				toCommit = tasks;
+				tasks = new ArrayList<>();
+			}
+			for (Runnable runnable : toCommit) {
+				ThreadlocalTransformManager.cast().resetTltm(null);
+				try {
+					LooseContext.push();
+					runnable.run();
+					events.addAll(TransformManager.get().getTransforms());
+					TransformManager.get().clearTransforms();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					LooseContext.pop();
+				}
+			}
+			ThreadlocalTransformManager.get().addTransforms(events, false);
+			if (events.size() > 0) {
+				logger.warn("(Backend queue)  - committing {} transforms",
+						events.size());
+			}
+			try {
+				LooseContext.push();
+				ServletLayerTransforms
+						.setPriority(TransformPriorityStd.Backend_admin);
+				Sx.commit();
+			} finally {
+				LooseContext.pop();
+			}
+		}
+	}
 
-    void appShutdown() {
-        persistTimer.cancel();
-        persistQueue();
-    }
+	void appShutdown() {
+		persistTimer.cancel();
+		persistQueue();
+	}
 
-    void persistQueue() {
-        ThreadedPermissionsManager.cast()
-                .runWithPushedSystemUserIfNeeded(() -> {
-                    try {
-                        persistQueue0();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-    }
+	void persistQueue() {
+		ThreadedPermissionsManager.cast()
+				.runWithPushedSystemUserIfNeeded(() -> {
+					try {
+						persistQueue0();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+	}
 
-    void start() {
-        int loopDelay = ResourceUtilities
-                .getInteger(BackendTransformQueue.class, "loopDelay");
-        persistTimer = new AtEndOfEventSeriesTimer<>(loopDelay, new Runnable() {
-            @Override
-            public void run() {
-                persistQueue();
-            }
-        }).maxDelayFromFirstAction(loopDelay);
-    }
+	void start() {
+		int loopDelay = ResourceUtilities
+				.getInteger(BackendTransformQueue.class, "loopDelay");
+		persistTimer = new AtEndOfEventSeriesTimer<>(loopDelay, new Runnable() {
+			@Override
+			public void run() {
+				persistQueue();
+			}
+		}).maxDelayFromFirstAction(loopDelay);
+	}
 }
