f838438aab821095aba2d4789b579353f6e4c44c
Nick Reddel <nick.reddel@gmail.com>
2020-03-05 15:20:27 -0800

    Improve format util

--- framework/common/src/cc/alcina/framework/common/client/util/FormatBuilder.java
+++ framework/common/src/cc/alcina/framework/common/client/util/FormatBuilder.java
@@ -16,6 +16,12 @@ public class FormatBuilder {
 		sb.append(string);
 	}
 
+	public void appendIf(boolean test, String string) {
+		if (test) {
+			format(string);
+		}
+	}
+
 	public FormatBuilder appendIfBuilderEmpty(String optional) {
 		if (sb.length() == 0) {
 			sb.append(optional);
