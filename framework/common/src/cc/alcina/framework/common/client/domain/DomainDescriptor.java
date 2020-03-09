49ca61c652f27a6602cf9ba6df1a329e2e6c3415
Nick Reddel <nick.reddel@gmail.com>
2020-03-09 12:48:16 -0700

    Whitespace

--- framework/common/src/cc/alcina/framework/common/client/domain/DomainDescriptor.java
+++ framework/common/src/cc/alcina/framework/common/client/domain/DomainDescriptor.java
@@ -13,91 +13,91 @@ import cc.alcina.framework.common.client.logic.permissions.IUser;
 import cc.alcina.framework.common.client.util.CachingMap;
 
 public abstract class DomainDescriptor {
-    public Map<Class, DomainClassDescriptor<?>> perClass = new LinkedHashMap<>();
+	public Map<Class, DomainClassDescriptor<?>> perClass = new LinkedHashMap<>();
 
-    public List<DomainStoreTask> postLoadTasks = new ArrayList<DomainStoreTask>();
+	public List<DomainStoreTask> postLoadTasks = new ArrayList<DomainStoreTask>();
 
-    public List<PreProvideTask> preProvideTasks = new ArrayList<PreProvideTask>();
+	public List<PreProvideTask> preProvideTasks = new ArrayList<PreProvideTask>();
 
-    public List<ComplexFilter> complexFilters = new ArrayList<>();
+	public List<ComplexFilter> complexFilters = new ArrayList<>();
 
-    private CachingMap<Class, List<PreProvideTask>> perClassTasks = new CachingMap<Class, List<PreProvideTask>>(
-            clazz -> preProvideTasks.stream().filter(
-                    task -> task.forClazz() == null || task.forClazz() == clazz)
-                    .collect(Collectors.toList()));
+	private CachingMap<Class, List<PreProvideTask>> perClassTasks = new CachingMap<Class, List<PreProvideTask>>(
+			clazz -> preProvideTasks.stream().filter(
+					task -> task.forClazz() == null || task.forClazz() == clazz)
+					.collect(Collectors.toList()));
 
-    public DomainDescriptor() {
-    }
+	public DomainDescriptor() {
+	}
 
-    public <T extends HasIdAndLocalId> DomainClassDescriptor<T> addClassDescriptor(
-            Class<T> clazz, String... indexProperties) {
-        DomainClassDescriptor classDescriptor = new DomainClassDescriptor(clazz,
-                indexProperties);
-        addClassDescriptor(classDescriptor);
-        return classDescriptor;
-    }
+	public <T extends HasIdAndLocalId> DomainClassDescriptor<T>
+			addClassDescriptor(Class<T> clazz, String... indexProperties) {
+		DomainClassDescriptor classDescriptor = new DomainClassDescriptor(clazz,
+				indexProperties);
+		addClassDescriptor(classDescriptor);
+		return classDescriptor;
+	}
 
-    public void addClassDescriptor(DomainClassDescriptor classDescriptor) {
-        perClass.put(classDescriptor.clazz, classDescriptor);
-    }
+	public void addClassDescriptor(DomainClassDescriptor classDescriptor) {
+		perClass.put(classDescriptor.clazz, classDescriptor);
+	}
 
-    public void addClasses(Class[] classes) {
-        for (Class clazz : classes) {
-            perClass.put(clazz, new DomainClassDescriptor(clazz));
-        }
-    }
+	public void addClasses(Class[] classes) {
+		for (Class clazz : classes) {
+			perClass.put(clazz, new DomainClassDescriptor(clazz));
+		}
+	}
 
-    public void addComplexFilter(ComplexFilter complexFilter) {
-        complexFilters.add(complexFilter);
-    }
+	public void addComplexFilter(ComplexFilter complexFilter) {
+		complexFilters.add(complexFilter);
+	}
 
-    public boolean applyPostTransform(Class clazz, DomainTransformEvent o) {
-        return perClass.containsKey(clazz);
-    }
+	public boolean applyPostTransform(Class clazz, DomainTransformEvent o) {
+		return perClass.containsKey(clazz);
+	}
 
-    public boolean customFilterPostProcess(DomainTransformEvent dte) {
-        return true;
-    }
+	public boolean customFilterPostProcess(DomainTransformEvent dte) {
+		return true;
+	}
 
-    public abstract Class<? extends IUser> getIUserClass();
+	public abstract Class<? extends IUser> getIUserClass();
 
-    public synchronized <T> List<PreProvideTask<T>> getPreProvideTasks(
-            Class<T> clazz) {
-        return (List) perClassTasks.get(clazz);
-    }
+	public synchronized <T> List<PreProvideTask<T>>
+			getPreProvideTasks(Class<T> clazz) {
+		return (List) perClassTasks.get(clazz);
+	}
 
-    public boolean joinPropertyCached(Class clazz) {
-        return perClass.containsKey(clazz);
-    }
+	public boolean joinPropertyCached(Class clazz) {
+		return perClass.containsKey(clazz);
+	}
 
-    public void registerStore(IDomainStore domainStore) {
-        preProvideTasks.stream()
-                .forEach(task -> task.registerStore(domainStore));
-        postLoadTasks.stream().forEach(task -> task.registerStore(domainStore));
-    }
+	public void registerStore(IDomainStore domainStore) {
+		preProvideTasks.stream()
+				.forEach(task -> task.registerStore(domainStore));
+		postLoadTasks.stream().forEach(task -> task.registerStore(domainStore));
+	}
 
-    public static interface DomainStoreTask {
-        /**
-         * @return the lock object, if any
-         */
-        public void run() throws Exception;
+	public static interface DomainStoreTask {
+		/**
+		 * @return the lock object, if any
+		 */
+		public void run() throws Exception;
 
-        default void registerStore(IDomainStore domainStore) {
-        }
-    }
+		default void registerStore(IDomainStore domainStore) {
+		}
+	}
 
-    public static interface PreProvideTask<T> {
-        /**
-         * @return true if cached data was modified
-         */
-        public void run(Class clazz, Collection<T> objects, boolean topLevel)
-                throws Exception;
+	public static interface PreProvideTask<T> {
+		/**
+		 * @return true if cached data was modified
+		 */
+		public void run(Class clazz, Collection<T> objects, boolean topLevel)
+				throws Exception;
 
-        public void writeLockedCleanup();
+		public void writeLockedCleanup();
 
-        Class<T> forClazz();
+		Class<T> forClazz();
 
-        default void registerStore(IDomainStore domainStore) {
-        }
-    }
+		default void registerStore(IDomainStore domainStore) {
+		}
+	}
 }
