a275e4be340482565bf0d0d79bec37eb9f8b0fbf
Nick Reddel <nick.reddel@gmail.com>
2020-06-01 06:56:48 -0700

    Improve docs

--- framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/mvcc/package-info.java
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/mvcc/package-info.java
@@ -1,12 +1,37 @@
 /**
+ * 
  * <h2>Transactional object</h2>
  * <ul>
  * <li>Every object implements MvccObject
- * <li>Every object has an MvccObjectTransactions field (accessible via the
+ * <li>Every object has an MvccObjectVersions getter/setter (accessible via the
  * interface)
  * </ul>
  * 
- * <h2>Transaction application</h2>
+ * 
+ * <h2>MvccObject identity and debugging gotchas</h2>
+ * <ul>
+ * <li>An mvccobject (transactional entity) has essentially two roles: a store
+ * of field values, and a 'domain identity'
+ * <li>The 'domain identity' object is always the first instance of a given
+ * entity (class/id or class/localid tuple) visible to the domain, and remains
+ * the same object for the lifetime of the domain (i.e webapp or jvm). Even when
+ * an entity is persisted to the db and assigned an id value, this relationship
+ * holds (so there's no need to call entity.domain().domainVersion() after
+ * persisting).
+ * <li>If an entity has not been changed in any unvacuumed transaction,
+ * MvccObjectVersions will be null and the property values returned from
+ * getters/setters will be the field values of the 'domain identity' object.
+ * <li>If an entity _has_ been changed, all visible methods will route to the
+ * appropriate versioned instance ( which acts as a container of field values
+ * for the transaction). There are some wrinkles to this logic (@see
+ * MvccAccessType) but not for getters/setters.
+ * <li>TLDR; For debugging, to view the field values of an object you're writing
+ * to (if it has a non-zero id value), look at:
+ * entity.__mvccObjectVersions__.__mostRecentWritable
+ * </ul>
+ * 
+ * 
+ * <h2>Transaction application (db tx -> domain store) (Nick's thoughts)</h2>
  * <ul>
  * <li>Transactions application is sequential, in db order
  * <li>Strategies to reduce application times...?
@@ -23,92 +48,6 @@
  * <li>And then of course there's ... eventual consistency
  * </ul>
  * 
- * <h2>Notes/think</h2>
- * <ul>
- * <li>Get rid of baselayerlist (transactionalmap)(special case the code)
- * <li>Use baseobject more in mvccversions (want to eventually have no ref to
- * base transaction)
- * <li>(Pre-jade) Cherry-pick memory usage to alcina.master. Compare memory
- * usage of a segment using propertystore vs regular objects for citaions/ccs.
- * Make framework-level 'populate DomainStoreProperty' - fingers crossed re
- * propertystore abandonment
- * </ul>
  * 
- * <h2>Versions</h2>
- * <h3>0.1</h3>
- * <h4>Goals</h4>
- * <ul>
- * <li>Have transactionally independent field values for all domain objects
- * <li>Have 'one unique object' for the lifetime of the vm - including when
- * promoted from 'local' to 'domain'
- * <li>Have relatively proveable zero-information leakage of transactional
- * version identity data (i.e. the unique-to-tx entity objects do not leak this
- * - and thereby hashcode/equals - outside their own code)
- * </ul>
- * <h4>Nice to have</h4>
- * <ul>
- * <li>Minimal memory use increase - or even lower it
- * <li>More performant transform request communication for cache change
- * propogation
- * 
- * </ul>
- * <h4>Non-goals</h4>
- * <ul>
- * <li>Rework the logging system
- * <li>Rework the jobs system
- * <li>Refactor DomainStore more than needed
- * </ul>
- * <h2>TODO</h2>
- * <ul>
- * <li>Facade class code creation (yep)
- * <li>Facade object creation (domain.create => mvcc.create) (yep)
- * <li>MvMaps (domaincache; indicies)(yep)
- * <li>Transaction phase management (local; remote)(vacuum) (yap)
- * <p>
- * This is actually easyish - a local transaction can be aborted (unsupp for the
- * mo) - a db-committed
- * </p>
- * <li>Graph projection
- * <li>Remove locks (make em just notational)(altho post-process remains
- * synchronized)
- * <li>Post-process and transaction cleanup. Delete cascade can be a lot less
- * brutal (just associations)
- * <li>Check no field assignment within entity private methods, and no calls to
- * setters (pretty sure checked as part of bytecode generation)
- * <li>Tx abort - check hits vacuum etc
- * </ul>
- * <h2>Vacuum</h2>
- * <ul>
- * <li>MvccObject - just remove invisible versions
- * <li>Transactional map - make new, squash, hot swap (10x 'levelled compaction'
- * scaling down)
- * </ul>
- * *
- * <h2>Tests (1)</h2>
- * <ul>
- * <li>Does the transactionalmap/layer/iterator work as intended? Any possible
- * multithreaded races??
- * <li>Does post-process work (devconsole) - do we get new obj versions derived
- * from correct version
- * <li>Does post-process work (devconsole) - ditto lookups
- * <li>Does post-process work (devconsole) - modify an object in two
- * transactions
- * <li>Do UI tree and collection UI selectors work?
- * </ul>
- * <h2>TODO - post</h2>
- * <ul>
- * <li>Should transactionid be used more?
- * <li>Divide transaction phases into categories
- * <li>fix non-safety of method comparisonString() (by removing - this
- * comparator shd be client only - have a real, caching comparator)
- * <li>Associations! General plan!
- * <li>Associations! General plan! (welll...we have one. write without
- * transforms, both server and client side, on post_process and on transform
- * receipt)
- * <li>Clone code creation
- * <li>Vacuum
- * <li>DirectAccess annotation should have a hash (of the method source)
- * <li>
- * </ul>
  */
 package cc.alcina.framework.entity.entityaccess.cache.mvcc;
