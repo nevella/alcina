d5b18f7bdf1aff5b33ab30f532fa7f1da37af3ba
Nick Reddel <nick.reddel@gmail.com>
2020-06-09 16:54:22 -0700

    Work on copy source task

--- extras/cluster/tasks-src/cc/alcina/extras/cluster/CopyEx.java
+++ extras/cluster/tasks-src/cc/alcina/extras/cluster/CopyEx.java
@@ -31,16 +31,24 @@ public class CopyEx extends Copy {
 	@Override
 	public void execute() throws BuildException {
 		setOverwrite(true);
+		setIncludeEmptyDirs(false);
 		super.execute();
 	}
 
 	@Override
 	protected void buildMap(File fromDir, File toDir, String[] names,
 			FileNameMapper mapper, Hashtable map) {
+		Map<String, FileSet> uniqueOrderedFilesets = new LinkedHashMap<>();
+		orderedFileSets.forEach(ofs -> uniqueOrderedFilesets
+				.put(getCanonicalPath(ofs.getDir()), ofs));
+		orderedFileSets
+				.removeIf(ofs -> !uniqueOrderedFilesets.containsValue(ofs));
 		{
 			BuildMapParams params = new BuildMapParams(fromDir, toDir, names,
 					mapper, map);
 			buildMapParamsList.add(params);
+			log(String.format("build: %s", getCanonicalPath(fromDir)),
+					Project.MSG_INFO);
 		}
 		if (buildMapParamsList.size() == orderedFileSets.size()) {
 			Map<String, String> toSrc = new LinkedHashMap<>();
@@ -54,52 +62,56 @@ public class CopyEx extends Copy {
 							.map(p -> getCanonicalPath(p.fromDir))
 							.collect(Collectors.joining("\n\t"))),
 					Project.MSG_DEBUG);
+			Map<String, BuildMapParams> pathToParams = buildMapParamsList
+					.stream()
+					.collect(Collectors.toMap(p -> getCanonicalPath(p.fromDir),
+							p -> p, (a, b) -> a));
+			// run in order, last overrides
 			for (int idx = orderedFileSets.size() - 1; idx >= 0; idx--) {
 				FileSet fileset = orderedFileSets.get(idx);
-				log(String.format("[--] : %s : %s", idx,
-						getCanonicalPath(fileset.getDir())), Project.MSG_DEBUG);
-				// run in order, last overrides
-				for (BuildMapParams params : buildMapParamsList) {
-					if (getCanonicalPath(params.fromDir)
-							.equals(getCanonicalPath(fileset.getDir()))) {
-						log(String.format("[--] : %s : %s", idx,
-								params.fromDir), Project.MSG_DEBUG);
-						Hashtable<String, String[]> typedMap = map;
-						// checks for difference (not just newer-than). if
-						// there
-						// are multiple
-						// matches, uses only the first (note, iterating in
-						// reversed order over ordered file sets).
-						fromDir = params.fromDir;
-						toDir = params.toDir;
-						names = params.names;
-						mapper = params.mapper;
-						map = params.map;
-						for (int i = 0; i < names.length; i++) {
-							String name = names[i];
-							if (mapper.mapFileName(name) != null) {
-								final File src = new File(fromDir, name);
-								final String[] mappedFiles = mapper
-										.mapFileName(name);
-								File toFile = new File(toDir, mappedFiles[0]);
-								String toPath = toFile.getAbsolutePath();
-								String srcPath = src.getAbsolutePath();
-								log("[0] " + srcPath, Project.MSG_DEBUG);
-								boolean unchanged = toFile.exists()
-										&& Math.abs(src.lastModified()
-												- toFile.lastModified()) < 100
-										&& src.length() == toFile.length();
-								if (toSrc.containsKey(toPath)) {
-									log("[-] " + srcPath, Project.MSG_DEBUG);
-								} else {
-									toSrc.put(toPath, srcPath);
-									if (!unchanged) {
-										log("[+] " + srcPath,
-												Project.MSG_DEBUG);
-										map.put(srcPath, new String[] {
-												toFile.getAbsolutePath() });
-									}
-								}
+				String filesetDir = getCanonicalPath(fileset.getDir());
+				log(String.format("[--] : %s : %s", idx, filesetDir),
+						Project.MSG_DEBUG);
+				BuildMapParams params = pathToParams.get(filesetDir);
+				if (params == null) {
+					log(String.format("[--] : no buildMapParams - %s",
+							filesetDir), Project.MSG_WARN);
+					continue;
+				}
+				log(String.format("[--] : %s : %s", idx, params.fromDir),
+						Project.MSG_DEBUG);
+				Hashtable<String, String[]> typedMap = map;
+				// checks for difference (not just newer-than). if
+				// there
+				// are multiple
+				// matches, uses only the first (note, iterating in
+				// reversed order over ordered file sets).
+				fromDir = params.fromDir;
+				toDir = params.toDir;
+				names = params.names;
+				mapper = params.mapper;
+				map = params.map;
+				for (int i = 0; i < names.length; i++) {
+					String name = names[i];
+					if (mapper.mapFileName(name) != null) {
+						final File src = new File(fromDir, name);
+						final String[] mappedFiles = mapper.mapFileName(name);
+						File toFile = new File(toDir, mappedFiles[0]);
+						String toPath = toFile.getAbsolutePath();
+						String srcPath = src.getAbsolutePath();
+						log("[0] " + srcPath, Project.MSG_DEBUG);
+						boolean unchanged = toFile.exists()
+								&& Math.abs(src.lastModified()
+										- toFile.lastModified()) < 100
+								&& src.length() == toFile.length();
+						if (toSrc.containsKey(toPath)) {
+							log("[-] " + srcPath, Project.MSG_DEBUG);
+						} else {
+							toSrc.put(toPath, srcPath);
+							if (!unchanged) {
+								log("[+] " + srcPath, Project.MSG_DEBUG);
+								map.put(srcPath, new String[] {
+										toFile.getAbsolutePath() });
 							}
 						}
 					}
