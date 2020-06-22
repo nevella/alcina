23660942952b5b73fa9f204fade622286251c9f6
Jumail Mundekkat <jmundekkat@barnet.com.au>
2020-06-22 14:38:09 +1000

    Add long comparison search tools

--- framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/packs/BaseLongComparatorCriterionPack.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/objecttree/search/packs/BaseLongComparatorCriterionPack.java
@@ -9,31 +9,32 @@ import com.totsp.gwittir.client.validator.Validator;
 
 import cc.alcina.framework.common.client.collections.CollectionFilter;
 import cc.alcina.framework.common.client.domain.DomainFilter;
-import cc.alcina.framework.common.client.gwittir.validator.DoubleValidator;
+import cc.alcina.framework.common.client.gwittir.validator.LongValidator;
 import cc.alcina.framework.common.client.search.DoubleCriterion;
+import cc.alcina.framework.common.client.search.LongCriterion;
 import cc.alcina.framework.gwt.client.gwittir.widget.TextBox;
 import cc.alcina.framework.gwt.client.objecttree.search.FlatSearchable;
 import cc.alcina.framework.gwt.client.objecttree.search.StandardSearchOperator;
 
-public class BaseDoubleCriterionPack {
-	public interface BaseDoubleCriterionHandler<T> extends Function<T, Double> {
+public class BaseLongComparatorCriterionPack {
+	public interface BaseLongComparatorCriterionHandler<T> extends Function<T, Long> {
 		@Override
-		public Double apply(T t);
+		public Long apply(T t);
 
-		default DomainFilter getFilter0(DoubleCriterion sc) {
-			Double value = sc.getDouble();
+		default DomainFilter getFilter0(LongCriterion sc) {
+			Long value = sc.getLong();
 			if (value == null) {
 				return null;
 			}
-			double p_value = value;
+			long p_value = value;
 			return new DomainFilter(new CollectionFilter<T>() {
 				@Override
 				public boolean allow(T o) {
-					Double d = apply(o);
+					Long d = apply(o);
 					if (d == null) {
 						return false;
 					}
-					double p_d = d;
+					long p_d = d;
 					switch (sc.getOperator()) {
 					case GREATER_THAN:
 						return p_d > p_value;
@@ -51,9 +52,9 @@ public class BaseDoubleCriterionPack {
 		}
 	}
 
-	public static abstract class BaseDoubleCriterionSearchable<T extends DoubleCriterion>
+	public static abstract class BaseLongComparatorCriterionSearchable<T extends LongCriterion>
 			extends FlatSearchable<T> {
-		public BaseDoubleCriterionSearchable(Class<T> clazz, String objectName,
+		public BaseLongComparatorCriterionSearchable(Class<T> clazz, String objectName,
 				String fieldName) {
 			super(clazz, objectName, fieldName,
 					Arrays.asList(StandardSearchOperator.EQUALS,
@@ -74,35 +75,35 @@ public class BaseDoubleCriterionPack {
 
 		@Override
 		public Validator getValidator() {
-			return new DoubleValidator();
+			return new LongValidator();
 		}
 
 		@Override
-		public boolean hasValue(DoubleCriterion sc) {
-			return sc.getDouble() != null;
+		public boolean hasValue(LongCriterion sc) {
+			return sc.getLong() != null;
 		}
 	}
 
-	public interface BaseDoubleStreamCriterionHandler<T>
-			extends Function<T, Stream<Double>> {
+	public interface BaseLongStreamCriterionHandler<T>
+			extends Function<T, Stream<Long>> {
 		@Override
-		public Stream<Double> apply(T t);
+		public Stream<Long> apply(T t);
 
-		default DomainFilter getFilter0(DoubleCriterion sc) {
-			Double value = sc.getDouble();
+		default DomainFilter getFilter0(LongCriterion sc) {
+			Long value = sc.getLong();
 			if (value == null) {
 				return null;
 			}
-			double p_value = value;
+			long p_value = value;
 			return new DomainFilter(new CollectionFilter<T>() {
 				@Override
 				public boolean allow(T o) {
-					Stream<Double> stream = apply(o);
+					Stream<Long> stream = apply(o);
 					if (stream == null) {
 						return false;
 					}
 					return stream.anyMatch(d -> {
-						double p_d = d;
+						long p_d = d;
 						switch (sc.getOperator()) {
 						case GREATER_THAN:
 							return p_d > p_value;
