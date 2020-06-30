d131be41e1e3bf626862b1a0c8eb00af1ae0130e
Nick Reddel <nick.reddel@gmail.com>
2020-06-30 15:23:19 -0700

    Restore jvmclient utils

--- framework/gwt/src/cc/alcina/framework/gwt/client/util/ClientUtilsNonGwt.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/util/ClientUtilsNonGwt.java
@@ -3,9 +3,6 @@ package cc.alcina.framework.gwt.client.util;
 import com.google.gwt.user.client.rpc.StatusCodeException;
 
 import cc.alcina.framework.common.client.WrappedRuntimeException;
-import cc.alcina.framework.common.client.logic.reflection.ClientInstantiable;
-import cc.alcina.framework.common.client.logic.reflection.RegistryLocation;
-import cc.alcina.framework.common.client.logic.reflection.RegistryLocation.ImplementationType;
 import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
 import cc.alcina.framework.gwt.client.ClientNotifications;
 import cc.alcina.framework.gwt.client.browsermod.BrowserMod;
@@ -13,47 +10,41 @@ import cc.alcina.framework.gwt.client.logic.AlcinaDebugIds;
 
 //FIXME - mvcc.4 - jvmclient - post merge of localdom - remove (with prejudice)
 public class ClientUtilsNonGwt {
-	@RegistryLocation(registryPoint = ClientUtilsNonGwtImpl.class, implementationType = ImplementationType.SINGLETON, priority = RegistryLocation.DEFAULT_PRIORITY)
-	@ClientInstantiable
-	public static class ClientUtilsNonGwtImpl {
-		public boolean maybeOffline(Throwable t) {
-			while (t instanceof WrappedRuntimeException) {
-				if (t == t.getCause() || t.getCause() == null) {
-					break;
-				}
-				t = t.getCause();
+	public static boolean maybeOffline(Throwable t) {
+		while (t instanceof WrappedRuntimeException) {
+			if (t == t.getCause() || t.getCause() == null) {
+				break;
 			}
-			if (t.getMessage() != null && t.getMessage()
-					.contains("IOException while sending RPC request")) {
-				return true;
-			}
-			if (t.getMessage() != null && t.getMessage()
-					.contains("IOException while receiving RPC response")) {
-				return true;
-			}
-			if (t instanceof StatusCodeException) {
-				if (AlcinaDebugIds
-						.hasFlag(AlcinaDebugIds.DEBUG_SIMULATE_OFFLINE)) {
-					return true;
-				}
-				StatusCodeException sce = (StatusCodeException) t;
-				Registry.impl(ClientNotifications.class).log(
-						"** Status code exception: " + sce.getStatusCode());
-				if (sce.getStatusCode() == 0) {
-					return true;
-				}
-				boolean internetExplorerErrOffline = BrowserMod
-						.isInternetExplorer() && sce.getStatusCode() > 600;
-				if (internetExplorerErrOffline) {
-					return true;
-				}
-				// DNS error in Africa
-				if (t.toString()
-						.contains("was not able to resolve the hostname")) {
-					return true;
-				}
-			}
-			return false;
+			t = t.getCause();
 		}
+		if (t.getMessage() != null && t.getMessage()
+				.contains("IOException while sending RPC request")) {
+			return true;
+		}
+		if (t.getMessage() != null && t.getMessage()
+				.contains("IOException while receiving RPC response")) {
+			return true;
+		}
+		if (t instanceof StatusCodeException) {
+			if (AlcinaDebugIds.hasFlag(AlcinaDebugIds.DEBUG_SIMULATE_OFFLINE)) {
+				return true;
+			}
+			StatusCodeException sce = (StatusCodeException) t;
+			Registry.impl(ClientNotifications.class)
+					.log("** Status code exception: " + sce.getStatusCode());
+			if (sce.getStatusCode() == 0) {
+				return true;
+			}
+			boolean internetExplorerErrOffline = BrowserMod.isInternetExplorer()
+					&& sce.getStatusCode() > 600;
+			if (internetExplorerErrOffline) {
+				return true;
+			}
+			// DNS error in Africa
+			if (t.toString().contains("was not able to resolve the hostname")) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
