c9ef994f550af1025e9641149a475e9987d19e1c
Nick Reddel <nick.reddel@gmail.com>
2020-07-31 23:42:44 -0700

    ...and back

--- framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/layout/MockupNodeRenderer.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/layout/MockupNodeRenderer.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
+import cc.alcina.framework.common.client.util.Ax;
 import cc.alcina.framework.gwt.client.dirndl.layout.DirectedLayout.Node;
 
 public class MockupNodeRenderer implements DirectedNodeRenderer{
 	@Override
 	public Optional<Widget> render(Node node) {
-		Label label = new Label(node.model.getClass().getSimpleName());
+		Label label = new Label(Ax.format("%s :: %s",node.model.getClass().getSimpleName(),node.directed.cssClass()));
 		label.setStyleName("");
 		NodeRendererStyle.MOCKUP_NODE.addTo(label);
 		return Optional.of(label);
