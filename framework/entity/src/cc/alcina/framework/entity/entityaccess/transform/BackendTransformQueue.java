6595adf52d89b6afde99817ee1816aac1e560ffb
Nick Reddel <nick.reddel@gmail.com>
2020-08-06 13:59:29 -0700

    Fix threading issue with BackendTransformQueue

--- framework/entity/src/cc/alcina/framework/entity/entityaccess/transform/BackendTransformQueue.java
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/transform/BackendTransformQueue.java
@@ -2,6 +2,7 @@ package cc.alcina.framework.entity.entityaccess.transform;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +17,27 @@ import cc.alcina.framework.entity.entityaccess.transform.TransformCommit.Transfo
 import cc.alcina.framework.entity.util.AlcinaChildRunnable;
 import cc.alcina.framework.gwt.client.util.AtEndOfEventSeriesTimer;
 
+/*
+ * FIXME - mvcc.4 - the threading is overly complex - or could be abstracted out maybe? With a task queue?
+ * 
+ * Actually we want an 'acceptor' thread that runs tasks as they come in (if not persisting), persists to disk, and commits after the delay
+ * 
+ *  It should run for the app lifetime (other cases of AtEndOfEventSeriesTimer may be misapplied too)
+ * 
+ * FIXME - mvcc.5 - backup (fs?) persistence of transforms? (get em via scp on fail) (why aren't the runnables run on the originating thread?)
+ */
 class BackendTransformQueue {
 	private AtEndOfEventSeriesTimer persistTimer;
 
 	List<Runnable> tasks = new ArrayList<>();
 
-	Object persistMonitor = new Object();
-
 	Logger logger = LoggerFactory.getLogger(getClass());
+
+	AtomicBoolean persisting = new AtomicBoolean(false);
+
+	AtomicBoolean persistAtEndOfCurrentTask = new AtomicBoolean(false);
+
+	private long lastPersistStarted;
 
 	public void enqueue(Runnable runnable) {
 		int size = 0;
@@ -32,20 +46,22 @@ class BackendTransformQueue {
 			size = tasks.size();
 			persistTimer.triggerEventOccurred();
 		}
-		if (size > ResourceUtilities.getInteger(BackendTransformQueue.class,
-				"maxRunnables")) {
-			persistQueue();
-			Transaction.ensureBegun();
-		}
 	}
 
 	private void persistQueue0() {
-		synchronized (persistMonitor) {
+		boolean doPersist = true;
+		while (doPersist) {
+			lastPersistStarted = System.currentTimeMillis();
 			List<DomainTransformEvent> events = new ArrayList<>();
 			List<Runnable> toCommit = null;
 			synchronized (this) {
 				toCommit = tasks;
 				tasks = new ArrayList<>();
+				if (toCommit.size() == 0) {
+					synchronized (persisting) {
+						persisting.set(false);
+					}
+				}
 			}
 			for (Runnable runnable : toCommit) {
 				if (runnable instanceof AlcinaChildRunnable) {
@@ -76,17 +92,41 @@ class BackendTransformQueue {
 			} finally {
 				LooseContext.pop();
 			}
+			doPersist = false;
+			synchronized (persisting) {
+				if (persistAtEndOfCurrentTask.get()) {
+					doPersist = true;
+					persistAtEndOfCurrentTask.set(false);
+				}
+			}
 		}
+		persisting.set(false);
 	}
 
 	void appShutdown() {
 		persistTimer.cancel();
+		// force persist attempt, even if in-flight
+		persisting.set(false);
 		persistQueue();
 	}
 
 	void persistQueue() {
-		AlcinaChildRunnable.runInTransaction("backend-transform-persist",
-				() -> persistQueue0(), true, false);
+		boolean doPersist = false;
+		synchronized (persisting) {
+			if (!persisting.get()) {
+				persisting.set(true);
+				doPersist = true;
+			} else {
+				logger.info(
+						"Not persisting - there's an inflight persistence job started {} ms ago",
+						System.currentTimeMillis() - lastPersistStarted);
+				persistAtEndOfCurrentTask.set(true);
+			}
+		}
+		if (doPersist) {
+			AlcinaChildRunnable.runInTransaction("backend-transform-persist",
+					() -> persistQueue0(), true, false);
+		}
 	}
 
 	void start() {
