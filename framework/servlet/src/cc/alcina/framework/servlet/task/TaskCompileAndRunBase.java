90d802a960f50ad3909c3b892dd1c804ec50303b
Nick Reddel <nick.reddel@gmail.com>
2020-09-25 14:03:22 -0700

    Add dev code runner

--- framework/servlet/src/cc/alcina/framework/servlet/task/TaskCompileAndRunBase.java
+++ framework/servlet/src/cc/alcina/framework/servlet/task/TaskCompileAndRunBase.java
@@ -3,11 +3,13 @@ package cc.alcina.framework.servlet.task;
 import cc.alcina.framework.servlet.actionhandlers.AbstractTaskPerformer;
 import net.openhft.compiler.CompilerUtils;
 
-public class TaskCompileAndRun extends AbstractTaskPerformer {
+public abstract class TaskCompileAndRunBase extends AbstractTaskPerformer {
+	protected abstract String getCompiledClassName();
+
 	@Override
 	protected void run0() throws Exception {
 		String javaCode = value;
-		String className = "cc.alcina.framework.servlet.task.TaskCompileAndRunLocal";
+		String className = getCompiledClassName();
 		Class clazz = CompilerUtils.CACHED_COMPILER.loadFromJava(className,
 				javaCode);
 		AbstractTaskPerformer performer = (AbstractTaskPerformer) clazz
