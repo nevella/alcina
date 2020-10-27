59eb1c93805a573f7274798ba3f0dac7e9b12f97
Nick Reddel <nick.reddel@gmail.com>
2020-10-27 06:11:11 -0700

    Work on segmentloader/mvcc

--- framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/DomainSegmentLoader.java
+++ framework/entity/src/cc/alcina/framework/entity/entityaccess/cache/DomainSegmentLoader.java
@@ -14,6 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cc.alcina.framework.common.client.logic.domain.Entity;
+import cc.alcina.framework.common.client.util.Ax;
+import cc.alcina.framework.common.client.util.CommonUtils;
 import cc.alcina.framework.common.client.util.MultikeyMap;
 import cc.alcina.framework.common.client.util.Multiset;
 import cc.alcina.framework.common.client.util.UnsortedMultikeyMap;
@@ -25,6 +27,12 @@ import cc.alcina.framework.entity.entityaccess.cache.DomainStoreLoaderDatabase.L
 import cc.alcina.framework.entity.projection.GraphProjection;
 import cc.alcina.framework.entity.util.SimpleAtomModel.AtomKey;
 
+/**
+ * TODO - this isn't as well-adapted to mvcc as it could be (see e.g. the
+ * parallelisation of LaterItem.resolve by segmentRefs to handle resolution in
+ * different phases) - but....it works. Fix would involve making notifyLater()
+ * be sensitive to phase
+ */
 public abstract class DomainSegmentLoader implements ConnResultsReuse {
 	List<LaterItem> toResolve = new ArrayList<>();
 
@@ -73,10 +81,12 @@ public abstract class DomainSegmentLoader implements ConnResultsReuse {
 				savedResults = new ArrayList<>();
 				connResults.cachedValues = savedResults;
 				savedRsResults.put(key, savedResults);
-				DomainStore.stores().writableStore().sqlLogger.debug("{}: {}",
+				DomainStore.stores().writableStore().sqlLogger.debug(
+						"{}: [{}] - {}",
 						connResults.clazz == null ? "(null)"
 								: connResults.clazz.getSimpleName(),
-						connResults.sqlFilter);
+						connResults.sqlFilter.split(",").length,
+						CommonUtils.trimToWsChars(connResults.sqlFilter, 1000));
 				return itr;
 			} else {
 				return savedResults.iterator();
@@ -116,10 +126,14 @@ public abstract class DomainSegmentLoader implements ConnResultsReuse {
 		if (properties.stream().anyMatch(property -> property.isIgnore(item))) {
 			return;
 		}
+		if (properties.stream()
+				.anyMatch(property -> property.isRefsOnly(item))) {
+			segmentRefs.put(((Entity) item.source).entityClass(),
+					item.pdOperator.name, item.source.getId(), id);
+			return;
+		}
 		toResolve.add(item);
 		toLoadIds.add(type, id);
-		segmentRefs.put(((Entity) item.source).entityClass(),
-				item.pdOperator.name, item.source.getId(), id);
 	}
 
 	@Override
@@ -150,18 +164,21 @@ public abstract class DomainSegmentLoader implements ConnResultsReuse {
 		return loadedInPhase.get(clazz, id) == phase;
 	}
 
-	protected void addProperty(Class<? extends Entity> source,
-			String propertyName, Class<? extends Entity> target,
-			DomainSegmentPropertyType type) {
-		addProperty(source, propertyName, target, type,
+	protected DomainSegmentLoaderProperty addProperty(
+			Class<? extends Entity> source, String propertyName,
+			Class<? extends Entity> target, DomainSegmentPropertyType type) {
+		return addProperty(source, propertyName, target, type,
 				DomainSegmentLoaderPhase.ALL_PHASES);
 	}
 
-	protected void addProperty(Class<? extends Entity> source,
-			String propertyName, Class<? extends Entity> target,
-			DomainSegmentPropertyType type, DomainSegmentLoaderPhase phase) {
-		properties.add(new DomainSegmentLoaderProperty(source, propertyName,
-				target, type, phase));
+	protected DomainSegmentLoaderProperty addProperty(
+			Class<? extends Entity> source, String propertyName,
+			Class<? extends Entity> target, DomainSegmentPropertyType type,
+			DomainSegmentLoaderPhase phase) {
+		DomainSegmentLoaderProperty property = new DomainSegmentLoaderProperty(
+				source, propertyName, target, type, phase);
+		properties.add(property);
+		return property;
 	}
 
 	protected void clearCache() {
@@ -212,6 +229,8 @@ public abstract class DomainSegmentLoader implements ConnResultsReuse {
 
 		private DomainSegmentLoaderPhase phase;
 
+		private String columnName1;
+
 		public DomainSegmentLoaderProperty(Class<? extends Entity> source,
 				String propertyName, Class<? extends Entity> target,
 				DomainSegmentPropertyType type,
@@ -223,10 +242,14 @@ public abstract class DomainSegmentLoader implements ConnResultsReuse {
 			this.phase = phase;
 		}
 
+		public String columnName1() {
+			return Ax.blankTo(columnName1, propertyName1 + "_id");
+		}
+
 		public boolean isIgnore(LaterItem item) {
 			if (type == DomainSegmentPropertyType.IGNORE) {
 				if (item.pdOperator.pd.getName().equals(propertyName1)
-						&& item.source.getClass() == clazz1) {
+						&& ((Entity) item.source).entityClass() == clazz1) {
 					return true;
 				}
 			}
@@ -235,20 +258,33 @@ public abstract class DomainSegmentLoader implements ConnResultsReuse {
 
 		public boolean isIgnoreForPhase(DomainSegmentLoaderPhase forPhase) {
 			if (this.phase == null) {
-				return false;
+				return type == DomainSegmentPropertyType.IGNORE;
 			}
 			return this.phase.isIgnoreForPhase(forPhase);
+		}
+
+		public boolean isRefsOnly(LaterItem item) {
+			if (item.pdOperator.pd.getName().equals(propertyName1)
+					&& ((Entity) item.source).entityClass() == clazz1) {
+				return true;
+			}
+			return false;
 		}
 
 		@Override
 		public String toString() {
 			return GraphProjection.fieldwiseToStringOneLine(this);
 		}
+
+		public DomainSegmentLoaderProperty withColumnName1(String columnName1) {
+			this.columnName1 = columnName1;
+			return this;
+		}
 	}
 
 	public enum DomainSegmentPropertyType {
-		TABLE_REF_CLAZZ_1_RSCOL_REFS_CLAZZ_2,
-		STORE_REF_CLAZZ_1_PROP_EQ_CLAZZ_2_ID_LOAD_CLAZZ_2, IGNORE
+		CLAZZ_1_RSCOL_REFS_CLAZZ_2, CLAZZ_1_PROP_EQ_CLAZZ_2_ID_LOAD_CLAZZ_2,
+		IGNORE
 	}
 
 	public static class SavedSegmentDataHolder {
