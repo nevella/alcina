1de7861c212312a4f28bb0516a68a0f940ab0e13
Nick Reddel <nick.reddel@gmail.com>
2020-11-19 12:26:45 -0800

    Add generalised external repository support

---
+++ framework/common/src/cc/alcina/framework/common/client/publication/RepositoryDelivery.java
@@ -0,0 +1,6 @@
+package cc.alcina.framework.common.client.publication;
+
+public class RepositoryDelivery {
+	public static final transient String PUBLICATION_PROPERTY_REPOSITORY_CLASS = RepositoryDelivery.class.getName()+".PUBLICATION_PROPERTY_REPOSITORY_CLASS";
+	
+}
