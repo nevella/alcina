f5f2d924fbd429966d0f7ac07c7cad6d6a1e6149
Nick Reddel <nick.reddel@gmail.com>
2020-12-01 19:35:07 -0800

    Add bytearray ser/deser

--- framework/entity/src/cc/alcina/framework/entity/util/AlcinaBeanSerializerS.java
+++ framework/entity/src/cc/alcina/framework/entity/util/AlcinaBeanSerializerS.java
@@ -38,6 +38,7 @@ import cc.alcina.framework.common.client.util.CommonUtils;
 import cc.alcina.framework.common.client.util.CountingMap;
 import cc.alcina.framework.common.client.util.Multimap;
 import cc.alcina.framework.entity.SEUtilities;
+import cc.alcina.framework.entity.gen.Base64;
 
 @RegistryLocation(registryPoint = AlcinaBeanSerializer.class, implementationType = ImplementationType.INSTANCE, priority = 15)
 @ClientInstantiable
@@ -122,6 +123,9 @@ public class AlcinaBeanSerializerS extends AlcinaBeanSerializer {
 		}
 		if (type == Class.class) {
 			return cl.loadClass(o.toString());
+		}
+		if (type.isArray() && type.getComponentType() == byte.class) {
+			return Base64.decode(o.toString());
 		}
 		Collection c = null;
 		if (type == Set.class || type == LinkedHashSet.class) {
@@ -249,6 +253,9 @@ public class AlcinaBeanSerializerS extends AlcinaBeanSerializer {
 		}
 		if (type == Date.class) {
 			return (String.valueOf(((Date) value).getTime()));
+		}
+		if (type.isArray() && type.getComponentType() == byte.class) {
+			return Base64.encodeBytes((byte[]) value);
 		}
 		if (value instanceof Multimap) {
 			Multimap m = (Multimap) value;
