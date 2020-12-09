eecaae58af0e9b0a951b8d3b1828e0e3b093ba61
Nick Reddel <nick.reddel@gmail.com>
2020-12-09 15:06:39 -0800

    Guard against exceptions in notification handlers

--- framework/entity/src/cc/alcina/framework/entity/transform/ThreadlocalTransformManager.java
+++ framework/entity/src/cc/alcina/framework/entity/transform/ThreadlocalTransformManager.java
@@ -1147,7 +1147,12 @@ public class ThreadlocalTransformManager extends TransformManager
 					"**WARNING ** TLTM - cleared (but still pending) transforms:\n "
 							+ pendingTransforms);
 			Thread.dumpStack();
-			AlcinaTopics.notifyDevWarning(new UncomittedTransformsException());
+			try {
+				AlcinaTopics
+						.notifyDevWarning(new UncomittedTransformsException());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		clearTransforms();
 		addDomainTransformListener(new ServerTransformListener());
@@ -1155,7 +1160,11 @@ public class ThreadlocalTransformManager extends TransformManager
 			addDomainTransformListener(listener);
 		}
 		if (initialised) {
-			topicTransformManagerWasReset().publish(Thread.currentThread());
+			try {
+				topicTransformManagerWasReset().publish(Thread.currentThread());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else {
 			initialised = true;
 		}
