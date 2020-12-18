ed5de82791f6633c60e6a729e0426e1213401cc1
Nick Reddel <nick.reddel@gmail.com>
2020-12-17 17:54:50 -0800

    Fix per-thread logging

--- framework/servlet/src/cc/alcina/framework/servlet/logging/PerThreadLoggingHandler.java
+++ framework/servlet/src/cc/alcina/framework/servlet/logging/PerThreadLoggingHandler.java
@@ -3,26 +3,25 @@ package cc.alcina.framework.servlet.logging;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
-import cc.alcina.framework.common.client.util.LooseContext;
+public class PerThreadLoggingHandler extends Handler {
+	private static ThreadLocal<PerThreadBuffer> handlers = new ThreadLocal<>();
 
-public class PerThreadLoggingHandler extends Handler
-		implements PerThreadLogging {
-	private static final String CONTEXT_BUFFER = PerThreadLoggingHandler.class
-			.getName() + ".CONTEXT_BUFFER";
+	public static void beginBuffer() {
+		handlers.set(new PerThreadBuffer());
+	}
 
-	@Override
-	public void beginBuffer() {
-		LooseContext.set(CONTEXT_BUFFER, new PerThreadBuffer());
+	public static String endBuffer() {
+		PerThreadBuffer buffer = handlers.get();
+		if (buffer == null) {
+			return "(Per-thread buffer already cleared)";
+		} else {
+			handlers.remove();
+			return buffer.builder.toString();
+		}
 	}
 
 	@Override
 	public void close() throws SecurityException {
-	}
-
-	@Override
-	public String endBuffer() {
-		PerThreadBuffer buffer = LooseContext.remove(CONTEXT_BUFFER);
-		return buffer.toString();
 	}
 
 	@Override
@@ -32,7 +31,8 @@ public class PerThreadLoggingHandler extends Handler
 	@Override
 	public void publish(LogRecord record) {
 		// publishes all events - doesn't filter by log level
-		PerThreadBuffer buffer = LooseContext.get(CONTEXT_BUFFER);
+
+		PerThreadBuffer buffer = handlers.get();
 		if (buffer != null) {
 			buffer.publish(record);
 		}
