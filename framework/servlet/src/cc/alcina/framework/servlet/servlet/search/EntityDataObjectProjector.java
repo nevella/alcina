49b3fbe688ddf6df8cefc80f510fdc4ecf0661a2
Nick Reddel <nick.reddel@gmail.com>
2021-01-26 12:08:27 -0800

    Improve standard search projection

--- framework/servlet/src/cc/alcina/framework/servlet/servlet/search/EntityDataObjectProjector.java
+++ framework/servlet/src/cc/alcina/framework/servlet/servlet/search/EntityDataObjectProjector.java
@@ -1,57 +1,60 @@
 package cc.alcina.framework.servlet.servlet.search;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
-import javax.persistence.ManyToMany;
-
-import cc.alcina.framework.common.client.WrappedRuntimeException;
-import cc.alcina.framework.common.client.csobjects.Bindable;
 import cc.alcina.framework.common.client.logic.domain.Entity;
-import cc.alcina.framework.common.client.logic.domain.EntityDataObject;
-import cc.alcina.framework.common.client.logic.domain.EntityDataObject.ContextProjector;
-import cc.alcina.framework.common.client.logic.domain.EntityDataObject.EntityMultipleDataObjectDecorator;
-import cc.alcina.framework.common.client.logic.domain.EntityDataObject.EntitySingleDataObjectDecorator;
-import cc.alcina.framework.common.client.logic.domain.VersionableEntity;
-import cc.alcina.framework.common.client.logic.domaintransform.lookup.LiSet;
+import cc.alcina.framework.common.client.logic.domain.EntityDataObject.OneToManyMultipleSummary;
 import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
-import cc.alcina.framework.entity.SEUtilities;
+import cc.alcina.framework.common.client.util.CommonUtils;
 import cc.alcina.framework.entity.projection.CollectionProjectionFilter;
 import cc.alcina.framework.entity.projection.GraphProjection;
 import cc.alcina.framework.entity.projection.GraphProjection.GraphProjectionContext;
 import cc.alcina.framework.entity.projection.GraphProjections;
-import cc.alcina.framework.gwt.client.entity.search.EntitySearchDefinition;
-import cc.alcina.framework.gwt.client.entity.search.ModelSearchResults;
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 
 /*
  * Deliberately does not extend SearchResultProjector. To use this, create a subclass of SearchResultProjector (with a higher registry priority) and delegate to this
  */
 public class EntityDataObjectProjector {
-	private boolean multiple;
+	ReferenceOpenHashSet<Entity> projectable = new ReferenceOpenHashSet<>();
 
-	private boolean projectAsSingleEntityDataObjects;
-
-	public EntityDataObjectProjector withProjectAsSingleEntityDataObjects(
-			boolean projectAsSingleEntityDataObjects) {
-		this.projectAsSingleEntityDataObjects = projectAsSingleEntityDataObjects;
-		return this;
-	}
-
-	public <T> T project(T object) {
-		multiple = object instanceof Collection
-				&& !projectAsSingleEntityDataObjects;
-		if (multiple) {
-			Collection<VersionableEntity> collection = (Collection) object;
-			if (collection.size() > 0) {
-				Entity first = collection.iterator().next();
-				EntityMultipleDataObjectDecorator multipleDataObjectDecorator = Registry
-						.implOrNull(EntityMultipleDataObjectDecorator.class,
-								(first).entityClass());
-				if (multipleDataObjectDecorator != null&&"---".isEmpty()) {
-					object = (T) collection.stream()
-							.map(multipleDataObjectDecorator)
-							.collect(Collectors.toList());
+	/*
+	 * add the immediate entity reachables of either the single-entity or entity
+	 * collection
+	 */
+	public <T> T project(T object) throws Exception {
+		Set<Entity> seeds = (Set) CommonUtils.wrapInCollection(object).stream()
+				.collect(Collectors.toSet());
+		Stack<Entity> traverse = new Stack<>();
+		traverse.addAll(seeds);
+		while (!traverse.isEmpty()) {
+			Entity entity = traverse.pop();
+			projectable.add(entity);
+			GraphProjection projection = new GraphProjection();
+			List<Field> nonPrimitive = projection
+					.getNonPrimitiveOrDataFieldsForClass(entity.getClass());
+			for (Field field : nonPrimitive) {
+				if (Entity.class.isAssignableFrom(field.getType())) {
+					Entity toOneRel = (Entity) field.get(entity);
+					if (toOneRel != null) {
+						if (seeds.add(toOneRel)) {
+							projectable.add(toOneRel);
+							traverse.push(toOneRel);
+						}
+					}
+				}
+				if (Collection.class.isAssignableFrom(field.getType())) {
+					Collection collection = (Collection) field.get(entity);
+					if (collection != null) {
+						collection.stream().filter(o -> o instanceof Entity)
+								.forEach(o -> projectable.add((Entity) o));
+					}
 				}
 			}
 		}
@@ -60,105 +63,35 @@ public class EntityDataObjectProjector {
 	}
 
 	public class DataFilter extends CollectionProjectionFilter {
+		private CollectionProjectionFilter defaultDelegate = Registry
+				.impl(CollectionProjectionFilter.class);
+
 		@Override
 		public <T> T filterData(T original, T projected,
 				GraphProjectionContext context, GraphProjection graphProjection)
 				throws Exception {
-			ContextProjector contextProjector = new ContextProjector() {
-				@Override
-				public Object project(Object source) {
-					try {
-						return graphProjection.project(source, context);
-					} catch (Exception e) {
-						throw new WrappedRuntimeException(e);
-					}
+			Field field = context.field;
+			if (original instanceof Entity) {
+				if (!projectable.contains(original)) {
+					return null;
 				}
-
-				@Override
-				public <E> E registerProjected(E source, E dataObject) {
-					return graphProjection.registerProjected(source,
-							dataObject);
-				}
-			};
-			if (projected instanceof EntityDataObject) {
-				// passthrough
-			} else if (original instanceof VersionableEntity) {
-				if (multiple) {
-					// FIXME - dirndl.2 - we can't (because of graphiness)
-					// guarantee
-					// objects that exist at depth==2 are encountered first at
-					// depth ==2
-					// if (context.depth() > 2) {
-					// return null;
-					// }
-				} else {
-					EntitySingleDataObjectDecorator singleDataObjectDecorator = Registry
-							.implOrNull(EntitySingleDataObjectDecorator.class,
-									((Entity) original).entityClass());
-					if (singleDataObjectDecorator != null&&"---".isEmpty()) {
-						projected = (T) singleDataObjectDecorator.apply(
-								(VersionableEntity) original, contextProjector);
-						return graphProjection.project(original, projected,
-								context, false);
-					}
-				}
-			} else {
-				// lists (search result lists) will be projected normally -
-				// this
-				// is
-				// to filter entity.[oneToMany]
-				if (original instanceof Set) {
-					Set replacement = maybeReplaceSet(original, projected,
-							context);
-					if (replacement != projected) {
-						return (T) replacement;
-					}
-				}
+			} else if (field != null
+					&& OneToManyMultipleSummary.class == field.getType()) {
+				OneToManyMultipleSummary value = (OneToManyMultipleSummary) original;
+				String providerMethodName = value
+						.getCollectionAccessorMethodName();
+				Method method = context.sourceOwner.getClass()
+						.getMethod(providerMethodName, new Class[0]);
+				Collection<? extends Entity> collection = (Collection<? extends Entity>) method
+						.invoke(context.sourceOwner, new Object[0]);
+				OneToManyMultipleSummary summary = new OneToManyMultipleSummary(
+						(Entity) context.sourceOwner, collection,
+						value.getEntityClass());
+				return (T) summary;
 			}
-			return super.filterData(original, projected, context,
+			T result = defaultDelegate.filterData(original, projected, context,
 					graphProjection);
+			return result;
 		}
-	}
-
-	public Class<? extends Bindable>
-			getProjectedClass(ModelSearchResults modelSearchResults) {
-		Class<VersionableEntity> clazz = null;
-		if (modelSearchResults.def != null&&"---".isEmpty()) {
-			clazz = ((EntitySearchDefinition) modelSearchResults.def)
-					.queriedEntityClass();
-			EntityMultipleDataObjectDecorator multipleDataObjectDecorator = Registry
-					.implOrNull(EntityMultipleDataObjectDecorator.class, clazz);
-			if (multipleDataObjectDecorator != null) {
-				return multipleDataObjectDecorator.getProjectedClass();
-			}
-		} else if (modelSearchResults.rawEntity != null&&"---".isEmpty()) {
-			clazz = modelSearchResults.rawEntity.entityClass();
-			EntitySingleDataObjectDecorator singleDataObjectDecorator = Registry
-					.implOrNull(EntitySingleDataObjectDecorator.class, clazz);
-			if (singleDataObjectDecorator != null) {
-				return singleDataObjectDecorator.createDataObject().getClass();
-			}
-		}
-		return clazz;
-	}
-
-	public <T> Set maybeReplaceSet(T original, T projected,
-			GraphProjectionContext context) {
-		if (context.parent(o -> o instanceof VersionableEntity).isPresent()
-				&& context.sourceOwner instanceof VersionableEntity) {
-			if (SEUtilities
-					.getPropertyDescriptorByName(
-							((Entity) context.sourceOwner).entityClass(),
-							context.fieldName)
-					.getReadMethod().getAnnotation(ManyToMany.class) != null) {
-			} else {
-				if (original instanceof LiSet) {
-					return new LiSet();
-				} else {
-					// non-relational
-				}
-			}
-		}
-		return (Set) projected;
 	}
 }
