1c992fdd0d08b20a1ccb28bb5030c24d372f273d
Nick Reddel <nick.reddel@gmail.com>
2021-02-24 19:07:31 -0800

    Re-implement - the double-weak references mean the domdoc was in danger of being gc-ed

---
+++ framework/entity/src/cc/alcina/framework/entity/impl/PerDocumentSupplierCoreDocument.java
@@ -0,0 +1,33 @@
+package cc.alcina.framework.entity.impl;
+
+import org.apache.xerces.dom.CoreDocumentImpl;
+import org.w3c.dom.Document;
+
+import cc.alcina.framework.common.client.dom.DomDoc;
+import cc.alcina.framework.common.client.dom.DomDoc.PerDocumentSupplier;
+import cc.alcina.framework.common.client.logic.reflection.RegistryLocation;
+import cc.alcina.framework.common.client.logic.reflection.RegistryLocation.ImplementationType;
+
+@RegistryLocation(registryPoint = PerDocumentSupplier.class, implementationType = ImplementationType.SINGLETON, priority = RegistryLocation.MANUAL_PRIORITY)
+public class PerDocumentSupplierCoreDocument extends PerDocumentSupplier {
+	public static final transient String CORE_DOCUMENT_DOM_DOC_KEY = PerDocumentSupplierCoreDocument.class
+			.getName() + ".CORE_DOCUMENT_DOM_DOC_KEY";
+
+	public PerDocumentSupplierCoreDocument() {
+	}
+
+	@Override
+	public DomDoc get(Document document) {
+		synchronized (document) {
+			CoreDocumentImpl impl = (CoreDocumentImpl) document;
+			DomDoc userData = (DomDoc) impl.getUserData(impl,
+					CORE_DOCUMENT_DOM_DOC_KEY);
+			if (userData == null) {
+				userData = new DomDoc(document);
+				impl.setUserData(impl, CORE_DOCUMENT_DOM_DOC_KEY, userData,
+						null);
+			}
+			return userData;
+		}
+	}
+}
