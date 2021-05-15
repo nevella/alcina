91b4a76234a32fd109f284e0f0446e097832e10e
Nick Reddel <nick.reddel@gmail.com>
2021-05-15 15:42:07 -0700

    Add util for caches

--- framework/entity/src/cc/alcina/framework/entity/util/DataFolderProvider.java
+++ framework/entity/src/cc/alcina/framework/entity/util/DataFolderProvider.java
@@ -3,6 +3,7 @@ package cc.alcina.framework.entity.util;
 import java.io.File;
 
 import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
+import cc.alcina.framework.common.client.util.Ax;
 import cc.alcina.framework.entity.SEUtilities;
 
 //@RegistryLocation(registryPoint = DataFolderProvider.class, implementationType = ImplementationType.SINGLETON)
@@ -14,6 +15,11 @@ public abstract class DataFolderProvider {
 
 	public File getChildFile(String childFileName) {
 		return SEUtilities.getChildFile(getDataFolder(), childFileName);
+	}
+
+	public File getClassDataFile(Object instance) {
+		return getChildFile(Ax.format("class-data-file/%s.dat",
+				instance.getClass().getCanonicalName()));
 	}
 
 	public abstract File getDataFolder();
