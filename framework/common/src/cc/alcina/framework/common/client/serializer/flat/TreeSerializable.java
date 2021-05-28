14c5d7b9299d0cd77c3f8a421d8dd6b5d37e217c
Nick Reddel <nick.reddel@gmail.com>
2021-05-27 19:11:01 -0700

    Add docs

--- framework/common/src/cc/alcina/framework/common/client/serializer/flat/TreeSerializable.java
+++ framework/common/src/cc/alcina/framework/common/client/serializer/flat/TreeSerializable.java
@@ -2,6 +2,14 @@ package cc.alcina.framework.common.client.serializer.flat;
 
 import java.io.Serializable;
 
+/**
+ * Important! (because hard to enforce) - any class that implements
+ * TreeSerializable should also have (usually via a superclass)
+ * 
+ * @RegistryLocation(registryPoint = TreeSerializable.class)
+ * @author nick@alcina.cc
+ *
+ */
 public interface TreeSerializable extends Serializable {
 	default void onAfterTreeDeserialize() {
 	}
