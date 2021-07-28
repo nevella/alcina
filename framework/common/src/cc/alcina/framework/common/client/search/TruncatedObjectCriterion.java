c4135482983036c691a5d335ba3c9d863860f661
Nick Reddel <nick.reddel@gmail.com>
2021-07-27 19:04:17 -0700

    Cache displaytexts client-side

--- framework/common/src/cc/alcina/framework/common/client/search/TruncatedObjectCriterion.java
+++ framework/common/src/cc/alcina/framework/common/client/search/TruncatedObjectCriterion.java
@@ -1,11 +1,16 @@
 package cc.alcina.framework.common.client.search;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.xml.bind.annotation.XmlTransient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.gwt.core.client.GWT;
 
 import cc.alcina.framework.common.client.Reflections;
 import cc.alcina.framework.common.client.logic.domain.HasId;
+import cc.alcina.framework.common.client.logic.domaintransform.EntityLocator;
 import cc.alcina.framework.common.client.logic.reflection.AlcinaTransient;
 import cc.alcina.framework.common.client.serializer.flat.PropertySerialization;
 import cc.alcina.framework.common.client.util.CommonUtils;
@@ -13,6 +18,8 @@ import cc.alcina.framework.gwt.client.objecttree.search.StandardSearchOperator;
 
 public abstract class TruncatedObjectCriterion<E extends HasId>
 		extends SearchCriterion implements HasId {
+	private static Map<EntityLocator, String> clientDisplayTexts;
+
 	static final transient long serialVersionUID = 1;
 
 	private String displayText;
@@ -51,6 +58,25 @@ public abstract class TruncatedObjectCriterion<E extends HasId>
 
 	@PropertySerialization(fromClient = false)
 	public String getDisplayText() {
+		if (GWT.isClient()) {
+			Map<EntityLocator, String> clientDisplayTexts = ensureClientDisplayTexts();
+			if (id != 0) {
+				try {
+					EntityLocator locator = new EntityLocator(
+							(Class) getObjectClass(), id, 0L);
+					if (displayText != null) {
+						clientDisplayTexts.put(locator, displayText);
+					} else {
+						this.displayText = clientDisplayTexts.get(locator);
+					}
+				} catch (Exception e) {
+					// FIXME - meta - add isassignable (from Entity) check;
+					// should only fail
+					// if isAssignable fails
+					e.printStackTrace();
+				}
+			}
+		}
 		return this.displayText;
 	}
 
@@ -106,6 +132,13 @@ public abstract class TruncatedObjectCriterion<E extends HasId>
 	public <T extends TruncatedObjectCriterion<E>> T withObject(E withValue) {
 		setValue(withValue);
 		return (T) this;
+	}
+
+	private Map<EntityLocator, String> ensureClientDisplayTexts() {
+		if (clientDisplayTexts == null) {
+			clientDisplayTexts = new LinkedHashMap<>();
+		}
+		return clientDisplayTexts;
 	}
 
 	protected String provideDisplayTextFor(E value) {
