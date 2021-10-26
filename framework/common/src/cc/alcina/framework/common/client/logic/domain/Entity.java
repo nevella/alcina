47cdec7665e314c8e898fe511e5d39e8adbe5170
Nick Reddel <nick.reddel@gmail.com>
2021-10-26 09:33:52 -0700

    Simplify generics

--- framework/common/src/cc/alcina/framework/common/client/logic/domain/Entity.java
+++ framework/common/src/cc/alcina/framework/common/client/logic/domain/Entity.java
@@ -58,7 +58,7 @@ import cc.alcina.framework.entity.persistence.mvcc.MvccAccess.MvccAccessType;
 // the hash in recursive deserialization)
 @JsonPropertyOrder(value = { "id", "localId" }, alphabetic = true)
 public abstract class Entity<T extends Entity> extends Bindable
-		implements HasVersionNumber, HasId, HasEntity<T> {
+		implements HasVersionNumber, HasId, HasEntity {
 	public static final transient String CONTEXT_USE_SYSTEM_HASH_CODE_IF_ZERO_ID_AND_LOCAL_ID = Entity.class
 			+ ".CONTEXT_USE_SYSTEM_HASH_CODE_IF_ZERO_ID_AND_LOCAL_ID";
 
@@ -225,8 +225,8 @@ public abstract class Entity<T extends Entity> extends Bindable
 	}
 
 	@Override
-	public T provideEntity() {
-		return (T) domainIdentity();
+	public <E extends Entity> E provideEntity() {
+		return (E) domainIdentity();
 	}
 
 	@Override
