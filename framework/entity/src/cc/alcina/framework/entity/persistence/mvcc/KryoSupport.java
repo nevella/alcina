40fa957eda12d460520c2f7ecdd2bbd3eecc933d
Nick Reddel <nick@alcina.cc>
2021-11-06 11:46:59 -0700

    Improve entity serialization docs

--- framework/entity/src/cc/alcina/framework/entity/persistence/mvcc/KryoSupport.java
+++ framework/entity/src/cc/alcina/framework/entity/persistence/mvcc/KryoSupport.java
@@ -12,10 +12,14 @@ import cc.alcina.framework.common.client.logic.domain.Entity;
 import cc.alcina.framework.common.client.logic.reflection.RegistryLocation;
 import cc.alcina.framework.common.client.logic.reflection.RegistryLocation.ImplementationType;
 import cc.alcina.framework.common.client.util.Ax;
+import cc.alcina.framework.common.client.util.LooseContext;
 import cc.alcina.framework.entity.KryoUtils.EntitySerializer;
 import cc.alcina.framework.entity.persistence.AppPersistenceBase;
 
 public class KryoSupport {
+	public static final String CONTEXT_DESERIALIZING_PRODUCTION_GRAPH = KryoSupport.class
+			.getName() + ".CONTEXT_DESERIALIZING_PRODUCTION_GRAPH";
+
 	@RegistryLocation(registryPoint = SerializerFactory.class, implementationType = ImplementationType.SINGLETON)
 	public static class MvccInterceptorSerializer implements SerializerFactory {
 		@Override
@@ -24,9 +28,15 @@ public class KryoSupport {
 				return new MvccObjectSerializer(kryo, type);
 			}
 			if (Entity.class.isAssignableFrom(type)
-					&& (Ax.isTest() || AppPersistenceBase.isTestServer())) {
+					&& (Ax.isTest() || AppPersistenceBase.isTestServer())&&!LooseContext.is(CONTEXT_DESERIALIZING_PRODUCTION_GRAPH)) {
 				// this could go to production, but it's mostly needed for
-				// console/webapp comms
+				// console/webapp comms - since they have circular graphs where
+				// id/localid affects the hash
+				//
+				// FIXME - Kryo - make it production with kryo 7 (or whatever)
+				//
+				// in the meantime, set the context variable if deserializing a
+				// production graph
 				return new EntitySerializer(kryo, type);
 			}
 			return new FieldSerializer<>(kryo, type);
