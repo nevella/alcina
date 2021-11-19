c34dde05b33ad1215ca5885ea88058e5c023c0f3
Nick Reddel <nick@alcina.cc>
2021-11-19 07:01:28 -0800

    Switch to multline-capable, remove autofocus

--- framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/model/EditableStringModel.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/model/EditableStringModel.java
@@ -1,11 +1,12 @@
 package cc.alcina.framework.gwt.client.dirndl.model;
 
 import cc.alcina.framework.common.client.gwittir.validator.NotBlankValidator;
+import cc.alcina.framework.common.client.logic.reflection.Custom;
 import cc.alcina.framework.common.client.logic.reflection.Display;
+import cc.alcina.framework.common.client.logic.reflection.NamedParameter;
 import cc.alcina.framework.common.client.logic.reflection.Validator;
-import cc.alcina.framework.gwt.client.dirndl.model.FormModel.FormModelTransformer;
+import cc.alcina.framework.gwt.client.gwittir.customiser.TextAreaCustomiser;
 
-@FormModelTransformer.Args(focusOnAttach = "string")
 public class EditableStringModel extends Model {
 	private String string;
 
@@ -25,6 +26,9 @@ public class EditableStringModel extends Model {
 
 	@Display(name = "String")
 	@Validator(validator = NotBlankValidator.class)
+	@Custom(customiserClass = TextAreaCustomiser.class, parameters = {
+			@NamedParameter(name = TextAreaCustomiser.ENSURE_ALL_LINES_VISIBLE, booleanValue = true),
+			@NamedParameter(name = TextAreaCustomiser.LINES, intValue = 1)})
 	public String getString() {
 		return this.string;
 	}
