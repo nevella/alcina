11fc8ac95ecb7d5b340eff05b2258e73538d09e6
Nick Reddel <nick@alcina.cc>
2021-12-08 08:52:51 -0800

    Add pre-registry-2 serialization support

--- framework/servlet/src/cc/alcina/framework/servlet/publication/format/FormatConverterJson.java
+++ framework/servlet/src/cc/alcina/framework/servlet/publication/format/FormatConverterJson.java
@@ -8,17 +8,31 @@ import java.util.List;
 import org.w3c.dom.Document;
 
 import cc.alcina.framework.common.client.logic.reflection.RegistryLocation;
+import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
+import cc.alcina.framework.common.client.publication.PublicationContent;
 import cc.alcina.framework.common.client.publication.FormatConversionTarget.FormatConversionTarget_JSON;
 import cc.alcina.framework.entity.util.JacksonUtils;
 import cc.alcina.framework.servlet.grid.ExcelExporter;
 import cc.alcina.framework.servlet.publication.FormatConverter;
 import cc.alcina.framework.servlet.publication.PublicationContext;
+import cc.alcina.framework.servlet.publication.format.FormatConverterXml.TypedConverterXml;
 
 @RegistryLocation(registryPoint = FormatConverter.class, targetClass = FormatConversionTarget_JSON.class)
 public class FormatConverterJson implements FormatConverter {
+	// FIXME - registry.2 - remove
+	public static interface TypedConverterJson<T extends PublicationContent> {
+		public String toJson(T publicationContent);
+	}
+
 	@Override
 	public InputStream convert(PublicationContext ctx,
 			FormatConversionModel fcm) throws Exception {
+		TypedConverterJson typedConverter = Registry.implOrNull(
+				TypedConverterJson.class, ctx.publicationContent.getClass());
+		if (typedConverter != null) {
+			String json = typedConverter.toJson(ctx.publicationContent);
+			fcm.bytes = json.getBytes(StandardCharsets.UTF_8);
+		}
 		if (fcm.bytes == null) {
 			ExcelExporter exporter = new ExcelExporter();
 			Document doc = exporter.getTemplate();
--- framework/servlet/src/cc/alcina/framework/servlet/publication/format/FormatConverterXml.java
+++ framework/servlet/src/cc/alcina/framework/servlet/publication/format/FormatConverterXml.java
@@ -5,13 +5,20 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import cc.alcina.framework.common.client.logic.reflection.RegistryLocation;
+import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
 import cc.alcina.framework.common.client.publication.FormatConversionTarget.FormatConversionTarget_XML;
+import cc.alcina.framework.common.client.publication.PublicationContent;
 import cc.alcina.framework.common.client.util.CommonUtils;
 import cc.alcina.framework.servlet.publication.FormatConverter;
 import cc.alcina.framework.servlet.publication.PublicationContext;
 
 @RegistryLocation(registryPoint = FormatConverter.class, targetClass = FormatConversionTarget_XML.class)
 public class FormatConverterXml implements FormatConverter {
+	
+	//FIXME - registry.2 - remove
+	public static interface TypedConverterXml<T extends PublicationContent>{
+		public String toXml(T publicationContent);
+	}
 	@Override
 	public InputStream convert(PublicationContext ctx,
 			FormatConversionModel conversionModel) throws Exception {
@@ -21,6 +28,10 @@ public class FormatConverterXml implements FormatConverter {
 		}
 		if (!s.startsWith("<?xml")) {
 			s = CommonUtils.XML_PI + s;
+		}
+		TypedConverterXml typedConverter = Registry.implOrNull(TypedConverterXml.class, ctx.publicationContent.getClass());
+		if(typedConverter!=null){
+			s=typedConverter.toXml(ctx.publicationContent);
 		}
 		return new ByteArrayInputStream(s.getBytes("UTF-8"));
 	}
