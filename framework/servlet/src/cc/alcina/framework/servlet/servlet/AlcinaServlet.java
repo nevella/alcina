de2af8d1c911638cc3fac80110e3c1a6c2939524
Jumail Mundekkat <jmundekkat@barnet.com.au>
2021-12-17 19:21:40 +1100

    Revert AlcinaServlet.writeAndClose() to content type text/plain

--- framework/servlet/src/cc/alcina/framework/servlet/servlet/AlcinaServlet.java
+++ framework/servlet/src/cc/alcina/framework/servlet/servlet/AlcinaServlet.java
@@ -45,7 +45,7 @@ public abstract class AlcinaServlet extends HttpServlet {
 
 	public void writeAndClose(String s, HttpServletResponse response)
 			throws IOException {
-		response.setContentType("text/html");
+		response.setContentType("text/plain");
 		response.getWriter().write(s);
 		response.getWriter().close();
 	}
