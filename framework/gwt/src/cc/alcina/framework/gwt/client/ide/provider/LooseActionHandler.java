fe0a751f02136cba958f454a91e425bff9343bc5
Nick Reddel <nick@alcina.cc>
2022-02-25 14:53:22 -0800

    Reflection v2

--- framework/gwt/src/cc/alcina/framework/gwt/client/ide/provider/LooseActionHandler.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/ide/provider/LooseActionHandler.java
@@ -1,10 +1,10 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -13,11 +13,13 @@
  */
 package cc.alcina.framework.gwt.client.ide.provider;
 
+import cc.alcina.framework.common.client.logic.reflection.Registration;
+
 /**
  *
  * @author Nick Reddel
  */
-public interface LooseActionHandler {
+public interface LooseActionHandler extends Registration.Ensure {
 	public String getName();
 
 	public void performAction();
