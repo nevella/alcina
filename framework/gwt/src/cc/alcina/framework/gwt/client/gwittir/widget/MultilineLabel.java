3e2a53f9c34839c1ce60d3ebc74fbe06d6f2bed7
Jumail Mundekkat <jmundekkat@barnet.com.au>
2022-03-16 12:49:21 +1100

    Add MultilineLabel to Reflected classes

--- framework/gwt/src/cc/alcina/framework/gwt/client/gwittir/widget/MultilineLabel.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/gwittir/widget/MultilineLabel.java
@@ -2,8 +2,10 @@ package cc.alcina.framework.gwt.client.gwittir.widget;
 
 import com.totsp.gwittir.client.ui.Label;
 
+import cc.alcina.framework.common.client.logic.reflection.reachability.Reflected;
 import cc.alcina.framework.gwt.client.gwittir.customiser.MultilineWidget;
 
+@Reflected
 public class MultilineLabel extends Label implements MultilineWidget {
 	public MultilineLabel() {
 		super(null);
