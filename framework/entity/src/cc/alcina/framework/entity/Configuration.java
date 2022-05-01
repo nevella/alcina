3cde51a35187d35c571d0effb6f9307ed8e14145
Nick Reddel <nick@alcina.cc>
2022-05-01 16:32:08 -0700

    Begin replacement of ResourceUtilities/configuration

---
+++ framework/entity/src/cc/alcina/framework/entity/Configuration.java
@@ -0,0 +1,24 @@
+package cc.alcina.framework.entity;
+
+/*
+ * Replacement for system configuration portion of ResourceUtilities
+ */
+public class Configuration {
+	public static String get(Class clazz, String key) {
+		return ResourceUtilities.getBundledString(clazz, key);
+	}
+
+	public static String get(String key) {
+		String value = get(StackWalker
+				.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+				.getCallerClass(), key);
+		return value;
+	}
+
+	public static int getInt(String key) {
+		String value = get(StackWalker
+				.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+				.getCallerClass(), key);
+		return Integer.parseInt(value);
+	}
+}
