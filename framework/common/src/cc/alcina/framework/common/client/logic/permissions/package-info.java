f1fbf7a75bf334a65bb13209e804136d386a5ccd
Nick Reddel <nick@alcina.cc>
2022-05-10 10:16:42 -0700

    Begin permissions documentation

---
+++ framework/common/src/cc/alcina/framework/common/client/logic/permissions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <h2>Alcina Permissions</h2>
+ * <p>The alcina permissions system is used primarily to control access to entities and UI interface elements 
+ */
+package cc.alcina.framework.common.client.logic.permissions;
+
