ab2e9bd6aad8a24a3753fdfa954e3fba95f84398
Nick Reddel <nick.reddel@gmail.com>
2022-06-18 07:52:44 -0700

    Remove very much incorrect JaxbContextRegistration annotation
    (since it isn't JAXB-serializable)

--- framework/common/src/cc/alcina/framework/common/client/domain/search/GroupingParameters.java
+++ framework/common/src/cc/alcina/framework/common/client/domain/search/GroupingParameters.java
@@ -7,16 +7,13 @@ import java.util.List;
 import cc.alcina.framework.common.client.csobjects.Bindable;
 import cc.alcina.framework.common.client.domain.search.SearchOrders.ColumnSearchOrder;
 import cc.alcina.framework.common.client.logic.reflection.Display;
-import cc.alcina.framework.common.client.logic.reflection.Registration;
-import cc.alcina.framework.common.client.logic.reflection.misc.JaxbContextRegistration;
 import cc.alcina.framework.common.client.search.ReflectCloneable;
 import cc.alcina.framework.common.client.serializer.PropertySerialization;
 import cc.alcina.framework.common.client.serializer.TreeSerializable;
 import cc.alcina.framework.common.client.util.HasReflectiveEquivalence;
 
-@Registration(JaxbContextRegistration.class)
-public abstract class GroupingParameters<GP extends GroupingParameters> extends Bindable
-		implements Serializable, HasReflectiveEquivalence<GP>,
+public abstract class GroupingParameters<GP extends GroupingParameters>
+		extends Bindable implements Serializable, HasReflectiveEquivalence<GP>,
 		ReflectCloneable<GP>, TreeSerializable {
 	private List<ColumnSearchOrder> columnOrders = new ArrayList<>();
 
