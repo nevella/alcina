e7210619fc76fb53b3683811a2c88b15a7b8b67f
Nick Reddel <nick.reddel@gmail.com>
2022-07-02 07:10:33 -0700

    Add capacity to pass mime type

--- framework/common/src/cc/alcina/framework/common/client/publication/DeliveryModel.java
+++ framework/common/src/cc/alcina/framework/common/client/publication/DeliveryModel.java
@@ -1,10 +1,10 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -136,6 +136,8 @@ public interface DeliveryModel extends TreeSerializable {
 
 		private String transformerPropertiesSerialized;
 
+		private String mimeType;
+
 		public void addTransformerProperty(String key, String value) {
 			StringMap map = provideTransformerProperties();
 			map.put(key, value);
@@ -156,6 +158,10 @@ public interface DeliveryModel extends TreeSerializable {
 
 		public String getFileName() {
 			return this.fileName;
+		}
+
+		public String getMimeType() {
+			return this.mimeType;
 		}
 
 		public String getTransformerClassName() {
@@ -194,6 +200,10 @@ public interface DeliveryModel extends TreeSerializable {
 
 		public void setFileName(String fileName) {
 			this.fileName = fileName;
+		}
+
+		public void setMimeType(String mimeType) {
+			this.mimeType = mimeType;
 		}
 
 		public void setTransformerClassName(String transformerClassName) {
