fd7723804232fb0da997008cc4ef02bf3f8119b9
Nick Reddel <nick.reddel@gmail.com>
2022-08-24 14:02:14 +0200

    Docs

--- framework/common/src/cc/alcina/framework/package-info.java
+++ framework/common/src/cc/alcina/framework/package-info.java
@@ -6,6 +6,23 @@
  * support for mid-sized domain applications with strong interrelationships.
  * </p>
  * <h2>Main components</h2>
- * 
+ *
+ * <h2>Naming conventions</h2>
+ * <h3>Subclasses</h3>
+ * <p>
+ * If a subclass is named as a refinement of the superclass name, the refining
+ * adjective can go either at the beginning or end of the name.
+ * <p>
+ * If the superclass name is single-word (say <code>Filter</code>), prefer
+ * prefix naming (say <code>EntityFilter</code>), but prefer postfix naming for
+ * compound superclass names: <code>EntityFilter</code> -&gt;
+ * <code>EntityFilterRestriction</code>
+ * <p>
+ * The reasoning is derived by working backwords from usage in IDE autocomplete,
+ * that specialised subclasses of compound names are rarely referred to directly
+ * in Alcina code, more often they're created reflectively and so postfix naming
+ * prevents clustering around common adjectives
+ *
+ * @category philosophy
  */
 package cc.alcina.framework;
