214cbc5c99c99df947bac0efaa3e7bd602ada7bf
Nick Reddel <nick.reddel@gmail.com>
2022-08-28 20:29:35 -0700

    Extend DomNode.import, fix AlcinaCollections method

--- framework/common/src/cc/alcina/framework/common/client/util/AlcinaCollections.java
+++ framework/common/src/cc/alcina/framework/common/client/util/AlcinaCollections.java
@@ -6,11 +6,11 @@ import java.util.Set;
 import cc.alcina.framework.common.client.logic.reflection.registry.Registry;
 
 public class AlcinaCollections {
-	public static <T> Set<T> newHashSet() {
-		return Registry.impl(CollectionCreators.HashSetCreator.class).create();
+	public static <K, V> Map<K, V> newHashMap() {
+		return CollectionCreators.Bootstrap.getHashMapCreator().create();
 	}
 
-	public <K, V> Map<K, V> newHashMap() {
-		return CollectionCreators.Bootstrap.getHashMapCreator().create();
+	public static <T> Set<T> newHashSet() {
+		return Registry.impl(CollectionCreators.HashSetCreator.class).create();
 	}
 }
