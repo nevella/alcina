d18049dcce6da2e897e3afc9492ee8faaf4aee08
Nick Reddel <nick.reddel@gmail.com>
2022-10-03 20:02:50 -0700

    Revert logging change

--- extras/jsdom/src/com/google/gwt/dom/client/LocalDom.java
+++ extras/jsdom/src/com/google/gwt/dom/client/LocalDom.java
@@ -576,11 +576,10 @@ public class LocalDom {
 				return (T) childNode;
 			} else {
 				if (postReparse) {
-					// FIXME - Logging is extremely verbose on the server
-					// topicUnableToParse.publish(Ax.format(
-					// 		"Text node reparse - remote:\n%s\n\nlocal:\n%s\n",
-					// 		parentRemote.getOuterHtml(),
-					// 		((Element) parent).getOuterHtml()));
+					topicUnableToParse.publish(Ax.format(
+							"Text node reparse - remote:\n%s\n\nlocal:\n%s\n",
+							parentRemote.getOuterHtml(),
+							((Element) parent).getOuterHtml()));
 					throw new RuntimeException("Text node reparse");
 				}
 				ElementRemoteIndex remoteIndex = parentRemote
