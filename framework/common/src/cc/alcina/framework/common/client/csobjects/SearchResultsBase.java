7b933e678343bcee2a6f097f24f42297c79f9506
Nick Reddel <nick.reddel@gmail.com>
2022-10-11 15:29:27 -0700

    Support refl.serialization

--- framework/common/src/cc/alcina/framework/common/client/csobjects/SearchResultsBase.java
+++ framework/common/src/cc/alcina/framework/common/client/csobjects/SearchResultsBase.java
@@ -1,10 +1,10 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,15 +20,17 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlTransient;
 
+import cc.alcina.framework.common.client.logic.reflection.reachability.Bean;
 import cc.alcina.framework.common.client.search.SearchDefinition;
 import cc.alcina.framework.common.client.util.Ax;
 import cc.alcina.framework.common.client.util.CommonUtils;
 
 /**
- * 
+ *
  * @author Nick Reddel
  */
 @XmlAccessorType(XmlAccessType.FIELD)
+@Bean
 public class SearchResultsBase<B extends SearchResult> implements Serializable {
 	private String log = "";
 
