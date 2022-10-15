0951d35a0e9dc5de88fb810e03c268cd62422499
Nick Reddel <nick.reddel@gmail.com>
2022-10-15 12:47:21 -0700

    Work on dirndl docs

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/place/LoggedOutPlace.java
@@ -0,0 +1,11 @@
+package cc.alcina.framework.gwt.client.place;
+
+/**
+ * A place which will always be permitted if logged out, even if the app
+ * normally requires logged in
+ *
+ * @author nick@alcina.cc
+ *
+ */
+public interface LoggedOutPlace {
+}
