8ab16a6f14986f06d913a3f1ff643378b57cdf43
Nick Reddel <nick.reddel@gmail.com>
2022-10-19 14:41:08 -0700

    Add utility

--- framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/model/DetachList.java
+++ framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/model/DetachList.java
@@ -3,11 +3,17 @@ package cc.alcina.framework.gwt.client.dirndl.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.web.bindery.event.shared.HandlerRegistration;
+
 public class DetachList {
 	private List<Runnable> detachList = new ArrayList<>();
 
 	public void add(Runnable runnable) {
 		detachList.add(runnable);
+	}
+
+	public void addRegistration(HandlerRegistration handlerRegistration) {
+		detachList.add(() -> handlerRegistration.removeHandler());
 	}
 
 	public void detach() {
