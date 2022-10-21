760a6e5145bd992defd7302246a36695812a89f0
U-ERT\vlad.nekrashevych <vlad.nekrashevych@USLT10552.ert.com>
2022-10-21 09:25:19 -0700

    Added setters to Repositorycredentials

--- framework/common/src/cc/alcina/framework/common/client/publication/RepositoryCredentials.java
+++ framework/common/src/cc/alcina/framework/common/client/publication/RepositoryCredentials.java
@@ -1,11 +1,11 @@
 package cc.alcina.framework.common.client.publication;
 
 public class RepositoryCredentials {
-	private String principalId = "";
+	private String principalId;
 
-	private String principalSecret = "";
+	private String principalSecret;
 
-	private String repositoryPath = "";
+	private String repositoryPath;
 
 	public String getPrincipalId() {
 		return principalId;
@@ -17,5 +17,17 @@ public class RepositoryCredentials {
 
 	public String getRepositoryPath() {
 		return repositoryPath;
+	}
+	
+	public void setPrincipalId(String principalId) {
+		this.principalId = principalId;
+	}
+
+	public void setPrincipalSecret(String principalSecret) {
+		this.principalSecret = principalSecret;
+	}
+
+	public void setRepositoryPath(String repositoryPath) {
+		this.repositoryPath = repositoryPath;
 	}
 }
