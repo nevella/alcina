85346b36a652a028990e8b843c2bd8e63a6f4941
Nick Reddel <nick.reddel@gmail.com>
2023-01-05 18:37:12 -0800

    Add wildcard options/preflight handler

--- framework/servlet/src/cc/alcina/framework/servlet/servlet/ws/AllOptions.java
+++ framework/servlet/src/cc/alcina/framework/servlet/servlet/ws/AllOptions.java
@@ -1,13 +1,16 @@
 package cc.alcina.framework.servlet.servlet.ws;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/")
-@Consumes(MediaType.MEDIA_TYPE_WILDCARD)
+/**
+ * Extend this class to support all options (for a given resource provider with
+ * an @Path annotation)
+ *
+ * @author nick@alcina.cc
+ *
+ */
 public class AllOptions {
 	@OPTIONS
 	@Path("{path: .*}")
