908962bff3e31ac374d5f1948ae4995ad26bc87c
Nick Reddel <nick.reddel@gmail.com>
2023-01-19 08:12:44 -0800

    WS

--- framework/common/src/cc/alcina/framework/common/client/logic/domaintransform/lookup/JavascriptIntLookup.java
+++ framework/common/src/cc/alcina/framework/common/client/logic/domaintransform/lookup/JavascriptIntLookup.java
@@ -9,63 +9,63 @@ import com.google.gwt.core.client.JsArrayInteger;
 
 public final class JavascriptIntLookup extends JavaScriptObject {
 	public static native JavascriptIntLookup create()/*-{
-														var obj = {
-														length : 0,
-														modCount : 0,
-														valueLookup : {}
-														};
-														return obj;
-														}-*/;
+    var obj = {
+      length : 0,
+      modCount : 0,
+      valueLookup : {}
+    };
+    return obj;
+	}-*/;
 
 	protected JavascriptIntLookup() {
 	}
 
 	public native <V> V get(int key)/*-{
-									return this.valueLookup[key];
-									}-*/;
+    return this.valueLookup[key];
+	}-*/;
 
 	public native JsArrayInteger keys()/*-{
-										var v = [];
-										for ( var k in this.valueLookup) {
-										if (this.valueLookup.hasOwnProperty(k)) {
-										v.push(parseInt(k));
-										}
-										}
-										return v;
-										}-*/;
+    var v = [];
+    for ( var k in this.valueLookup) {
+      if (this.valueLookup.hasOwnProperty(k)) {
+        v.push(parseInt(k));
+      }
+    }
+    return v;
+	}-*/;
 
 	public native void put(int key, Object value)/*-{
-													if (this.valueLookup[key] === undefined) {
-													this.length++;
-													this.modCount++;
-													}
-													this.valueLookup[key] = value;
-													}-*/;
+    if (this.valueLookup[key] === undefined) {
+      this.length++;
+      this.modCount++;
+    }
+    this.valueLookup[key] = value;
+	}-*/;
 
 	public native boolean remove(int key)/*-{
-											if (this.valueLookup[key] === undefined) {
-											return false;
-											} else {
-											delete this.valueLookup[key];
-											this.modCount++;
-											this.length--;
-											return true;
-											}
-											;
-											}-*/;
+    if (this.valueLookup[key] === undefined) {
+      return false;
+    } else {
+      delete this.valueLookup[key];
+      this.modCount++;
+      this.length--;
+      return true;
+    }
+    ;
+	}-*/;
 
 	public native int size()/*-{
-							//should really be an assert here...
-							return this.length >= 0 ? this.length : 0;
-							}-*/;
+    //should really be an assert here...
+    return this.length >= 0 ? this.length : 0;
+	}-*/;
 
 	public Iterator valuesIterator() {
 		return new ValuesIterator();
 	}
 
 	native int modCount()/*-{
-							return this.modCount;
-							}-*/;
+    return this.modCount;
+	}-*/;
 
 	private class ValuesIterator implements Iterator {
 		JsArrayInteger keysSnapshot;
