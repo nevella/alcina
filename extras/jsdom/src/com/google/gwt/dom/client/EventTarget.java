2ef19e81644f970bac8224a3f758ec5d96ff42fc
Nick Reddel <nick.reddel@gmail.com>
2023-01-26 19:30:16 -0800

    Make a little mroe fluent

--- extras/jsdom/src/com/google/gwt/dom/client/EventTarget.java
+++ extras/jsdom/src/com/google/gwt/dom/client/EventTarget.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,19 +15,20 @@
  */
 package com.google.gwt.dom.client;
 
+import com.google.common.base.Preconditions;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JavascriptObjectEquivalent;
 
 /**
  * Represents the target of a JavaScript event.
- * 
+ *
  * <p>
  * This type is returned from methods such as
  * {@link NativeEvent#getEventTarget()}, and must usually be cast to another
  * type using methods such as {@link Element#is(JavaScriptObject)} and
  * {@link Element#as(JavaScriptObject)}.
  * </p>
- * 
+ *
  * <p>
  * This class intentionally does <em>not</em> specify the methods from the DOM
  * IDL (dispatchEvent, addEventListener, and removeEventListener).
@@ -38,6 +39,11 @@ public class EventTarget implements JavascriptObjectEquivalent {
 
 	EventTarget(JavaScriptObject nativeTarget) {
 		this.nativeTarget = nativeTarget;
+	}
+
+	public Element asElement() {
+		Preconditions.checkState(isElement());
+		return Element.as(this);
 	}
 
 	@Override
@@ -53,6 +59,10 @@ public class EventTarget implements JavascriptObjectEquivalent {
 			return true;
 		}
 		return false;
+	}
+
+	public boolean isElement() {
+		return is(Element.class);
 	}
 
 	@Override
