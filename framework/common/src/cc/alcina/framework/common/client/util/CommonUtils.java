5e2e25eb02ad7d440a84e8b4e11d2bafe857a16c
Nick Reddel <nick.reddel@gmail.com>
2023-06-01 14:16:38 -0700

    Add LinkedHashSet.of() (effectively)

--- framework/common/src/cc/alcina/framework/common/client/util/CommonUtils.java
+++ framework/common/src/cc/alcina/framework/common/client/util/CommonUtils.java
@@ -1334,6 +1334,11 @@ public class CommonUtils {
 		return items;
 	}
 
+	public static <E> Set<E> orderedSet(E... elements) {
+		return Arrays.stream(elements)
+				.collect(AlcinaCollectors.toLinkedHashSet());
+	}
+
 	public static String padEight(int number) {
 		if (number < 10000000) {
 			String s = String.valueOf(number);
