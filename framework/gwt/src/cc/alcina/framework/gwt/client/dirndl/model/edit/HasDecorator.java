b7dd0e494c240462ffcb3cde8b713c42be7de3dd
Nick Reddel <nick.reddel@gmail.com>
2023-06-21 09:29:33 -0700

    Work on decorator/contentEditable

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/model/edit/HasDecorator.java
@@ -0,0 +1,7 @@
+package cc.alcina.framework.gwt.client.dirndl.model.edit;
+
+import cc.alcina.framework.common.client.dom.DomNode;
+
+public interface HasDecorator {
+	boolean hasDecorator(DomNode node);
+}
