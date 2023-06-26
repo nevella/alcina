430305cd92c6c89241bb37a3f8cd20e1155dacaa
Nick Reddel <nick.reddel@gmail.com>
2023-06-26 08:34:41 -0700

    Begin FragmentModel

---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/model/fragment/FragmentModel.java
@@ -0,0 +1,23 @@
+package cc.alcina.framework.gwt.client.dirndl.model.fragment;
+
+import cc.alcina.framework.gwt.client.dirndl.model.Model;
+
+/**
+ *
+ * <p>
+ * Tracks FragmentModel - sync between dom and model tree
+ *
+ * <p>
+ * TODO
+ * <ul>
+ * <li>Remove DirectedLayout.Node.widget dep - instead switch to Rendered which
+ * wraps an org.w3c.dom.Node, has getWidget() etc
+ *
+ * </ul>
+ *
+ *
+ * @author nick@alcina.cc
+ *
+ */
+public class FragmentModel extends Model {
+}
---
+++ framework/meta/src/cc/alcina/framework/gwt/client/dirndl/model/fragment/Feature_Dirndl_FragmentModel.java
@@ -0,0 +1,14 @@
+package cc.alcina.framework.gwt.client.dirndl.model.fragment;
+
+import cc.alcina.framework.common.client.meta.Feature;
+import cc.alcina.framework.gwt.client.dirndl.layout.Feature_Dirndl;
+
+/**
+ * Tracks FragmentModel - sync between dom and model tree
+ *
+ * FIXME FragmentModel
+ */
+@Feature.Status.Ref(Feature.Status.In_Progress.class)
+@Feature.Parent(Feature_Dirndl.class)
+public interface Feature_Dirndl_FragmentModel extends Feature {
+}
