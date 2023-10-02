c70d9d7c9dcf7ba8c2900f9bfa9d2b13304a8861
Nick Reddel <nick.reddel@gmail.com>
2023-10-02 13:34:44 +0300

    Add subtype

--- framework/common/src/cc/alcina/framework/common/client/csobjects/view/EntityTransformModel.java
+++ framework/common/src/cc/alcina/framework/common/client/csobjects/view/EntityTransformModel.java
@@ -4,6 +4,10 @@ import cc.alcina.framework.common.client.serializer.TreeSerializable;
 import cc.alcina.framework.gwt.client.dirndl.model.Model;
 
 public interface EntityTransformModel extends TreeSerializable {
+	default Object provideType() {
+		return null;
+	}
+
 	public static abstract class BaseModel extends Model
 			implements EntityTransformModel {
 	}
