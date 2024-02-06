e8485350f6d31f6521ccc074283be799904e3a92
Nick Reddel <nick.reddel@gmail.com>
2024-02-06 16:23:04 +0200

    Add logging

--- framework/common/src/cc/alcina/framework/common/client/traversal/Layer.java
+++ framework/common/src/cc/alcina/framework/common/client/traversal/Layer.java
@@ -253,6 +253,15 @@ public abstract class Layer<S extends Selection> implements Iterable<S> {
 
 		Set<S> submitted = new LinkedHashSet<>();
 
+		Set<String> emittedWarnings = new LinkedHashSet<>();
+
+		public void warnOnce(String template, Object... args) {
+			String message = Ax.format(template, args);
+			if (emittedWarnings.add(message)) {
+				Ax.err(message);
+			}
+		}
+
 		public State(SelectionTraversal.State traversalState) {
 			this.traversalState = traversalState;
 		}
