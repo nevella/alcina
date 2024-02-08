fa4bb1a91b148cab5c9ef03b72c0a76dd175bda6
Nick Reddel <nick.reddel@gmail.com>
2024-02-08 14:19:13 +0200

    Update b64 utils

--- extras/dev/src/cc/alcina/extras/dev/console/DevUtils.java
+++ extras/dev/src/cc/alcina/extras/dev/console/DevUtils.java
@@ -7,33 +7,36 @@ import cc.alcina.framework.common.client.util.Ax;
 import cc.alcina.framework.entity.Io;
 
 public class DevUtils {
-	public static void fileToBase64(String path) {
+	public static String fileToBase64(String path) {
 		try {
 			byte[] bytes = Io.read().path(path).asBytes();
 			String encoded = Base64.getEncoder().encodeToString(bytes);
 			DevConsole.getInstance().setClipboardContents(encoded);
+			return encoded;
 		} catch (Exception e) {
 			throw WrappedRuntimeException.wrap(e);
 		}
 	}
 
-	public static void fileToDataUrl(String path) {
-		fileToDataUrl(path, "image/png");
+	public static String fileToDataUrl(String path) {
+		return fileToDataUrl(path, "image/png");
 	}
 
-	public static void fileToDataUrl(String path, String mimeType) {
+	public static String fileToDataUrl(String path, String mimeType) {
 		try {
 			byte[] bytes = Io.read().path(path).asBytes();
 			String encoded = Base64.getEncoder().encodeToString(bytes);
 			String url = Ax.format("data:%s;base64,%s", mimeType, encoded);
 			DevConsole.getInstance().setClipboardContents(url);
+			return url;
 		} catch (Exception e) {
 			throw WrappedRuntimeException.wrap(e);
 		}
 	}
 
-	public static void stringToBase64(String string) {
+	public static String stringToBase64(String string) {
 		String encoded = Base64.getEncoder().encodeToString(string.getBytes());
 		DevConsole.getInstance().setClipboardContents(encoded);
+		return string;
 	}
 }
