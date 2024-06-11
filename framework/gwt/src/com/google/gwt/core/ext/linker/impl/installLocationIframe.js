eddc9897d09dcb8cc4fb7e539cf626561beaa338
Nick Reddel <nick.reddel@gmail.com>
2024-06-11 11:40:23 +0300

    Add frame title

--- framework/gwt/src/com/google/gwt/core/ext/linker/impl/installLocationIframe.js
+++ framework/gwt/src/com/google/gwt/core/ext/linker/impl/installLocationIframe.js
@@ -16,40 +16,41 @@
 var frameDoc;
 
 function getInstallLocationDoc() {
-  setupInstallLocation();
-  return frameDoc;
+	setupInstallLocation();
+	return frameDoc;
 }
 
 // This function is left for compatibility
 // and may be used by custom linkers
 function getInstallLocation() {
-  return getInstallLocationDoc().body;
+	return getInstallLocationDoc().body;
 }
 
 function setupInstallLocation() {
-  if (frameDoc) { return; }
-  // Create the script frame, making sure it's invisible, but not
-  // "display:none", which keeps some browsers from running code in it.
-  var scriptFrame = $doc.createElement('iframe');
-  //only needed for old IE, and breaks chrome >=73
-  //scriptFrame.src = 'javascript:""';
-  scriptFrame.id = '__MODULE_NAME__';
-  scriptFrame.style.cssText = 'position:absolute; width:0; height:0; border:none; left: -1000px;'
-    + ' top: -1000px;';
-  scriptFrame.tabIndex = -1;
-  $doc.body.appendChild(scriptFrame);
+	if (frameDoc) { return; }
+	// Create the script frame, making sure it's invisible, but not
+	// "display:none", which keeps some browsers from running code in it.
+	var scriptFrame = $doc.createElement('iframe');
+	//only needed for old IE, and breaks chrome >=73
+	//scriptFrame.src = 'javascript:""';
+	scriptFrame.id = '__MODULE_NAME__';
+	scriptFrame.title = 'Javascript container. No human-readable content'
+	scriptFrame.style.cssText = 'position:absolute; width:0; height:0; border:none; left: -1000px;'
+		+ ' top: -1000px;';
+	scriptFrame.tabIndex = -1;
+	$doc.body.appendChild(scriptFrame);
 
-  frameDoc = scriptFrame.contentDocument;
-  if (!frameDoc) {
-    frameDoc = scriptFrame.contentWindow.document;
-  }
+	frameDoc = scriptFrame.contentDocument;
+	if (!frameDoc) {
+		frameDoc = scriptFrame.contentWindow.document;
+	}
 
-  // The missing content has been seen on Safari 3 and firebug will
-  // behave incorrectly on soft refresh unless we explicitly set the content
-  // of the frame. However, we don't want to do this when runAsync calls
-  // installCode, so we do it here when we create the iframe.
-  frameDoc.open();
-  var doctype = (document.compatMode == 'CSS1Compat') ? '<!doctype html>' : '';
-  frameDoc.write(doctype + '<html><head></head><body></body></html>');
-  frameDoc.close();
+	// The missing content has been seen on Safari 3 and firebug will
+	// behave incorrectly on soft refresh unless we explicitly set the content
+	// of the frame. However, we don't want to do this when runAsync calls
+	// installCode, so we do it here when we create the iframe.
+	frameDoc.open();
+	var doctype = (document.compatMode == 'CSS1Compat') ? '<!doctype html>' : '';
+	frameDoc.write(doctype + '<html><head></head><body></body></html>');
+	frameDoc.close();
 }
