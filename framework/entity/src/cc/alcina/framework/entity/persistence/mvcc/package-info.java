01ac1b9fa3783f99c26554ed2f04c90cfc4e7ac8
Nick Reddel <nick.reddel@gmail.com>
2024-09-19 08:55:08 -0700

    mvcc - docs

--- framework/entity/src/cc/alcina/framework/entity/persistence/mvcc/package-info.java
+++ framework/entity/src/cc/alcina/framework/entity/persistence/mvcc/package-info.java
@@ -13,5 +13,119 @@
  * Principal classes: [Domain, Transactions, Transaction, TLTM, Entity,
  * ObjectVersions, ObjectVersion]
  * 
+ * <p>
+ * The domain/mvcc subsystem provides high-performance transactional views of
+ * domain objects (entities, indicies, tries). The implementation is biased
+ * towards read-heavy workloads - when an entity has not been changed during a
+ * non-vacuumed transaction, the extra cost of any public entity method call is
+ * a single null check - i.e. extremely low.
+ * 
+ * <p>
+ * When an entity has been changed, each changed version of the entity is
+ * modelled by a new instance of the entity class. Those instances act
+ * essentially as stores of values - specifically code analysis disallows usage
+ * of the version instance's identity - the keyword this cannot be used in
+ * transactional entity code, instead the {@link Entity#domainIdentity} method
+ * is used to access the object identity of the entity. Routing and maintenance
+ * of versions is controlled by a per-entity {@link MvccObjectVersions} object.
+ * 
+ * 
+ * <h4>Example of transactional entity access and modification</h4>
+ * 
+ * <pre>
+ * <code>
+ * 
+ class Test {
+	Pastry pastry;
+
+	CountDownLatch thread2commitAwait = new CountDownLatch(1);
+
+	void run() {
+		pastry = Pastry.create();
+		pastry.setName("Ensaimada");
+		pastry.setUnitPrice(2.5);
+		Transaction.commit();
+		new Thread("thread-1") {
+			&#64;Override
+			public void run() {
+				try {
+					Transaction.ensureBegun();
+					//
+						// wait until thread2 completes (note that the current
+						// tx will have started before t2's )
+						//
+					thread2commitAwait.await();
+					// do *not* start a new transaction -
+					// Transaction.endAndBeginNew();
+					// will be 2.5
+					Ax.out("%s - %a", Thread.currentThread().getName(),
+							pastry.getUnitPrice());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+		new Thread("thread-2") {
+			&#64;Override
+			public void run() {
+				Transaction.begin();
+				pastry.setUnitPrice(3.5);
+				Transaction.commit();
+				thread2commitAwait.countDown();
+				// will be 3.5
+				Ax.out("%s - %a", Thread.currentThread().getName(),
+						pastry.getUnitPrice());
+				Transaction.end();
+			}
+		}.start();
+		new Thread("thread-3") {
+		&#64;Override
+			public void run() {
+				try {
+					thread2commitAwait.await();
+					// start a new transaction to see the committed value
+					// from thread 1
+					Transaction.begin();
+					// will be 3.5
+					Ax.out("%s - %a", Thread.currentThread().getName(),
+							pastry.getUnitPrice());
+					Transaction.end();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+}
+ * </code>
+ * </pre>
+ * 
+ * 
+ * <p>
+ * Exampe of rewritten code
+ * 
+ * 
+ * <h4>Diagrams:</h4>
+ * <ul>
+ * <li>Version creation and vacuum sequence
+ * </ul>
+ * 
+ * <h4>Major classes in the system:</h4>
+ * 
+ * <ul>
+ * <li>Vacuum
+ * <li>TransactonalMap
+ * <li>Trie
+ * <li>MvccObjectVersions
+ * <li>[Domain, Transactions, Transaction, TLTM, Entity, ObjectVersions,
+ * ObjectVersion] </ul
+ * <p>
+ * Catechism: *
+ * <p>
+ * General implementation notes: Some of the transactional id terminology is
+ * borrowed from the postgresql project's mvcc system, but there are few other
+ * similarities (since this is an MVCC object, not relational database system).
  */
 package cc.alcina.framework.entity.persistence.mvcc;
+
+import cc.alcina.framework.common.client.logic.domain.Entity;
---
+++ framework/gwt/src/cc/alcina/framework/gwt/client/dirndl/layout/doc/example/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p>
+ * This is very much a WIP example. Eventually I'd like to show the mvcc/entity
+ * system backing the (romcom) Croissanteria UI - but that requires a few Alcina
+ * tickets, particularly client/mvcc(!)
+ * 
+ * <p>
+ * In the meatinme, the entities exist (to provide mvcc examples)
+ */
+package cc.alcina.framework.gwt.client.dirndl.layout.doc.example;
