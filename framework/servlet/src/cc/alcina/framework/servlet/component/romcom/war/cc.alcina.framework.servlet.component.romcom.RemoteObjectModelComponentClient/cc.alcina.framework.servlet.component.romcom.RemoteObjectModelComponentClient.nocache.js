a75e90f139720d465e9cbe13e50e6eaee8fc5272
Nick Reddel <nick.reddel@gmail.com>
2024-11-02 17:45:32 -0700

    update romcom build

--- framework/servlet/src/cc/alcina/framework/servlet/component/romcom/war/cc.alcina.framework.servlet.component.romcom.RemoteObjectModelComponentClient/cc.alcina.framework.servlet.component.romcom.RemoteObjectModelComponentClient.nocache.js
+++ framework/servlet/src/cc/alcina/framework/servlet/component/romcom/war/cc.alcina.framework.servlet.component.romcom.RemoteObjectModelComponentClient/cc.alcina.framework.servlet.component.romcom.RemoteObjectModelComponentClient.nocache.js
@@ -335,7 +335,7 @@ function cc_alcina_framework_servlet_component_romcom_RemoteObjectModelComponent
     }
     var strongName;
     try {
-      strongName = 'B4BD4E595EC8BB262DDAA0F29FA8D8DB';
+      strongName = 'B8DEBBAC29450162ADC8B5E9552753DD';
       var idx = strongName.indexOf(':');
       if (idx != -1) {
         softPermutationId = parseInt(strongName.substring(idx + 1), 10);
