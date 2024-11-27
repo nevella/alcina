f417f9f7c2fbaf98b056dcda5aa19bdec7f4779b
Nick Reddel <nick.reddel@gmail.com>
2024-11-27 12:02:15 -0800

    Tweak api

--- framework/servlet/src/cc/alcina/framework/servlet/job/TaskProcessOrphans.java
+++ framework/servlet/src/cc/alcina/framework/servlet/job/TaskProcessOrphans.java
@@ -1,19 +1,8 @@
 package cc.alcina.framework.servlet.job;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import cc.alcina.framework.common.client.job.Job;
-import cc.alcina.framework.common.client.job.Task;
-import cc.alcina.framework.common.client.logic.reflection.reachability.Bean;
-import cc.alcina.framework.common.client.logic.reflection.reachability.Bean.PropertySource;
-import cc.alcina.framework.entity.persistence.domain.descriptor.JobDomain;
-import cc.alcina.framework.entity.persistence.mvcc.Transaction;
 import cc.alcina.framework.servlet.schedule.PerformerTask;
 
-public class TaskProcessOrphans extends PerformerTask {
+public class TaskProcessOrphans extends PerformerTask.Remote {
 	@Override
 	public void run() throws Exception {
 		JobRegistry.get().processOrphans();
