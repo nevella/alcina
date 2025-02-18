0706b1732036c1b390099418fd5ddaf226ec0583
Nick Reddel <nick.reddel@gmail.com>
2025-02-18 10:16:50 -0800

    Tweak hierarchy

--- framework/servlet/src/cc/alcina/framework/servlet/task/TaskDumpStackTraces.java
+++ framework/servlet/src/cc/alcina/framework/servlet/task/TaskDumpStackTraces.java
@@ -3,7 +3,7 @@ package cc.alcina.framework.servlet.task;
 import cc.alcina.framework.entity.SEUtilities;
 import cc.alcina.framework.servlet.schedule.PerformerTask;
 
-public class TaskDumpStackTraces extends PerformerTask {
+public class TaskDumpStackTraces extends PerformerTask.Remote {
 	@Override
 	public void run() throws Exception {
 		SEUtilities.dumpAllThreads();
