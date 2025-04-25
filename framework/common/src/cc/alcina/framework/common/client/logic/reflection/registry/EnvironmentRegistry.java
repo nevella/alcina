074f13e062591214d97eb328c436e2de21cf4184
Nick Reddel <nick.reddel@gmail.com>
2025-04-25 08:49:52 -0700

    Handle infinite loop in shutdown

--- framework/common/src/cc/alcina/framework/common/client/logic/reflection/registry/EnvironmentRegistry.java
+++ framework/common/src/cc/alcina/framework/common/client/logic/reflection/registry/EnvironmentRegistry.java
@@ -63,7 +63,11 @@ public class EnvironmentRegistry extends Registry {
 			if (contextRegistry != null) {
 				return contextRegistry;
 			} else {
-				return delegateProvider.getRegistry();
+				if (delegateProvider == this) {
+					return null;
+				} else {
+					return delegateProvider.getRegistry();
+				}
 			}
 		}
 	}
