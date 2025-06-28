2196ae1ddb7c94ac1b4a5c10a4f59021f941f453
Nick Reddel <nick.reddel@gmail.com>
2025-06-28 16:05:32 -0700

    Update docs

--- framework/common/src/cc/alcina/framework/common/client/logic/permissions/package-info.java
+++ framework/common/src/cc/alcina/framework/common/client/logic/permissions/package-info.java
@@ -27,5 +27,18 @@
  * attribute returns true for the given user, object and context. The evaluator
  * can be found by searching for usages of the string value of the rule()
  * attribute
+ * 
+ * <h3>Uncategorised notes re the interaction between clientinstance and
+ * permissions modelled by PermissionState (context)
+ * 
+ * <code><pre>
+ * * doc - alcina - permissions + identity
+    * a client instance is many things - an owner of a local domain, a record of user interaction with a system
+    * by default server threads have no permission/identity context. This is because implicit context in a stackable context system is just...no
+        * TODO - a transaction.begin requires.a ClientInstance (identity context)
+        * note that rpc threads currently have the client user context - and need to be explicitly raised to allow domain mutation
+ * 
+ * </pre></code>
+ * 
  */
 package cc.alcina.framework.common.client.logic.permissions;
