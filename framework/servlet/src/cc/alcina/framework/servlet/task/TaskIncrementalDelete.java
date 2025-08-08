bcc9fa420176ede974f24f7ecd55bfd9bdd4ae17
Nick Reddel <nick.reddel@gmail.com>
2025-08-08 08:16:39 -0700

    Tweak logging

--- framework/servlet/src/cc/alcina/framework/servlet/task/TaskIncrementalDelete.java
+++ framework/servlet/src/cc/alcina/framework/servlet/task/TaskIncrementalDelete.java
@@ -10,11 +10,16 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 import cc.alcina.framework.common.client.logic.reflection.reachability.Bean;
 import cc.alcina.framework.common.client.logic.reflection.reachability.Bean.PropertySource;
+import cc.alcina.framework.common.client.util.SystemoutCounter;
 import cc.alcina.framework.servlet.schedule.PerformerTask;
 
 @Bean(PropertySource.FIELDS)
 public class TaskIncrementalDelete extends PerformerTask {
 	public String path;
+
+	public boolean logFiles = true;
+
+	transient SystemoutCounter counter = new SystemoutCounter(100, 100);
 
 	@Override
 	public void run() throws Exception {
@@ -23,7 +28,10 @@ public class TaskIncrementalDelete extends PerformerTask {
 			public FileVisitResult visitFile(Path file,
 					BasicFileAttributes attrs) throws IOException {
 				file.toFile().delete();
-				logger.info("deleted {}", file);
+				if (logFiles) {
+					logger.info("deleted {}", file);
+				}
+				counter.tick();
 				return FileVisitResult.CONTINUE;
 			}
 		};
