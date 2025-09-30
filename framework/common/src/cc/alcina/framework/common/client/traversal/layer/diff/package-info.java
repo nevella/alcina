6b2e5efa7f4cd415494228ae7f5557b980eff293
Nick Reddel <nick.reddel@gmail.com>
2025-09-30 08:24:22 -0700

    Add diff docs

--- framework/common/src/cc/alcina/framework/common/client/traversal/layer/diff/package-info.java
+++ framework/common/src/cc/alcina/framework/common/client/traversal/layer/diff/package-info.java
@@ -1,4 +1,49 @@
 /**
  * Create a union tree of two dom subtrees, with correspondences merged
  */
+/*
+ * @formatter:off
+ 
+notes on diff
+
+* if a text node is being appended to a container, the previous text -identity- structure must match - e.g.
+
+div.a0
+  p.a1		p.a3
+    $text.a2  		$text.a4
+			
+  -->
+div.b0
+  p.b1		p.b3
+    $text.b2  		$text.b4. because $text.a4 [parent] != $text.a2 [parent]
+
+
+how this affects table merges
+
+
+table.a0
+  tr.a1                
+    td.a2              td.a4
+        $t[bruce].a3            $t[cat].a5
+
+table.b0
+  tr.b1                
+    td.b2              td.b4
+        $t[bruce].b3.            $t[dog].b5
+
+table.a0
+  tr.a1                
+    td.a2              td.a4
+        $t[cat].a3            $t[bruce].a5
+
+table.b0
+  tr.b1                
+    td.b2              td.b4
+        $t[dog].b3.            $t[bruce].b5
+
+
+
+
+ * @formatter:on
+ */
 package cc.alcina.framework.common.client.traversal.layer.diff;
