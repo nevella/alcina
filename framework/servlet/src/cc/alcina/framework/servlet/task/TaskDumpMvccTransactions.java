0c60b7bf5d4bc7e9a1fc5188f32c780fc87d4c26
Nick Reddel <nick.reddel@gmail.com>
2025-10-29 18:58:23 -0700

    Change class signature

--- framework/servlet/src/cc/alcina/framework/servlet/task/TaskDumpMvccTransactions.java
+++ framework/servlet/src/cc/alcina/framework/servlet/task/TaskDumpMvccTransactions.java
@@ -3,7 +3,7 @@ package cc.alcina.framework.servlet.task;
 import cc.alcina.framework.entity.persistence.mvcc.Transactions;
 import cc.alcina.framework.servlet.schedule.PerformerTask;
 
-public class TaskDumpMvccTransactions extends PerformerTask {
+public class TaskDumpMvccTransactions extends PerformerTask.Remote {
 	@Override
 	public void run() throws Exception {
 		logger.warn(Transactions.stats().describeTransactions());
