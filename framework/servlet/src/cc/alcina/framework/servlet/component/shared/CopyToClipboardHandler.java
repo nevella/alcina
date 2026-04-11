4af4fdef9e1cd93656857f01fbc9f12ec33d64b1
Nick Reddel <nick.reddel@gmail.com>
2026-04-11 10:32:25 -0700

    use host clipboard util for console, since it may be a non-secure host

--- framework/servlet/src/cc/alcina/framework/servlet/component/shared/CopyToClipboardHandler.java
+++ framework/servlet/src/cc/alcina/framework/servlet/component/shared/CopyToClipboardHandler.java
@@ -2,6 +2,8 @@ package cc.alcina.framework.servlet.component.shared;
 
 import com.google.gwt.dom.client.Document;
 
+import cc.alcina.framework.common.client.util.Ax;
+import cc.alcina.framework.entity.ConsoleUtil;
 import cc.alcina.framework.gwt.client.dirndl.cmp.status.StatusModule;
 import cc.alcina.framework.gwt.client.dirndl.event.ModelEvents;
 import cc.alcina.framework.gwt.client.dirndl.event.ModelEvents.CopyToClipboard;
@@ -10,7 +12,11 @@ public interface CopyToClipboardHandler
 		extends ModelEvents.CopyToClipboard.Handler {
 	@Override
 	default void onCopyToClipboard(CopyToClipboard event) {
-		Document.get().writeClipboardText(event.getModel());
+		if (Ax.isTest()) {
+			ConsoleUtil.copyToClipboard(event.getModel());
+		} else {
+			Document.get().writeClipboardText(event.getModel());
+		}
 		StatusModule.get().showMessageTransitional("Copied to clipboard");
 	}
 }
