c5b8a7602d61648921c782744310073cfa46c867
Nick Reddel <nick.reddel@gmail.com>
2026-05-20 14:41:33 -0700

    Fix task persistence issue

--- framework/servlet/src/cc/alcina/framework/servlet/task/TaskScheduleJob.java
+++ framework/servlet/src/cc/alcina/framework/servlet/task/TaskScheduleJob.java
@@ -1,7 +1,7 @@
 package cc.alcina.framework.servlet.task;
 
 import cc.alcina.framework.common.client.job.JobState;
-import cc.alcina.framework.common.client.job.Task;
+import cc.alcina.framework.common.client.serializer.ReflectiveSerializer;
 import cc.alcina.framework.entity.persistence.domain.descriptor.JobDomain;
 import cc.alcina.framework.entity.persistence.mvcc.Transaction;
 import cc.alcina.framework.servlet.job.JobRegistry;
@@ -9,14 +9,14 @@ import cc.alcina.framework.servlet.job.JobRegistry.Builder;
 import cc.alcina.framework.servlet.schedule.PerformerTask;
 
 public class TaskScheduleJob extends PerformerTask.Remote {
-	public Task task;
-
 	public JobDomain.DefaultConsistencyPriorities priority;
+
+	public String taskSerialized;
 
 	@Override
 	public void run() throws Exception {
 		Builder builder = JobRegistry.createBuilder();
-		builder.withTask(task);
+		builder.withTask(ReflectiveSerializer.deserialize(taskSerialized));
 		if (priority != null) {
 			builder.withInitialState(JobState.FUTURE_CONSISTENCY)
 					.withConsistencyPriority(priority);
